4a8e1afc8d306e4f371c9eecfa337a703bbf89ea
Joel Hooks <joelhooks@gmail.com>
2021-08-13 15:24:27 -0700

    #31 extract quiz and related react components

---
+++ packages/quiz/src/components/forms/quiz/completed.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import type {Question, Questions} from '@skillrecordings/types'
+import last from 'lodash/last'
+
+const CompletedMessage: React.FC<{
+  answeredCorrectly?: boolean
+  neutral?: boolean
+  question: Question
+  questions: Questions
+  title?: string
+  author?: string
+}> = ({
+  answeredCorrectly,
+  neutral = false,
+  question,
+  questions,
+  title = 'Skill Recordings Product',
+  author = 'The Teacher',
+}) => {
+  const questionsKeys: string[] = Object.keys(questions)
+  const lastQuestionKey: string = last(questionsKeys) || ''
+  const isLast: boolean = questions[lastQuestionKey].tagId === question.tagId
+  const srMessage = <span className="sr-only">Quiz complete.&nbsp;</span>
+  const answeredMessageRef: any = React.useRef()
+  React.useEffect(() => {
+    answeredMessageRef.current.focus()
+  }, [])
+  return (
+    <>
+      {neutral ? (
+        <div className="pt-10 prose prose-lg text-center sm:prose-xl dark:prose-dark">
+          {isLast ? (
+            <p tabIndex={-1} ref={answeredMessageRef}>
+              {srMessage}
+              This was the last lesson from the {title} email course. We hope
+              you learned something new, and we look forward to sharing more in
+              the future!
+            </p>
+          ) : (
+            <p tabIndex={-1} ref={answeredMessageRef}>
+              {srMessage}
+              <span>
+                Thanks for submitting your answer! We'll send the next lesson in
+                5-10 minutes. Check your inbox.
+              </span>
+            </p>
+          )}
+          <p>
+            Thanks, <br /> {author}
+          </p>
+        </div>
+      ) : answeredCorrectly ? (
+        <div className="pt-10 mt-10 prose prose-lg text-center border-t border-gray-200 sm:prose-xl dark:prose-dark dark:border-gray-700">
+          <p tabIndex={-1} ref={answeredMessageRef}>
+            Nice work. You chose the correct answer!
+          </p>
+          {isLast ? (
+            <p>
+              This was the last lesson from the {title} email course. We hope
+              you learned something new, and I look forward to sharing more in
+              the future!
+            </p>
+          ) : (
+            <p>We'll send the next lesson in 5-10 minutes. Check your inbox.</p>
+          )}
+          <p>
+            Thanks, <br /> {author}
+          </p>
+        </div>
+      ) : (
+        <div className="pt-10 mt-10 prose prose-lg text-center border-t border-gray-200 sm:prose-xl dark:prose-dark dark:border-gray-700">
+          <p tabIndex={-1} ref={answeredMessageRef}>
+            You chose an incorrect answer, but don't worry. Just go back and
+            re-read the email and check out any linked resources. You can
+            refresh the page if you'd like to try again! 👍
+          </p>
+          {isLast ? (
+            <p>
+              This was the last lesson from the {title} email course. We hope
+              you learned something new, and I look forward to sharing more in
+              the future!
+            </p>
+          ) : (
+            <p>
+              We'll send the next email in 5-10 minutes too so you can learn
+              more.
+            </p>
+          )}
+          <p>
+            Thanks, <br /> {author}
+          </p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default CompletedMessage
