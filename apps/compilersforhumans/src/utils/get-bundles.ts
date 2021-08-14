2647710b584c0b0d15a6c1e3d96d890f086950b5
Joel Hooks <joelhooks@gmail.com>
2021-08-14 11:09:36 -0700

    #32 various config and build stuff

--- apps/compilersforhumans/src/utils/get-bundles.ts
+++ apps/compilersforhumans/src/utils/get-bundles.ts
@@ -1,5 +1,5 @@
-import DevBundles from '../../data/bundles.development.json'
-import ProdBundles from '../../data/bundles.production.json'
+import DevBundles from '../data/bundles.development.json'
+import ProdBundles from '../data/bundles.production.json'
 
 const getBundles = () => {
   return process.env.NODE_ENV === 'production' ? ProdBundles : DevBundles
--- apps/compilersforhumans/src/utils/get-collections.ts
+++ apps/compilersforhumans/src/utils/get-collections.ts
@@ -1,5 +1,5 @@
-import DevCollections from '../../data/collections.development.json'
-import ProdCollections from '../../data/collections.production.json'
+import DevCollections from '../data/collections.development.json'
+import ProdCollections from '../data/collections.production.json'
 
 const getCollections = () => {
   return process.env.NODE_ENV === 'production'
