2b74a92abe68b5db100d32877e06749ff1ec91fe
Vojta Holik <vojta@egghead.io>
2021-08-26 15:11:59 +0200

    add "the future of remote work" article

--- apps/engmanagement/src/components/navigation.tsx
+++ apps/engmanagement/src/components/navigation.tsx
@@ -1,21 +1,35 @@
 import * as React from 'react'
-import DarkModeToggle from './color-mode-toggle'
 import config from '../config'
 import Link from 'next/link'
 import {useViewer} from '@skillrecordings/viewer'
+import {useRouter} from 'next/router'
 
 const Navigation = () => {
   const {isAuthenticated, logout} = useViewer()
+  const router = useRouter()
   return (
-    <nav className="absolute top-0 left-0 p-5 w-full flex items-center justify-center print:hidden z-20">
+    <nav className="absolute top-0 left-0 px-5 w-full flex items-center justify-between print:hidden z-20">
       <Link href="/">
-        <a className="uppercase font-din text-orange-50 hover:text-orange-200 text-2xl max-w-[250px] text-center tracking-wide leading-tight">
-          {config.defaultTitle}
+        <a className="ml-5 font-din pt-4 scale-90 uppercase text-white text-2xl tracking-wide leading-none hover:opacity-100 opacity-90 transition-all ease-in-out duration-300 relative">
+          {/* {config.defaultTitle} */}
+          <div className="w-[1px] h-[120%] bg-gray-200 opacity-80 absolute -left-3 top-[-22%]" />
+          <span className="tracking-wider ">Engineering</span>
+          <span className="block"> Management</span>
+          <span className="normal-case font-light font-souvenir block text-orange-300 text-[0.735rem]">
+            with Sarah Drasner
+          </span>
         </a>
       </Link>
-      <div className="flex space-x-3 items-center">
+      {router.pathname !== '/' && (
+        <Link href="/">
+          <a className="mt-3 rounded-full scale-90 px-5 py-3 text-sm border border-gray-400 border-opacity-30 hover:bg-white hover:bg-opacity-5 transition-all duration-300 ease-in-out">
+            I wrote a book →
+          </a>
+        </Link>
+      )}
+      {/* <div className="flex space-x-3 items-center">
         {isAuthenticated && <button onClick={logout}>log out</button>}
-      </div>
+      </div> */}
     </nav>
   )
 }
--- apps/engmanagement/src/templates/article.tsx
+++ apps/engmanagement/src/templates/article.tsx
@@ -4,42 +4,183 @@ import PoliteConvertkitForm from '@skillrecordings/convertkit/dist/forms/polite'
 import Markdown from 'react-markdown'
 import config from '../config'
 import Image from 'next/image'
+import {format} from 'date-fns'
+import {
+  Twitter,
+  Facebook,
+  Reddit,
+  CopyToClipboard,
+  LinkedIn,
+  Hacker,
+} from 'components/share'
+import {useRouter} from 'next/router'
+import ConvertkitSubscribeForm from '@skillrecordings/convertkit/dist/forms'
+import BookCover from '../../public/images/engineering-management-for-the-rest-of-us-book-mockup-perspective@2x.png'
 
 type ArticleTemplateProps = {
   meta?: any
 }
 
+type AuthorProps = {
+  name: string
+  image: any
+  url: string
+}
+
+const ShareArticle: React.FC<{title: string}> = ({children, title}) => {
+  const router = useRouter()
+  return (
+    <div className="flex items-center md:flex-row flex-col md:space-x-2 md:space-y-0 space-y-5">
+      {children}
+      <div className="flex items-center justify-center flex-wrap md:pb-0">
+        <Twitter
+          link={`https://${config.siteUrl}${router.pathname}`}
+          message={`${title}, by ${config.twitter.handle}`}
+        />
+        <Facebook link={`https://${config.siteUrl}${router.pathname}`} />
+        <Reddit
+          link={`https://${config.siteUrl}${router.pathname}`}
+          message={`${title}, by ${config.author}`}
+        />
+        <LinkedIn link={`https://${config.siteUrl}${router.pathname}`} />
+        <Hacker
+          link={`https://${config.siteUrl}${router.pathname}`}
+          message={`${title}, by ${config.author}`}
+        />
+        <CopyToClipboard link={`https://${config.siteUrl}${router.pathname}`} />
+      </div>
+    </div>
+  )
+}
+
+const Author: React.FC<AuthorProps> = ({name, image, url}) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className="group">
+      <div className="relative z-10 flex items-center justify-center">
+        <div className="rounded-full border-2 border-orange-300 flex items-center justify-center">
+          <Image
+            src={image}
+            alt={name}
+            width={60}
+            height={60}
+            className="rounded-full"
+          />
+        </div>
+        <div className="pl-3 flex flex-col">
+          <span className="text-sm text-orange-300 leading-none">
+            Article by
+          </span>
+          <span className="text-lg group-hover:underline">{name}</span>
+        </div>
+      </div>
+    </a>
+  )
+}
+
 const ArticleTemplate: React.FC<ArticleTemplateProps> = ({meta, children}) => {
-  const {title, image} = meta
+  const {title, image, published, card, background} = meta
+  const author = meta.author || {
+    name: config.author,
+    image: require('../../public/images/sarah-drasner@2x.jpg'),
+    url: 'https://twitter.com/sarah_edo',
+  }
+  const dateFormatted = format(new Date(published), 'MMMM dd, y')
 
   return (
-    <Layout meta={meta}>
+    <Layout meta={meta} className="bg-[#111725]">
       <article>
-        <header className="py-64 relative flex flex-col items-center justify-center">
-          {image && (
-            <div className="lg:w-[800px] w-auto absolute">
-              <Image src={image} alt="" placeholder="blur" />
+        <header>
+          <div className="pt-40 min-h-[750px] -mx-5 px-5 -mt-5 relative flex flex-col items-center justify-between">
+            {title && (
+              <h1 className="sm:text-7xl text-5xl font-bold font-din uppercase text-center relative z-10 py-8">
+                <Markdown>{title}</Markdown>
+              </h1>
+            )}
+            <Image
+              src={image}
+              alt=""
+              layout="fill"
+              className="md:object-contain object-cover object-top"
+              quality={100}
+              placeholder="blur"
+            />
+          </div>
+          <div className="max-w-screen-sm w-full mx-auto flex md:flex-row flex-col items-center md:justify-between justify-center md:space-y-0 space-y-10 pb-16">
+            <Author {...author} />
+            <div>
+              <ShareArticle title={title}>
+                <time
+                  className="pr-2 text-xs text-gray-400 opacity-80"
+                  dateTime={published}
+                >
+                  {dateFormatted}
+                </time>
+              </ShareArticle>
             </div>
-          )}
-          {title && (
-            <h1 className="text-8xl font-bold font-din uppercase text-center relative z-10">
-              <Markdown>{title}</Markdown>
-            </h1>
-          )}
+          </div>
         </header>
-        <main className="prose prose-dark sm:prose-xl max-w-screen-md prose-lg mx-auto">
-          {children}
-        </main>
-        <footer className="mx-auto max-w-screen-md border-t dark:border-gray-800 border-gray-200 py-16">
-          by{' '}
-          {meta.contributors
-            ? meta.contributors[0].name
-            : config.additionalMetaTags[0].content}
+        <div className="relative overflow-hidden -mx-5 px-5">
+          <main className="prose prose-dark sm:prose-xl max-w-screen-sm prose-lg mx-auto relative z-10">
+            {children}
+          </main>
+          {background && background}
+        </div>
+        <footer className="mx-auto max-w-screen-md flex md:flex-row flex-col md:space-y-0 space-y-10 items-center w-full justify-between pt-24 pb-32">
+          <Author {...author} />
+          <ShareArticle title={title}>
+            <div className="text-sm text-gray-300 opacity-80 pr-2">
+              Share with your friends
+            </div>
+          </ShareArticle>
         </footer>
+        <section className=" max-w-screen-md w-full mx-auto sm:pb-48 pb-28">
+          <div className="flex items-start justify-center ">
+            <div className="w-full">
+              <h4 className="sm:text-5xl text-4xl font-medium pb-10 leading-none font-din uppercase text-center">
+                Subscribe for More
+              </h4>
+              <ConvertkitSubscribeForm
+                classNames={{
+                  form: 'max-w-xs mx-auto space-y-4',
+                  input:
+                    'focus:outline-none focus:ring-2 focus:ring-orange-300 border-none rounded-lg bg-white text-black placeholder-coolGray-400 w-full',
+                  label: 'block pb-1 font-medium',
+                  asterisk: 'text-orange-300',
+                  buttonContainer: 'flex items-center w-full justify-center',
+                  button:
+                    'px-6 py-3 rounded-lg border-orange-400 font-semibold mt-4 hover:scale-105 transition-all duration-300 ease-in-out border shadow-inner bg-orange-400 bg-opacity-5 hover:bg-opacity-20',
+                  comment: 'text-center text-sm text-gray-300 opacity-60 pt-6',
+                }}
+              />
+            </div>
+            <div className="">
+              {/* <div className="scale-[0.4] font-din lg:text-7xl text-6xl uppercase leading-[90%]">
+                Engineering
+                <br />
+                Management
+                <br />
+                <div className="leading-[100%] transform sm:-translate-y-5 -translate-y-4">
+                  <span className="font-souvenir font-medium inline-block tracking-wider lg:text-4xl text-3xl text-orange-300">
+                    for the
+                  </span>
+                  <span className="block">Rest of Us</span>
+                </div>
+              </div> */}
+              {/* <div className="max-w-[350px]">
+                <Image
+                  placeholder="blur"
+                  src={BookCover}
+                  alt="Engineering Management for the Rest of Us book by Sarah Drasner"
+                  quality={95}
+                />
+              </div> */}
+            </div>
+          </div>
+        </section>
       </article>
-      <PoliteConvertkitForm peakingContent={'Hello!'}>
+      {/* <PoliteConvertkitForm peakingContent={'Hello!'}>
         <p>Subscribe today!</p>
-      </PoliteConvertkitForm>
+      </PoliteConvertkitForm> */}
     </Layout>
   )
 }
