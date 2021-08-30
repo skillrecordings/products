852784c05622defa5564a7619f91152b3abf1071
Vojta Holik <vojta@egghead.io>
2021-08-30 12:52:21 +0200

    get ck subscriber tags

--- packages/convertkit/src/utils/fetch-convertkit-subscriber.ts
+++ packages/convertkit/src/utils/fetch-convertkit-subscriber.ts
@@ -3,7 +3,12 @@ import {convertkitAxios} from '@skillrecordings/axios'
 import {first, isEmpty} from 'lodash'
 import serializeConvertkitCookie from './serialize-convertkit-cookie'
 import * as serverCookie from 'cookie'
-import {ACCESS_TOKEN_KEY, CK_SUBSCRIBER_KEY} from '@skillrecordings/config'
+import {
+  ACCESS_TOKEN_KEY,
+  CK_SUBSCRIBER_KEY,
+  CONVERTKIT_TOKEN,
+  CONVERTKIT_API_SECRET,
+} from '@skillrecordings/config'
 
 interface ParsedCookie {
   [key: string]: string
@@ -24,8 +29,7 @@ function getTokenFromCookieHeaders(serverCookies: string = '') {
 export default async function fetchConvertkitSubscriberFromServerCookie(
   header: string,
 ) {
-  if (!process.env.CONVERTKIT_API_SECRET)
-    throw new Error('No Convertkit Secret Key Found')
+  if (!CONVERTKIT_API_SECRET) throw new Error('No Convertkit Secret Key Found')
 
   const {convertkitId, eggheadToken} = getTokenFromCookieHeaders(header)
 
@@ -37,19 +41,23 @@ export default async function fetchConvertkitSubscriberFromServerCookie(
       throw new Error('unable to load convertkit subscriber')
     subscriber = await convertkitAxios
       .get(
-        `/subscribers?api_secret=${process.env.CONVERTKIT_API_SECRET}&email_address=${eggheadUser.email}`,
+        `/subscribers?api_secret=${CONVERTKIT_API_SECRET}&email_address=${eggheadUser.email}`,
       )
       .then(({data}) => first(data.subscribers))
   } else {
     subscriber = await convertkitAxios
-      .get(
-        `/subscribers/${convertkitId}?api_secret=${process.env.CONVERTKIT_API_SECRET}`,
-      )
+      .get(`/subscribers/${convertkitId}?api_secret=${CONVERTKIT_API_SECRET}`)
       .then(({data}) => data.subscriber)
   }
 
   if (isEmpty(subscriber))
     throw new Error('no convertkit subscriber was loaded')
+
+  const tags = await convertkitAxios
+    .get(`/subscribers/${subscriber.id}/tags?api_key=${CONVERTKIT_TOKEN}`)
+    .then(({data}) => data.tags)
+
+  subscriber = {...subscriber, tags}
 
   const ckCookie = serializeConvertkitCookie(subscriber.id)
 
