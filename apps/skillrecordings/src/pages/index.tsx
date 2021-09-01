3144f07c54fcd4e91b55dc0e487429c8cb8c9684
Joel Hooks <joelhooks@gmail.com>
2021-09-01 14:38:57 -0700

    bring that logo back

--- apps/skillrecordings/src/pages/index.tsx
+++ apps/skillrecordings/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import Layout from 'components/app/layout'
+import SkillRecordingsLogo from '../components/icons/sr-logo'
 
 export default function Home() {
   return (
     <>
-      <Layout meta={{title: 'Skill Recordings'}}></Layout>
+      <Layout meta={{title: 'Skill Recordings'}}>
+        <SkillRecordingsLogo fill={true} className="text-pink-500 p-1 w-full" />
+      </Layout>
     </>
   )
 }
