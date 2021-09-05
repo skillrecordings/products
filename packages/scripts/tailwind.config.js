24c136cd09180446579c1156fbf4c96b4f55dc02
Joel Hooks <joelhooks@gmail.com>
2021-09-04 20:05:51 -0700

    don't purge player styles

--- packages/scripts/tailwind.config.js
+++ packages/scripts/tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       './node_modules/@skillrecordings/commerce/dist/**/*.js',
       './node_modules/@skillrecordings/react/dist/**/*.js',
       './node_modules/@skillrecordings/convertkit/dist/**/*.js',
-      './node_modules/@skillrecordings/quiz/dist/**/*.js'
+      './node_modules/@skillrecordings/quiz/dist/**/*.js',
+      './node_modules/@skillrecordings/player/dist/**/*.js',
     ],
   },
 }
