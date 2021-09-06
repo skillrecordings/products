860af7f1fbe78d12b2d602cad21cc654c7129583
Joel Hooks <joelhooks@gmail.com>
2021-09-05 17:39:48 -0700

    playback rate control

---
+++ packages/player/src/components/menu/menu-button.tsx
@@ -0,0 +1,152 @@
+import * as React from 'react'
+import cx from 'classnames'
+import {ClickableComponent} from '../clickable-component'
+import {Menu} from './menu'
+import {MenuItem} from './menu-item'
+
+export const MenuButton: React.FC<any> = (props) => {
+  const [active, setActive] = React.useState(false)
+  const [activateIndex, setActivateIndex] = React.useState(0)
+  const {inline, className} = props
+  const menuButtonRef = React.useRef(null)
+
+  function handleIndexChange(index: number) {
+    const {onSelectItem} = props
+    onSelectItem(index)
+  }
+
+  function commitSelection(index: number) {
+    setActivateIndex(index)
+    handleIndexChange(index)
+  }
+
+  function handleSelectItem(i: number) {
+    commitSelection(i)
+  }
+
+  function handleClick() {
+    setActive((prevActive) => !prevActive)
+  }
+
+  function handleFocus() {
+    document.addEventListener('keydown', handleKeyPress)
+  }
+
+  function handleBlur() {
+    setActive(false)
+    document.removeEventListener('keydown', handleKeyPress)
+  }
+
+  function activateMenuItem(index: number) {
+    setActivateIndex(index)
+    handleIndexChange(index)
+  }
+
+  function handleUpArrow(e: KeyboardEvent) {
+    const {items} = props
+    if (active) {
+      e.preventDefault()
+      let newIndex = activateIndex - 1
+      if (newIndex < 0) {
+        newIndex = items.length ? items.length - 1 : 0
+      }
+      activateMenuItem(newIndex)
+    }
+  }
+
+  function handleDownArrow(e: KeyboardEvent) {
+    const {items} = props
+    if (active) {
+      e.preventDefault()
+      let newIndex = activateIndex + 1
+      if (newIndex >= items.length) {
+        newIndex = 0
+      }
+      activateMenuItem(newIndex)
+    }
+  }
+
+  function handleTab(e: KeyboardEvent) {
+    if (active) {
+      e.preventDefault()
+      commitSelection(activateIndex)
+    }
+  }
+
+  function handleReturn(e: KeyboardEvent) {
+    e.preventDefault()
+    if (active) {
+      commitSelection(activateIndex)
+    } else {
+      setActive(true)
+    }
+  }
+
+  function handleEscape() {
+    setActive(false)
+    setActivateIndex(0)
+  }
+
+  function handleKeyPress(event: KeyboardEvent) {
+    // Escape (27) key
+    if (event.which === 27) {
+      handleEscape()
+    } else if (event.which === 9) {
+      // Tab (9) key
+      handleTab(event)
+    } else if (event.which === 13) {
+      // Enter (13) key
+      handleReturn(event)
+    } else if (event.which === 38) {
+      // Up (38) key
+      handleUpArrow(event)
+    } else if (event.which === 40) {
+      // Down (40) key press
+      handleDownArrow(event)
+    }
+  }
+
+  function renderMenu() {
+    if (!active) {
+      return null
+    }
+
+    const {items} = props
+    return (
+      <Menu>
+        {items.map((item: string, i: number) => (
+          <MenuItem
+            item={item}
+            index={i}
+            onSelectItem={handleSelectItem}
+            activateIndex={activateIndex}
+            key={`item-${i++}`}
+          />
+        ))}
+      </Menu>
+    )
+  }
+
+  return (
+    <ClickableComponent
+      className={cx(
+        className,
+        {
+          'cueplayer-react-menu-button-inline': !!inline,
+          'cueplayer-react-menu-button-popup': !inline,
+          'cueplayer-react-menu-button-active': active,
+        },
+        'cueplayer-react-control cueplayer-react-button cueplayer-react-menu-button',
+      )}
+      role="button"
+      tabIndex={0}
+      ref={menuButtonRef}
+      onClick={handleClick}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    >
+      {props.children}
+      {renderMenu()}
+    </ClickableComponent>
+  )
+}
---
+++ packages/player/src/components/menu/menu-item.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import cx from 'classnames'
+
+export const MenuItem: React.FC<any> = (props) => {
+  const {item, index, activateIndex} = props
+
+  function handleClick() {
+    const {index, onSelectItem} = props
+    onSelectItem(index)
+  }
+
+  return (
+    <li
+      className={cx({
+        'cueplayer-react-menu-item': true,
+        'cueplayer-react-selected': index === activateIndex,
+      })}
+      role="menuitem"
+      onClick={handleClick}
+    >
+      {item.label}
+      <span className="cueplayer-react-control-text" />
+    </li>
+  )
+}
