7b3d5c448d26460c6cc0e0d99ab7575544cdb0e9
Taylor Bell <taylorbell@gmail.com>
2021-09-07 14:23:30 -0600

    quiz tags and questions

--- apps/compilersforhumans/src/pages/answer.tsx
+++ apps/compilersforhumans/src/pages/answer.tsx
@@ -39,7 +39,7 @@ export async function getStaticProps() {
       type: `multiple-choice`,
       tagId: 2514822, // ec - c4h - 002 Three Steps completed
       correct: ['parsing'],
-      answer: ``,
+      answer: `Parsing failed. When attempting to transform your code string into an AST the parser came across a token it didn’t recognize, findLast. So it stopped parsing and threw an error.`,
       choices: [
         {
           answer: 'parsing',
@@ -59,20 +59,45 @@ export async function getStaticProps() {
         },
       ],
     },
-    asts: {
-      question: `Given the line of code \`const example = true;\`, how does the AST representation change if we use \`let\` instead? What if we change \`example\` to be a string \`"My new boolean"\`? Use [astexplorer.net](https://astexplorer.net) for help!`,
+    parsing: {
+      question: `## What do you think an AST looks like? What sort of information does it need to contain in order to faithfully represent a program? How is it different than the parse tree we just looked at?`,
       type: `essay`,
-      tagId: 2514826, // ec - c4h - 003 ASTs completed
+      tagId: 2514826, // ec - e4h - 003 Parsing completed
     },
-    transform: {
-      question: `Given the line of code \`let example = true;\`, write a transform that will replace \`let\` with \`const\` for only this line. Submit your code snippet below.`,
+    asts: {
+      question: `## ASTs are cool, but how are they used? Can you think of what comes next? What does Internet Explorer’s JS engine take as input?`,
       type: `essay`,
-      tagId: 2514835, // ec - c4h - 004 Transform completed
+      tagId: 2514835, // ec - c4h - 004 - ASTs completed
     },
     codegen: {
-      question: `## What compiler topics are you most interested in exploring further?`,
+      question: `## As we’ve just discussed, codegen takes an AST and gives us back code in string form. So where in this three step compilation process are all of our changes happening?`,
       type: `essay`,
       tagId: 2514839, // ec - c4h - 005 codegen completed
+      correct: ['transform'],
+      answer: `The answer is transform. Code is easiest to change when its in the form of an AST. The goal of the parsing step is to create the AST and the goal of the codegen step is to transform the AST back into code.`,
+      choices: [
+        {
+          answer: 'parsing',
+          label: 'Parsing',
+        },
+        {
+          answer: 'transform',
+          label: 'Transform',
+        },
+        {
+          answer: 'codegen',
+          label: 'Code Generation',
+        },
+        {
+          answer: 'all',
+          label: 'All of the Above',
+        },
+      ],
+    },
+    transform: {
+      question: `## Given the line of code \`let example = true;\`, write a transform that will replace \`let\` with \`const\` for only this line. Submit your code snippet below.`,
+      type: `essay`,
+      tagId: 2610486, // ec - c4h - 005 Codegen completed
     },
   }
   return {
