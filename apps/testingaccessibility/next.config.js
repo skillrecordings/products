1982d33d0d82c7e9798166615ca3dad9ccd80982
Vojta Holik <vojta@egghead.io>
2021-10-12 16:24:58 +0200

    next@canary

--- apps/testingaccessibility/next.config.js
+++ apps/testingaccessibility/next.config.js
@@ -1,1 +1,50 @@
-module.exports = require('@skillrecordings/scripts/next.config.js')
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+const withMDX = require('@next/mdx')()
+
+const IMAGE_HOST_DOMAINS = [
+  `res.cloudinary.com`,
+  `d2eip9sf3oo6c2.cloudfront.net`,
+  `cdn.sanity.io`,
+]
+
+const nextConfig = {
+  experimental: {
+    outputFileTracing: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 😭
+  },
+  webpack: (config, {isServer}) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+    return config
+  },
+  reactStrictMode: true,
+  images: {
+    domains: IMAGE_HOST_DOMAINS,
+  },
+  async redirects() {
+    return []
+  },
+}
+
+module.exports = withPlugins(
+  [
+    withImages(),
+    withMDX({
+      pageExtensions: ['ts', 'tsx', 'mdx'],
+      remarkPlugins: [
+        require('remark-slug'),
+        require('remark-footnotes'),
+        require('remark-code-titles'),
+      ],
+      rehypePlugins: [require('mdx-prism')],
+    }),
+  ],
+  nextConfig,
+)
