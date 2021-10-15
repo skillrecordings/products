840e4f0f2d3b3f190ebf7ce8c578172c8969d449
Vojta Holik <vojta@egghead.io>
2021-10-15 13:58:34 +0200

    simplified file structure

--- packages/quiz/src/index.ts
+++ packages/quiz/src/index.ts
@@ -1,4 +1,3 @@
 export {default as useQuizQuestion} from './hooks/use-quiz-question'
 export {default as QuizAnswerPage} from './pages/answer'
 export {default as quizAnswerApiHandler} from './pages/api/answer'
-export * from './components/forms/quiz'
