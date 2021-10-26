e3090a5416db1b2604bc576c69135108354055ed
Taylor Bell <taylorbell@gmail.com>
2021-10-26 16:28:57 -0600

    add experience page

---
+++ apps/cssdestructured/src/pages/experience.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import config from '../config'
+import {useRouter} from 'next/router'
+import axios from 'axios'
+import isEmpty from 'lodash/isEmpty'
+import {useConvertkit} from '@skillrecordings/convertkit'
+
+const Experience = () => {
+  const router = useRouter()
+  const {subscriber} = useConvertkit()
+  const tagId = Number(router.query?.tag)
+
+  const message = (tagId: number) => {
+    switch (tagId) {
+      case 2710250:
+        return 'looking for your first job'
+      case 2710261:
+        return 'a junior developer'
+      case 2710262:
+        return 'a senior developer'
+      default:
+        return 'a developer'
+    }
+  }
+
+  React.useEffect(() => {
+    if (tagId && !isEmpty(subscriber)) {
+      axios
+        .post('/api/convertkit/subscribe', {...subscriber, tag: tagId})
+        .catch((err) => console.log(err))
+    }
+  }, [tagId, subscriber])
+
+  console.log(`use ck ${useConvertkit}`)
+  console.log(`subscriber: ${subscriber}`)
+  return (
+    <>
+      <div className="prose dark:prose-dark prose-lg max-w-md mx-auto py-24">
+        <h1>Thanks!</h1>
+        <p>
+          Thanks for letting me know you're a {message(tagId)}.<br />
+          I'm sure you'll find something helpful in CSS Destructured!
+        </p>
+        <p>
+          Watch your inbox for more,
+          <br />
+          Emma
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Experience
