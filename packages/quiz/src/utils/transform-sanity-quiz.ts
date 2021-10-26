2a4e7e25d1cc0697ded7061a7f6c115b2d569c26
Vojta Holik <vojta@egghead.io>
2021-10-26 16:22:41 +0200

    load quiz data from sanity

---
+++ packages/quiz/src/utils/transform-sanity-quiz.ts
@@ -0,0 +1,29 @@
+import {QuestionSet} from '@skillrecordings/types'
+import isEmpty from 'lodash/isEmpty'
+
+type SanityQuiz = {
+  title: string
+  slug: string
+  questions: any
+}
+
+export default function transformSanityQuiz(quiz: SanityQuiz) {
+  const questionSet: QuestionSet = quiz.questions.reduce((a: any, v: any) => {
+    return {
+      ...a,
+      [v.id]: {
+        ...v,
+        // since correct field always returns an array
+        // if there are multiple correct answers, we pass a string and render checkboxes
+        // if there's only one, we pass a string and render radio buttons
+        correct: !isEmpty(v.correct)
+          ? v.correct.length > 1
+            ? v.correct.map((ch: any) => ch)
+            : v.correct[0]
+          : null,
+      },
+    }
+  }, {})
+
+  return questionSet
+}
