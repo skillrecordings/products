594bd6d2d98933a1b12d080890885667cda91260
Joel Hooks <joelhooks@gmail.com>
2021-10-28 12:16:04 -0700

    build a smart bundle for checkout

--- apps/kcdbundle/src/utils/sessions.ts
+++ apps/kcdbundle/src/utils/sessions.ts
@@ -5,9 +5,6 @@ import {SellableResource} from '@skillrecordings/types'
 const CHECKOUT_SESSION_URL =
   process.env.NEXT_PUBLIC_STRIPE_CHECKOUT_SESSIONS_URL
 
-const EPIC_REACT_CLIENT_ID = `4118545974333dd5a03999d7b141ec809b9e83725630934e907e7205a9ac83cf`
-const TESTING_JAVASCRIPT_CLIENT_ID = `870edf7cfb5f6a3088fa8580452f90e111df42f7b46de9c969832bb8205ac38d`
-
 export async function createCheckoutSession(
   sellables: SellableResource[],
   site: string = 'kcdbundle',
@@ -19,8 +16,8 @@ export async function createCheckoutSession(
       CHECKOUT_SESSION_URL,
       pickBy({
         sellables,
-        // code: appliedCoupon,
-        client_id: TESTING_JAVASCRIPT_CLIENT_ID,
+        code: appliedCoupon,
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
         site,
         success_url:
           process.env.NEXT_PUBLIC_STRIPE_CHECKOUT_SESSIONS_SUCCESS_URL,
