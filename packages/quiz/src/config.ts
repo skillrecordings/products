0337609178b1a61f9dd469e350fdd3edccd0d2ac
Taylor Bell <taylorbell@gmail.com>
2021-10-28 17:33:55 -0600

    add new line

--- packages/quiz/src/config.ts
+++ packages/quiz/src/config.ts
@@ -15,14 +15,14 @@ export default function getConfig(
     afterCompletionMessages: {
       neutral: {
         default: `Thanks for submitting your answer!\n\nWe'll send the next lesson in 5-10 minutes. Check your inbox.\n\n_Thanks, ${AUTHOR}_`,
-        last: `Thanks for submitting your answer!\n\nThis was the last lesson from the ${PRODUCT_TITLE} email course. We hope you learned something new, and we look forward to sharing more in the future!\n\n_Thanks, ${AUTHOR}_`,
+        last: `Thanks for submitting your answer!\n\nThis was the last lesson from the ${PRODUCT_TITLE} email course. We hope you learned something new, and we look forward to sharing more in the future!\n\n_Thanks,\n${AUTHOR}_`,
       },
       correct: {
         default: `Nice work. You chose the correct answer!\n\nWe'll send the next lesson in 5-10 minutes. Check your inbox.\n\n_Thanks, ${AUTHOR}_`,
-        last: `Nice work. You chose the correct answer!\n\nThis was the last lesson from the ${PRODUCT_TITLE} email course. We hope you learned something new, and I look forward to sharing more in the future!\n\n_Thanks, ${AUTHOR}_`,
+        last: `Nice work. You chose the correct answer!\n\nThis was the last lesson from the ${PRODUCT_TITLE} email course. We hope you learned something new, and I look forward to sharing more in the future!\n\n_Thanks,\n${AUTHOR}_`,
       },
       incorrect: {
-        default: `You chose an incorrect answer, but don't worry. Just go back and re-read the email and check out any linked resources. You can refresh the page if you'd like to try again! 👍\n\nWe'll send the next email in 5-10 minutes too so you can learn more.\n\n_Thanks, ${AUTHOR}_`,
+        default: `You chose an incorrect answer, but don't worry. Just go back and re-read the email and check out any linked resources. You can refresh the page if you'd like to try again! 👍\n\nWe'll send the next email in 5-10 minutes too so you can learn more.\n\n_Thanks,\n${AUTHOR}_`,
         last: `You chose an incorrect answer, but don't worry. Just go back and re-read the email and check out any linked resources. You can refresh the page if you'd like to try again! 👍\n\nThis was the last lesson from the ${PRODUCT_TITLE} email course. We hope you learned something new, and I look forward to sharing more in the future!\n\n_Thanks, ${AUTHOR}_`,
       },
     },
