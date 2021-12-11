0b6f5ebfffd7e0685c7c612426b72e321b8f00b4
Joel Hooks <joelhooks@gmail.com>
2021-12-10 21:45:07 -0800

    bump turbo

--- apps/badass/src/pages/index.tsx
+++ apps/badass/src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Example() {
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              Build Your Course Platform and Ship Courses the Sell
+              Build Your Course Platform to Ship Courses the Sell
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Take ownership of your educational content and deliver
