bd42eb718fef7e2e721adef8220eb76c11029533
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-06 16:33:06 +0200

    clean up

--- packages/player/src/components/cue-bar.tsx
+++ packages/player/src/components/cue-bar.tsx
@@ -6,12 +6,7 @@ import {track} from '@skillrecordings/analytics'
 import CodeBlock from './code-block'
 import {useVideo} from '../context/video-context'
 import {useSelector} from '@xstate/react'
-import {
-  // selectActiveCues,
-  // selectCuesMuted,
-  selectDuration,
-  // selectVideo,
-} from '../selectors'
+import {selectDuration} from '../selectors'
 import {useCue} from '../hooks/use-cue'
 import {ElementType} from 'react'
 import ReactMarkdown from 'react-markdown'
@@ -44,48 +39,14 @@ export const CueBar: React.FC<any> = ({
   )
 }
 
-// const MutePopupButton: React.FC<any> = () => {
-//   const videoService = useVideo()
-//   const cuesMuted = useSelector(videoService, selectCuesMuted)
-
-//   return (
-//     <button
-//       className="cueplayer-react-cue-popup-mute-button"
-//       onClick={() => {
-//         track(cuesMuted ? 'unmuted cues' : 'muted cues')
-//         videoService.send('TOGGLE_MUTE_CUES')
-//       }}
-//     >
-//       {cuesMuted ? (
-//         <>
-//           <span>unmute notes</span>
-//           <IconVolumeOff />
-//         </>
-//       ) : (
-//         <>
-//           <span>mute notes</span>
-//           <IconVolumeOn />
-//         </>
-//       )}
-//     </button>
-//   )
-// }
-
 const NoteCue: React.FC<any> = ({cue, duration, className}) => {
   const [visible, setVisible] = React.useState(false)
-  // const [clickedOpen, setClickedOpen] = React.useState(false)
   const [mouseOverTippy, setMouseOverTippy] = React.useState(false)
   const videoService = useVideo()
-  // const video = useSelector(videoService, selectVideo)
-  // const activeCues = useSelector(videoService, selectActiveCues)
-
-  // const cuesMuted = useSelector(videoService, selectCuesMuted)
 
   useCue(cue)
 
   const clickOpen = () => {
-    // setVisible(true)
-    // setClickedOpen(true)
     // if we seek to the correct time, the note is displayed
     videoService.send({
       type: 'SEEKING',
@@ -97,24 +58,6 @@ const NoteCue: React.FC<any> = ({cue, duration, className}) => {
 
     track('opened cue', {cue: cue.text})
   }
-
-  // const clickClose = () => {
-  //   setClickedOpen(false)
-  //   setVisible(false)
-  // }
-
-  // const cueActive = activeCues.includes(cue)
-  // const seeking = video?.seeking
-  // const playerReadyEnough = (video?.readyState ?? 0) > 0
-
-  // React.useEffect(() => {
-  //   const isVisible = !cuesMuted && cueActive && !seeking && playerReadyEnough
-  //   if (!clickedOpen) {
-  //     setVisible(isVisible)
-  //   }
-  // }, [clickedOpen, cueActive, seeking, cuesMuted, playerReadyEnough])
-
-  // added seeking to the list here but getting some janky perf issues
 
   const startPosition = `${(cue.startTime / duration) * 100}%`
 
@@ -174,66 +117,3 @@ const NoteCue: React.FC<any> = ({cue, duration, className}) => {
     </Tippy>
   )
 }
-
-// const IconVolumeOff: React.FC<any> = ({className}) => {
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       stroke="currentColor"
-//       viewBox="0 0 24 24"
-//       className={`w-4 h-4 ${className ?? ''}`}
-//     >
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         strokeWidth="2"
-//         d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
-//         clipRule="evenodd"
-//       />
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         strokeWidth="2"
-//         d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"
-//       />
-//     </svg>
-//   )
-// }
-
-// const IconVolumeOn: React.FC<any> = ({className}) => {
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       fill="none"
-//       stroke="currentColor"
-//       viewBox="0 0 24 24"
-//       className={`w-4 h-4 ${className ?? ''}`}
-//     >
-//       <path
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//         strokeWidth="2"
-//         d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
-//       />
-//     </svg>
-//   )
-// }
-
-// const IconX: React.FC<any> = ({className}) => (
-//   <svg
-//     viewBox="0 0 24 24"
-//     xmlns="http://www.w3.org/2000/svg"
-//     className={`w-4 h-4 ${className ?? ''}`}
-//   >
-//     <g fill="none">
-//       <path
-//         d="M6 18L18 6M6 6l12 12"
-//         stroke="currentColor"
-//         strokeWidth="2"
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//       />
-//     </g>
-//   </svg>
-// )
