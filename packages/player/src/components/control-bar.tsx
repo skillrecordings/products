a6f83a307ac1eccf8ee0a8e7cb06d32ca78b9220
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-01-06 13:05:41 +0200

    alignment in control bar

--- packages/player/src/components/control-bar.tsx
+++ packages/player/src/components/control-bar.tsx
@@ -5,8 +5,6 @@ import {VolumeMenuButtonControl} from './controls/volume-menu-button-control'
 import {CurrentTimeDisplay} from './time-controls/current-time-display'
 import {TimeDivider} from './time-controls/time-divider'
 import {DurationDisplay} from './time-controls/duration-display'
-import {ProgressControl} from './controls/progress-control'
-import {RemainingTimeDisplay} from './time-controls/remaining-time-display'
 import {PlaybackRateMenuButtonControl} from './controls/playback-rate-menu-button-control'
 import {FullscreenToggleControl} from './controls/fullscreen-toggle-control'
 import {SidePanelToggleControl} from './controls/side-panel-toggle-control'
@@ -14,20 +12,22 @@ import {SidePanelToggleControl} from './controls/side-panel-toggle-control'
 export const ControlBar: React.FC = () => {
   return (
     <div className="cueplayer-react-control-bar">
-      <PlayToggleControl />
-      <ForwardReplayControl mode="replay" />
-      <ForwardReplayControl />
-      <VolumeMenuButtonControl />
-      <div className="cueplayer-player-control-bar-expander">
+      <div className="cueplayer-react-control-bar-left-part">
+        <PlayToggleControl />
+        <ForwardReplayControl mode="replay" />
+        <ForwardReplayControl />
+        <VolumeMenuButtonControl />
+      </div>
+      <div className="cueplayer-react-control-bar-center-part">
         <CurrentTimeDisplay />
         <TimeDivider />
         <DurationDisplay />
       </div>
-      {/* <ProgressControl /> */}
-      {/* <RemainingTimeDisplay /> */}
-      <PlaybackRateMenuButtonControl />
-      <SidePanelToggleControl />
-      <FullscreenToggleControl />
+      <div className="cueplayer-react-control-bar-right-part">
+        <PlaybackRateMenuButtonControl />
+        <SidePanelToggleControl />
+        <FullscreenToggleControl />
+      </div>
     </div>
   )
 }
