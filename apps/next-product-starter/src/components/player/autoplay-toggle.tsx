904b461042e6cbee2040eead184169041cfe9ed2
Vojta Holik <vojta@egghead.io>
2022-01-26 21:34:13 +0100

    be concistent in naming

--- apps/next-product-starter/src/components/player/autoplay-toggle.tsx
+++ apps/next-product-starter/src/components/player/autoplay-toggle.tsx
@@ -3,12 +3,12 @@ import {getPlayerPrefs, savePlayerPrefs} from '@skillrecordings/player'
 import Tippy from '@tippyjs/react'
 import classNames from 'classnames'
 
-const AutoplayToggle = () => {
-  const [autoplayState, setAutoplayState] = React.useState<boolean>(false)
+const AutoPlayToggle = () => {
+  const [isAutoPlayActive, setIsAutoPlayActive] = React.useState<boolean>(false)
   const {autoplay} = getPlayerPrefs()
 
   React.useEffect(() => {
-    setAutoplayState(autoplay)
+    setIsAutoPlayActive(autoplay)
   }, [])
 
   return (
@@ -26,32 +26,32 @@ const AutoplayToggle = () => {
       <button
         onClick={() => {
           savePlayerPrefs({autoplay: !autoplay})
-          setAutoplayState(!autoplayState)
+          setIsAutoPlayActive(!isAutoPlayActive)
         }}
         type="button"
         role="switch"
-        aria-checked={autoplayState}
+        aria-checked={isAutoPlayActive}
         className="px-3 py-2 flex items-center w-full group"
       >
         <span className="text-xs pr-2 font-mono uppercase">
           Autoplay{' '}
-          <span className="sr-only">is {autoplayState ? 'on' : 'off'}</span>
+          <span className="sr-only">is {isAutoPlayActive ? 'on' : 'off'}</span>
         </span>
         <div
           className={`${classNames({
-            'bg-indigo-500': autoplayState,
+            'bg-indigo-500': isAutoPlayActive,
             'bg-gray-200 dark:bg-gray-600 group-hover:bg-indigo-200 group-hover:bg-opacity-60 bg-opacity-100 transition':
-              !autoplayState,
+              !isAutoPlayActive,
           })} scale-90 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         >
           <span
             className={`${classNames({
-              'translate-x-0': !autoplayState,
-              'translate-x-5': autoplayState,
+              'translate-x-0': !isAutoPlayActive,
+              'translate-x-5': isAutoPlayActive,
             })} pointer-events-none inline-flex h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200 text-indigo-500 text-[0.5rem] tracking-tight font-mono uppercase font-bold items-center justify-center leading-none`}
             aria-hidden="true"
           >
-            {autoplayState ? (
+            {isAutoPlayActive ? (
               <>on</>
             ) : (
               <span className="group-hover:opacity-40 text-black opacity-0 transition">
@@ -66,4 +66,4 @@ const AutoplayToggle = () => {
   )
 }
 
-export {AutoplayToggle}
+export {AutoPlayToggle}
