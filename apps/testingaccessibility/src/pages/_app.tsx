5036c9bcba07729a5a027526f4927c92779fc2ab
Vojta Holik <vojta@egghead.io>
2022-03-04 15:02:53 +0100

    chore(testingaccessibility): update all packages

--- apps/testingaccessibility/src/pages/_app.tsx
+++ apps/testingaccessibility/src/pages/_app.tsx
@@ -26,7 +26,7 @@ function MyApp({Component, pageProps}: AppProps) {
       <DefaultSeo {...config} />
       <ConvertkitProvider>
         <ViewerProvider>
-          <ThemeProvider forcedTheme="dark" attribute="class">
+          <ThemeProvider forcedTheme="light" attribute="class">
             <MDXProvider components={MDXComponents}>
               <Component {...pageProps} />
             </MDXProvider>
