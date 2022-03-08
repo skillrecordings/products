6349dcd445a574292b579f1ca3187d2386c21962
Joel Hooks <joelhooks@gmail.com>
2022-03-07 18:55:09 -0800

    feat(testingaccessibility): add prisma database

--- apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
+++ apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -20,14 +25,24 @@ export default NextAuth({
         },
       },
     }),
+    EmailProvider({
+      server: {
+        service: 'Postmark',
+        auth: {
+          user: process.env.POSTMARK_KEY,
+          pass: process.env.POSTMARK_KEY,
+        },
+      },
+      from: 'team@testingaccessibility.com',
+    }),
   ],
   callbacks: {
-    async jwt({token, profile}) {
+    async jwt({token, profile, account, user}) {
       // this is only adding github but you could add anything here
       // and it's async, so you could load other stuff as needed as
       // well probably...
 
-      console.log({profile})
+      console.log({profile, user, account})
       token.profile = profile
       return token
     },
