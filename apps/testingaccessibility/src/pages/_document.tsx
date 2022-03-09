bc1748c8fdbdd1f9f057f5198c62f08b3bff70b7
Joel Hooks <joelhooks@gmail.com>
2022-03-09 12:04:00 -0800

    feat(testingaccessibility): add google analytics

--- apps/testingaccessibility/src/pages/_document.tsx
+++ apps/testingaccessibility/src/pages/_document.tsx
@@ -27,6 +27,22 @@ export default class MyDocument extends Document {
             sizes="any"
             color="#DA6E47"
           />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body className="text-black bg-white">
           <Main />
