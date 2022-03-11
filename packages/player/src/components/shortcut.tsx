0d8476d1586d1906902f6d129f5e6c7f5db20d5b
Vojta Holik <vojta@egghead.io>
2022-03-11 19:33:26 +0100

    fix(player): dont let shortcuts dominate page

--- packages/player/src/components/shortcut.tsx
+++ packages/player/src/components/shortcut.tsx
@@ -101,208 +101,207 @@ export const Shortcut: React.FC<ShortcutProps> = ({
   }, [paused, videoService])
 
   const mergeShortcuts = React.useCallback(() => {
-    const standardShortcuts = !shortcutsEnabled
-      ? []
-      : [
-          {
-            keyCode: 32, // spacebar
-            handle: () => togglePlay(),
-          },
-          {
-            keyCode: 75, // k
-            handle: () => togglePlay(),
-          },
-          {
-            keyCode: 70, // f
-            ctrl: false,
-            handle: () =>
-              videoService.send({type: 'TOGGLE_FULLSCREEN', element: rootElem}),
-          },
-          {
-            keyCode: 67, // c
-            ctrl: false,
-            handle: () => {
-              if (canAddNotes) {
-                // focus the cue note input
-                cueFormElem?.input?.focus()
-                // start note taking session
-                videoService.send({type: 'TAKE_NOTE', source: 'shortcut'})
-                // pause video playback
-                videoService.send({type: 'CHANGE'})
-              }
-            },
-          },
-          {
-            keyCode: 37, // Left arrow
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              const seekingTime = currentTime - 5
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: seekingTime > 0 ? seekingTime : 0,
-                source: 'shortcut',
-              })
-              videoService.send({type: 'END_SEEKING'})
-            },
-          },
-          {
-            keyCode: 74, // j
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              const seekingTime = currentTime - 10
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: seekingTime > 0 ? seekingTime : 0,
-                source: 'shortcut',
-              })
-              videoService.send({type: 'END_SEEKING'})
-            },
-          },
-          {
-            keyCode: 39, // Right arrow
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              const seekingTime = currentTime + 5
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: seekingTime < duration ? seekingTime : duration,
-                source: 'shortcut',
-              })
+    const standardShortcuts = [
+      {
+        keyCode: 32, // spacebar
+        handle: () => togglePlay(),
+      },
+      {
+        keyCode: 75, // k
+        handle: () => togglePlay(),
+      },
+      {
+        keyCode: 70, // f
+        ctrl: false,
+        handle: () =>
+          videoService.send({type: 'TOGGLE_FULLSCREEN', element: rootElem}),
+      },
+      {
+        keyCode: 67, // c
+        ctrl: false,
+        handle: () => {
+          if (canAddNotes) {
+            // focus the cue note input
+            cueFormElem?.input?.focus()
+            // start note taking session
+            videoService.send({type: 'TAKE_NOTE', source: 'shortcut'})
+            // pause video playback
+            videoService.send({type: 'CHANGE'})
+          }
+        },
+      },
+      {
+        keyCode: 37, // Left arrow
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          const seekingTime = currentTime - 5
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: seekingTime > 0 ? seekingTime : 0,
+            source: 'shortcut',
+          })
+          videoService.send({type: 'END_SEEKING'})
+        },
+      },
+      {
+        keyCode: 74, // j
+        ctrl: false,
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          const seekingTime = currentTime - 10
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: seekingTime > 0 ? seekingTime : 0,
+            source: 'shortcut',
+          })
+          videoService.send({type: 'END_SEEKING'})
+        },
+      },
+      {
+        keyCode: 39, // Right arrow
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          const seekingTime = currentTime + 5
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: seekingTime < duration ? seekingTime : duration,
+            source: 'shortcut',
+          })
 
-              videoService.send({type: 'END_SEEKING'})
-            },
-          },
-          {
-            keyCode: 76, // l
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              const seekingTime = currentTime + 10
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: seekingTime < duration ? seekingTime : duration,
-                source: 'shortcut',
-              })
-              videoService.send({type: 'END_SEEKING'})
-            },
-          },
-          {
-            keyCode: 36, // Home
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: 0,
-                source: 'shortcut',
-              })
-            },
-          },
-          {
-            keyCode: 35, // End
-            handle: () => {
-              if (!hasStarted) {
-                return
-              }
-              // Go to end of video
-              videoService.send({
-                type: 'SEEKING',
-                seekingTime: duration,
-                source: 'shortcut',
-              })
-            },
-          },
-          {
-            keyCode: 38, // Up arrow
-            handle: () => {
-              // Increase volume 5%
-              let v = volume + 0.05
-              if (v > 1) {
-                v = 1
-              }
-              videoService.send({
-                type: 'VOLUME_CHANGE',
-                volume: v < 1.0 ? v : 1.0,
-                source: 'shortcut',
-              })
-            },
-          },
-          {
-            keyCode: 40, // Down arrow
-            handle: () => {
-              // Decrease volume 5%
-              let v = volume - 0.05
-              if (v < 0) {
-                v = 0
-              }
-              //TODO difference between volume down and volume off actions
-              videoService.send({
-                type: 'VOLUME_CHANGE',
-                volume: v > 0 ? v : 0,
-                source: 'shortcut',
-              })
-            },
-          },
-          {
-            keyCode: 190, // Shift + >
-            shift: true,
-            handle: () => {
-              // Increase speed
-              let newPlaybackRate = playbackRate
-              if (playbackRate >= 1.5) {
-                newPlaybackRate = 2
-              } else if (playbackRate >= 1.25) {
-                newPlaybackRate = 1.5
-              } else if (playbackRate >= 1.0) {
-                newPlaybackRate = 1.25
-              } else if (playbackRate >= 0.5) {
-                newPlaybackRate = 1.0
-              } else if (playbackRate >= 0.25) {
-                newPlaybackRate = 0.5
-              } else if (playbackRate >= 0) {
-                newPlaybackRate = 0.25
-              }
+          videoService.send({type: 'END_SEEKING'})
+        },
+      },
+      {
+        keyCode: 76, // l
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          const seekingTime = currentTime + 10
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: seekingTime < duration ? seekingTime : duration,
+            source: 'shortcut',
+          })
+          videoService.send({type: 'END_SEEKING'})
+        },
+      },
+      {
+        keyCode: 36, // Home
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: 0,
+            source: 'shortcut',
+          })
+        },
+      },
+      {
+        keyCode: 35, // End
+        handle: () => {
+          if (!hasStarted) {
+            return
+          }
+          // Go to end of video
+          videoService.send({
+            type: 'SEEKING',
+            seekingTime: duration,
+            source: 'shortcut',
+          })
+        },
+      },
+      {
+        keyCode: 38, // Up arrow
+        handle: () => {
+          // Increase volume 5%
+          let v = volume + 0.05
+          if (v > 1) {
+            v = 1
+          }
+          videoService.send({
+            type: 'VOLUME_CHANGE',
+            volume: v < 1.0 ? v : 1.0,
+            source: 'shortcut',
+          })
+        },
+      },
+      {
+        keyCode: 40, // Down arrow
+        handle: () => {
+          // Decrease volume 5%
+          let v = volume - 0.05
+          if (v < 0) {
+            v = 0
+          }
+          //TODO difference between volume down and volume off actions
+          videoService.send({
+            type: 'VOLUME_CHANGE',
+            volume: v > 0 ? v : 0,
+            source: 'shortcut',
+          })
+        },
+      },
+      {
+        keyCode: 190, // Shift + >
+        shift: true,
+        handle: () => {
+          // Increase speed
+          let newPlaybackRate = playbackRate
+          if (playbackRate >= 1.5) {
+            newPlaybackRate = 2
+          } else if (playbackRate >= 1.25) {
+            newPlaybackRate = 1.5
+          } else if (playbackRate >= 1.0) {
+            newPlaybackRate = 1.25
+          } else if (playbackRate >= 0.5) {
+            newPlaybackRate = 1.0
+          } else if (playbackRate >= 0.25) {
+            newPlaybackRate = 0.5
+          } else if (playbackRate >= 0) {
+            newPlaybackRate = 0.25
+          }
 
-              videoService.send({
-                type: 'PLAYBACKRATE_CHANGE',
-                playbackRate: newPlaybackRate,
-                source: 'shortcut',
-              })
-            },
-          },
-          {
-            keyCode: 188, // Shift + <
-            shift: true,
-            handle: () => {
-              // Decrease speed
-              let newPlaybackRate = playbackRate
-              if (playbackRate <= 0.5) {
-                newPlaybackRate = 0.25
-              } else if (playbackRate <= 1.0) {
-                newPlaybackRate = 0.5
-              } else if (playbackRate <= 1.25) {
-                newPlaybackRate = 1.0
-              } else if (playbackRate <= 1.5) {
-                newPlaybackRate = 1.25
-              } else if (playbackRate <= 2) {
-                newPlaybackRate = 1.5
-              }
-              videoService.send({
-                type: 'PLAYBACKRATE_CHANGE',
-                playbackRate: newPlaybackRate,
-                source: 'shortcut',
-              })
-            },
-          },
-        ]
+          videoService.send({
+            type: 'PLAYBACKRATE_CHANGE',
+            playbackRate: newPlaybackRate,
+            source: 'shortcut',
+          })
+        },
+      },
+      {
+        keyCode: 188, // Shift + <
+        shift: true,
+        handle: () => {
+          // Decrease speed
+          let newPlaybackRate = playbackRate
+          if (playbackRate <= 0.5) {
+            newPlaybackRate = 0.25
+          } else if (playbackRate <= 1.0) {
+            newPlaybackRate = 0.5
+          } else if (playbackRate <= 1.25) {
+            newPlaybackRate = 1.0
+          } else if (playbackRate <= 1.5) {
+            newPlaybackRate = 1.25
+          } else if (playbackRate <= 2) {
+            newPlaybackRate = 1.5
+          }
+          videoService.send({
+            type: 'PLAYBACKRATE_CHANGE',
+            playbackRate: newPlaybackRate,
+            source: 'shortcut',
+          })
+        },
+      },
+    ]
 
     const getShortcutKey = ({
       keyCode = 0,
@@ -391,7 +390,12 @@ export const Shortcut: React.FC<ShortcutProps> = ({
         )
       })[0]
 
-      if (shortcut) {
+      if (
+        shortcut &&
+        shortcutsEnabled &&
+        (document.activeElement?.nodeName === 'BODY' ||
+          document.activeElement?.nodeName === 'VIDEO')
+      ) {
         shortcut.handle()
         e.preventDefault()
       }
