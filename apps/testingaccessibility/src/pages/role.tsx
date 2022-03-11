31c3fcde313cbd80702436f9a97d14c00d005c9e
Taylor Bell <taylorbell@gmail.com>
2022-03-11 13:00:37 -0700

    add role page

---
+++ apps/testingaccessibility/src/pages/role.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import {useConvertkit} from '@skillrecordings/convertkit'
+import {useRouter} from 'next/router'
+import isEmpty from 'lodash/isEmpty'
+import axios from 'axios'
+import Layout from 'components/app/layout'
+
+const JobRole = () => {
+  const router = useRouter()
+  const {subscriber} = useConvertkit()
+  const tagId = Number(router.query?.tag)
+
+  const message = (tagId: number) => {
+    switch (tagId) {
+      case 2982275:
+        return 'a developer'
+      case 2982283:
+        return 'in QA'
+      case 2982286:
+        return 'in a less-technical role'
+      default:
+        return 'interested in Testing Accessibility'
+    }
+  }
+
+  React.useEffect(() => {
+    if (tagId && !isEmpty(subscriber)) {
+      axios
+        .post('/api/convertkit/subscribe', {
+          ...subscriber,
+          tag: tagId,
+          email: subscriber?.email_address,
+        })
+        .catch((err) => console.log(err))
+    }
+  }, [tagId, subscriber])
+
+  return (
+    <Layout>
+      <div className="pt-8 pb-24 h-full flex flex-col items-center justify-center flex-grow">
+        <header className="text-center flex flex-col items-center justify-center">
+          <h1 className="flex items-center sm:text-4xl text-3xl font-semibold pb-5">
+            Thanks!
+          </h1>
+        </header>
+        <main className="prose dark:prose-dark sm:prose-lg max-w-lg mx-auto px-5 leading-tight text-center">
+          <p>Thanks for letting me know you're {message(tagId)}.</p>
+          <p>
+            Watch your inbox for more,
+            <br />
+            Marcy
+          </p>
+        </main>
+      </div>
+    </Layout>
+  )
+}
+
+export default JobRole
