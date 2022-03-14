e45632f9021c57721056849236179f86053dd22c
Joel Hooks <joelhooks@gmail.com>
2022-03-13 19:02:44 -0700

    feat(testingaccessibility): make the hasura adapter much clearer and use graphql-request

--- apps/testingaccessibility/src/utils/next-auth/hasura-adapter.ts
+++ apps/testingaccessibility/src/utils/next-auth/hasura-adapter.ts
@@ -1,82 +1,87 @@
-import {Adapter, VerificationToken} from 'next-auth/adapters'
-import {first, isEmpty} from 'lodash'
+import {Adapter} from 'next-auth/adapters'
+import {GraphQLClient, gql} from 'graphql-request'
 
-async function makeQuery(query: string) {
-  return await fetch(process.env.HASURA_GRAPHQL_ENDPOINT || '', {
-    method: 'POST',
-    body: query,
+export function HasuraAdapter(endpoint: string, adminSecret: string): Adapter {
+  const client = new GraphQLClient(endpoint, {
     headers: {
-      'content-type': 'application/json',
-      'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET || '',
+      'X-Hasura-Admin-Secret': adminSecret,
     },
   })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(JSON.stringify(response))
-      return response.data
-    })
-}
 
-export function HasuraAdapter(): Adapter {
   return {
     async createUser(data) {
-      console.log('createUser', {data})
-      const query = JSON.stringify({
-        query: `mutation CreateUser($data: users_insert_input!) {
-                  insert_users_one(object: $data) {
-                    email
-                    emailVerified
-                    id
-                    name
-                    image
-                  }
-                }
-          `,
-        variables: {data},
-      })
+      const query = gql`
+        mutation CreateUser($data: users_insert_input!) {
+          insert_users_one(object: $data) {
+            email
+            emailVerified
+            id
+            name
+            image
+          }
+        }
+      `
 
-      const {insert_users_one} = await makeQuery(query)
+      const {insert_users_one} = await client.request(query, {data})
 
       return insert_users_one
     },
     async getUser(id: string) {
-      console.log('getUser', {id})
-      const query = JSON.stringify({
-        query: `query loadUser($user: String) {
-              users(where: {id: {_eq: $user}}) {
-                id
-                name
-                email 
-                image
-              }
-            
-            }`,
-        variables: {user: id},
-      })
-      return await makeQuery(query)
+      const query = gql`
+        query loadUser($id: uuid!) {
+          users_by_pk(id: $id) {
+            id
+            name
+            email
+            image
+          }
+        }
+      `
+      const {users_by_pk: user} = await client.request(query, {id})
+
+      if (!user) {
+        return null
+      }
+
+      return user
     },
     async getUserByEmail(email) {
-      console.log('getUserByEmail', {email})
       const query = JSON.stringify({
-        query: `query loadUserByEmail($email: String) {
-                  users(where: {email: {_eq: $email}}) {
-                    id
-                    name
-                    email 
-                    image
-                  }
-                }`,
+        query: gql`
+          query loadUserByEmail($email: String) {
+            users(where: {email: {_eq: $email}}) {
+              id
+              name
+              email
+              image
+            }
+          }
+        `,
         variables: {email},
       })
-      const {users} = await makeQuery(query)
+      const {users} = await client.request(query, {email})
 
-      return isEmpty(users) ? null : first(users) ?? null
+      const user = users?.[0]
+
+      if (!user) {
+        return null
+      }
+
+      return user
     },
     async getUserByAccount({providerAccountId, provider}) {
       console.log('getUserByAccount', {providerAccountId, provider})
-      const queryText = `
-        query loadUserForAccount($providerAccountId: String, $provider:String ) {
-          accounts(where: {providerAccountId: {_eq: $providerAccountId}, provider: {_eq: $provider}}) {
+      const query = gql`
+        query loadUserForAccount(
+          $providerAccountId: String
+          $provider: String
+        ) {
+          accounts(
+            where: {
+              providerAccountId: {_eq: $providerAccountId}
+              provider: {_eq: $provider}
+            }
+          ) {
             user {
               image
               id
@@ -86,155 +91,146 @@ export function HasuraAdapter(): Adapter {
           }
         }
       `
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {providerAccountId, provider},
+
+      const {accounts} = await client.request(query, {
+        providerAccountId,
+        provider,
       })
 
-      const {accounts} = await makeQuery(query)
+      const account = accounts?.[0]
 
-      if (isEmpty(accounts)) {
+      if (!account || !account.user) {
         return null
       }
-
-      const account: any = first(accounts)
 
       return account.user
     },
     async updateUser(data) {
-      console.log('updateUser', {data})
-      const queryText = `mutation UpdateUser($id: uuid!, $data: users_set_input!) {
-                          update_users_by_pk(_set: $data, pk_columns: { id: $id }) {
-                            email
-                            emailVerified
-                            id
-                            image
-                            name
-                          }
-                        }
+      const query = gql`
+        mutation UpdateUser($id: uuid!, $data: users_set_input!) {
+          update_users_by_pk(_set: $data, pk_columns: {id: $id}) {
+            email
+            emailVerified
+            id
+            image
+            name
+          }
+        }
       `
 
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {
-          id: data.id,
-          data,
-        },
+      const {update_users_by_pk: user} = await client.request(query, {
+        id: data.id,
+        data,
       })
 
-      const {update_users_by_pk} = await makeQuery(query)
+      if (!user) {
+        return null
+      }
 
-      return update_users_by_pk
+      return user
     },
     async deleteUser(userId) {
-      console.log('deleteUser', {userId})
-      return
+      // not implemented
     },
     async linkAccount(data) {
-      console.log('linkAccount', {data})
-      const mutation = `mutation linkAccount($data: accounts_insert_input!) {
-            insert_accounts_one(object: $data) { 
-              id
-              access_token
-              expires_at
-              provider
-              providerAccountId
-              refresh_token
-              refresh_token_expires_in
-              scope
-              session_state
-              token_type
-              type
-              userId
-            }
+      const mutation = gql`
+        mutation linkAccount($data: accounts_insert_input!) {
+          insert_accounts_one(object: $data) {
+            id
+            access_token
+            expires_at
+            provider
+            providerAccountId
+            refresh_token
+            refresh_token_expires_in
+            scope
+            session_state
+            token_type
+            type
+            userId
           }
-          `
+        }
+      `
 
-      const query = JSON.stringify({
-        query: mutation,
-        variables: {data},
+      const {insert_accounts_one: account} = await client.request(mutation, {
+        data,
       })
 
-      const {insert_accounts_one: updatedAccount} = await makeQuery(query)
-
-      return updatedAccount
+      return account
     },
     async unlinkAccount({providerAccountId, provider}) {
-      console.log('unlinkAccount', {providerAccountId, provider})
-      return
+      //not implemented
     },
     async createSession(data) {
-      console.log('createSession', {data})
-      const mutation = `mutation ($data: sessions_insert_input!){
-                          insert_sessions_one(object: $data) {
-                            expires
-                            id
-                            sessionToken
-                            userId
-                          }
-                      `
+      const mutation = gql`
+        mutation ($data: sessions_insert_input!) {
+          insert_sessions_one(object: $data) {
+            expires
+            id
+            sessionToken
+            userId
+          }
+        }
+      `
 
-      const query = JSON.stringify({
-        query: mutation,
-        variables: {data},
+      const {insert_sessions_one: session} = await client.request(mutation, {
+        data,
       })
 
-      const {insert_sessions_one} = await makeQuery(query)
-
-      console.log({
-        insert_sessions_one,
-        expires: new Date(insert_sessions_one.expires * 1000),
-      })
+      if (!session) {
+        return null
+      }
 
       return {
-        ...insert_sessions_one,
-        expires: new Date(insert_sessions_one.expires * 1000),
+        ...session,
+        expires: new Date(session.expires * 1000),
       }
     },
     async getSessionAndUser(sessionToken) {
       console.log('getSessionAndUser', {sessionToken})
-      const queryText = `
-      query loadSessionAndUser($sessionToken: String) {
-        sessions(where: {sessionToken: {_eq: $sessionToken}}) {
-          sessionToken
-          id
-          expires
-          userId
-          user {
+      const query = gql`
+        query loadSessionAndUser($sessionToken: String) {
+          sessions(where: {sessionToken: {_eq: $sessionToken}}) {
+            sessionToken
             id
-            name
-            image
-            email
+            expires
+            userId
+            user {
+              id
+              name
+              image
+              email
+            }
           }
         }
-      }
       `
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {sessionToken},
-      })
 
-      const {sessions} = await makeQuery(query)
+      const {sessions} = await client.request(query, {sessionToken})
 
-      if (isEmpty(sessions)) {
+      if (!sessions?.[0]) {
         return null
       }
 
-      const {user, ...session}: any = first(sessions)
+      const {user, ...session}: any = sessions?.[0]
 
       return {
         user,
         session: {
           ...session,
-          expires: new Date(session.expires * 1000),
+          expires: new Date(session.expires),
         },
       }
     },
-    async updateSession({sessionToken, expires, userId}) {
-      console.log('updateSession', sessionToken)
-      const queryText = `
-        mutation updateSession($expires: Int, $sessionToken: String) {
-          update_sessions(where: {sessionToken: {_eq: $sessionToken}}, _set: {expires: $expires}) {
+    async updateSession(data) {
+      const query = gql`
+        mutation UpdateSession(
+          $sessionToken: String
+          $data: sessions_set_input!
+        ) {
+          update_sessions(
+            where: {sessionToken: {_eq: $sessionToken}}
+            _set: $data
+          ) {
             returning {
               expires
               id
@@ -244,94 +240,100 @@ export function HasuraAdapter(): Adapter {
           }
         }
       `
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {
-          sessionToken,
-          expires: Math.floor(Number(expires?.getTime()) / 1000),
-        },
+
+      const {update_sessions} = await client.request(query, {
+        sessionToken: data.sessionToken,
+        data,
       })
 
-      const {update_sessions} = await makeQuery(query)
+      const session = update_sessions?.returning?.[0]
 
-      return update_sessions.returning
+      if (!session) return null
+
+      return {...session, expires: new Date(session.expires)}
     },
     async deleteSession(sessionToken) {
       console.log('deleteSession', {sessionToken})
-      const queryText = `
-      mutation deleteSession($sessionToken: String) {
-        delete_sessions(where: {sessionToken: {_eq: $sessionToken}}) {
-          returning {
-            expires
-            id
-            sessionToken
-            userId
+      const query = gql`
+        mutation deleteSession($sessionToken: String) {
+          delete_sessions(where: {sessionToken: {_eq: $sessionToken}}) {
+            returning {
+              expires
+              id
+              sessionToken
+              userId
+            }
           }
         }
-      }
       `
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {
-          sessionToken,
-        },
-      })
-      const {delete_sessions} = await makeQuery(query)
 
-      return delete_sessions.returns
+      const {delete_sessions} = await client.request(query, {
+        sessionToken,
+      })
+
+      const session = delete_sessions?.returning?.[0]
+      if (!session) return null
+
+      return {...session, expires: new Date(session.expires)}
     },
     async createVerificationToken(data) {
       console.log('createVerificationToken', {data})
-      const queryText = `mutation CreateVerificationToken($data: verification_tokens_insert_input!) {
-                          insert_verification_tokens_one(object: $data) {
-                            expires
-                            identifier
-                            token
-                          }
-                        }`
-      const query = JSON.stringify({
-        query: queryText,
-        variables: {data},
-      })
-
-      const {insert_verification_tokens_one} = await makeQuery(query)
-
-      return {
-        ...insert_verification_tokens_one,
-        expires: new Date(insert_verification_tokens_one.expires),
-      }
-    },
-    async useVerificationToken({identifier, token}) {
-      try {
-        const queryText = `
-      mutation insertVerification($identifier: String, $token: String) {
-        delete_verification_tokens(where: {identifier: {_eq: $identifier}, token: {_eq: $token}}) {
-          returning {
+      const query = gql`
+        mutation CreateVerificationToken(
+          $data: verification_tokens_insert_input!
+        ) {
+          insert_verification_tokens_one(object: $data) {
+            expires
             identifier
             token
-            expires
           }
         }
-      }
       `
-        const query = JSON.stringify({
-          query: queryText,
-          variables: {
-            identifier,
-            token,
-          },
+
+      const {insert_verification_tokens_one: verificationToken} =
+        await client.request(query, {
+          data,
         })
 
-        const {delete_verification_tokens} = await makeQuery(query)
+      if (!verificationToken) {
+        return null
+      }
 
-        const vToken = first(
-          delete_verification_tokens.returning,
-        ) as VerificationToken
+      return {
+        ...verificationToken,
+        expires: new Date(verificationToken.expires),
+      }
+    },
+    async useVerificationToken(data) {
+      try {
+        const query = gql`
+          mutation insertVerification($identifier: String, $token: String) {
+            delete_verification_tokens(
+              where: {identifier: {_eq: $identifier}, token: {_eq: $token}}
+            ) {
+              returning {
+                identifier
+                token
+                expires
+              }
+            }
+          }
+        `
+
+        const {delete_verification_tokens: deletedTokens} =
+          await client.request(query, {
+            data,
+          })
+
+        const deletedToken = deletedTokens?.[0]
+
+        if (!deletedToken) {
+          return null
+        }
 
         return {
-          identifier: vToken.identifier,
-          token: vToken.token,
-          expires: new Date(vToken.expires),
+          ...deletedToken,
+          expires: new Date(deletedToken.expires),
         }
       } catch (e) {
         return null
