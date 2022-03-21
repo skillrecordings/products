22149b6581776e7bf2515ad0f3e52042cf8b0603
Joel Hooks <joelhooks@gmail.com>
2022-03-21 10:36:29 -0700

    feat: add Prisma using planetscale database

--- apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
+++ apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
@@ -2,24 +2,26 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import prisma from 'db/index'
 
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: 'jwt',
   },
-
   providers: [
-    // EmailProvider({
-    //   server: {
-    //     service: 'Postmark',
-    //     auth: {
-    //       user: process.env.POSTMARK_KEY,
-    //       pass: process.env.POSTMARK_KEY,
-    //     },
-    //   },
-    //   from: process.env.NEXT_PUBLIC_SUPPORT_EMAIL,
-    // }),
+    EmailProvider({
+      server: {
+        service: 'Postmark',
+        auth: {
+          user: process.env.POSTMARK_KEY,
+          pass: process.env.POSTMARK_KEY,
+        },
+      },
+      from: process.env.NEXT_PUBLIC_SUPPORT_EMAIL,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
