c0976552012aa61d818c8ac282bac5963ff0ec88
Vojta Holik <vojta@egghead.io>
2022-03-22 12:52:55 +0100

    feat(player): improved volume controls a11y (#193)
    * feat(player): improved volume control a11y
    
    * feat(player): improved volume control a11y
    
    * feat(player): improved volume control a11y

--- packages/player/src/components/volume-control/volume-bar.tsx
+++ packages/player/src/components/volume-control/volume-bar.tsx
@@ -1,32 +1,22 @@
 import * as React from 'react'
 import cx from 'classnames'
-import {Slider} from '../core/slider'
 import {useSelector} from '@xstate/react'
-import {SyntheticEvent} from 'react'
-import {VolumeLevel} from './volume-level'
 import {useVideo} from '../../context/video-context'
-import {getPointerPosition} from '../../utils'
 import {selectMuted, selectVolume} from '../../selectors'
+import {
+  SliderInput,
+  SliderTrack,
+  SliderRange,
+  SliderHandle,
+} from '@reach/slider'
 
 export const VolumeBar: React.FC<any> = (props) => {
-  const {className} = props
+  const {className, onMouseEnter, onMouseLeave, isActive} = props
   const videoService = useVideo()
   const volume = useSelector(videoService, selectVolume)
   const muted = useSelector(videoService, selectMuted)
 
-  const sliderRef = React.useRef(null)
-
-  const formattedVolume = (volume * 100).toFixed(2)
-
-  function calculateDistance(event: Event | SyntheticEvent) {
-    // forwarding refs made for a bit of a weird situation with
-    // the typing but there IS a ref here
-    const node = sliderRef?.current
-    const position = getPointerPosition(node, event)
-    return position.x
-  }
-
-  function getPercent() {
+  function getVolume() {
     if (muted) {
       return 0
     }
@@ -39,60 +29,41 @@ export const VolumeBar: React.FC<any> = (props) => {
     }
   }
 
-  function handleMouseMove(event: Event) {
-    checkMuted()
-    const distance = calculateDistance(event)
-    videoService.send({type: 'VOLUME_CHANGE', volume: distance})
-  }
-
-  function stepForward() {
-    checkMuted()
-    videoService.send({type: 'VOLUME_CHANGE', volume: volume + 0.1})
-  }
-
-  function stepBack() {
-    checkMuted()
-    videoService.send({type: 'VOLUME_CHANGE', volume: volume - 0.1})
-  }
-
-  function handleFocus(e: Event) {
-    if (props.onFocus) {
-      props.onFocus(e)
-    }
-  }
-
-  function handleBlur(e: Event) {
-    if (props.onBlur) {
-      props.onBlur(e)
-    }
-  }
-
-  function handleClick(event: Event) {
-    event.stopPropagation()
+  function formattedVolume(value: number) {
+    return (value * 100).toFixed(0) + `% ${muted ? 'volume muted' : 'volume'}`
   }
 
   return (
-    <Slider
-      ref={sliderRef}
-      label="volume level"
-      valuenow={formattedVolume}
-      valuetext={`${formattedVolume}%`}
-      onMouseMove={handleMouseMove}
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-      onClick={handleClick}
-      sliderActive={handleFocus}
-      sliderInactive={handleBlur}
-      getPercent={getPercent}
-      stepForward={stepForward}
-      stepBack={stepBack}
+    <SliderInput
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      min={0}
+      max={1}
+      value={getVolume()}
+      defaultValue={getVolume()}
+      step={0.05}
+      handleAlignment="contain"
+      getAriaValueText={(value) => formattedVolume(muted ? volume : value)}
+      onChange={(newValue) => {
+        checkMuted()
+        videoService.send({type: 'VOLUME_CHANGE', volume: newValue})
+      }}
+      className={cx(className, 'cueplayer-react-volume-bar')}
       {...props}
-      className={cx(
-        className,
-        'cueplayer-react-volume-bar cueplayer-react-slider-bar',
-      )}
     >
-      <VolumeLevel {...props} />
-    </Slider>
+      {({hasFocus}) => {
+        const isVisible = hasFocus || isActive
+        return (
+          <SliderTrack
+            className={cx('cueplayer-react-volume-slider', {
+              'cueplayer-react-volume-slider-active': isVisible,
+            })}
+          >
+            <SliderRange />
+            <SliderHandle />
+          </SliderTrack>
+        )
+      }}
+    </SliderInput>
   )
 }
