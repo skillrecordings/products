8206951205815accd521a26e2abce0219c049ffb
Joel Hooks <joelhooks@gmail.com>
2022-03-25 09:56:38 -0700

    feat: mock graphql api requests

--- apps/testingaccessibility/src/utils/format-prices-for-product.test.ts
+++ apps/testingaccessibility/src/utils/format-prices-for-product.test.ts
@@ -1,11 +1,46 @@
 import {graphql} from 'msw'
 import {getAdminSDK} from '../lib/api'
 import {setupServer} from 'msw/node'
+import {formatPricesForProduct} from './format-prices-for-product'
 
 process.env.HASURA_GRAPHQL_ENDPOINT = 'http://localhost:8080/v1/graphql'
 process.env.HASURA_ADMIN_SECRET = 'admin_secret'
 
 export const handlers = [
+  graphql.query('getCouponsForTypeAndDiscount', (req, res, ctx) => {
+    return res(
+      ctx.data({
+        coupons: [
+          {
+            id: 'test coupon',
+            type: req?.body?.variables.type,
+            percentage_discount: req?.body?.variables.percentage_discount,
+          },
+        ],
+      }),
+    )
+  }),
+  graphql.query('getMerchantCoupon', (req, res, ctx) => {
+    return res(
+      ctx.data({
+        merchant_coupons_by_pk: {
+          percentage_discount: 0.5,
+        },
+      }),
+    )
+  }),
+  graphql.query('getCouponForCode', (req, res, ctx) => {
+    return res(
+      ctx.data({
+        coupons: [
+          {
+            restricted_to_product_id: 'test',
+            percentage_discount: 0.5,
+          },
+        ],
+      }),
+    )
+  }),
   graphql.query('getProduct', (req, res, ctx) => {
     const {id} = req?.body?.variables
     return res(
@@ -38,8 +73,10 @@ afterAll(() => server.close())
 test('will return a mock result', async () => {
   const {getProduct} = getAdminSDK()
   const id = 'this is my id'
-  const {products_by_pk: product} = await getProduct({
-    id,
-  })
+
+  const product = await formatPricesForProduct({productId: id})
+  // const {products_by_pk: product} = await getProduct({
+  //   id,
+  // })
   expect(product?.id).toBe(id)
 })
