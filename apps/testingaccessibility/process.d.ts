898a50385aad23b4d46a46916b2d10c18c8027fb
Joel Hooks <joelhooks@gmail.com>
2022-03-25 11:22:04 -0700

    update: hasura cloud uses different naming for integration

--- apps/testingaccessibility/process.d.ts
+++ apps/testingaccessibility/process.d.ts
@@ -6,7 +6,7 @@ declare namespace NodeJS {
     GITHUB_SECRET: string
     GOOGLE_ID: string
     GOOGLE_SECRET: string
-    HASURA_GRAPHQL_ENDPOINT: string
+    HASURA_PROJECT_ENDPOINT: string
     HASURA_ADMIN_SECRET: string
     POSTMARK_KEY: string
     STRIPE_SECRET_TOKEN: string
--- apps/testingaccessibility/src/lib/api.ts
+++ apps/testingaccessibility/src/lib/api.ts
@@ -2,7 +2,7 @@ import {getSdk} from './generated-api'
 import {GraphQLClient} from 'graphql-request'
 
 export const getAdminSDK = () => {
-  const gqpClient = new GraphQLClient(process.env.HASURA_GRAPHQL_ENDPOINT, {
+  const gqpClient = new GraphQLClient(process.env.HASURA_PROJECT_ENDPOINT, {
     headers: {
       [`x-hasura-admin-secret`]: process.env.HASURA_ADMIN_SECRET,
     },
