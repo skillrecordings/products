7896264e37884702c3d15f782d6ba7e5f5a494a6
Joel Hooks <joelhooks@gmail.com>
2022-03-25 11:47:35 -0700

    update: bump

--- apps/testingaccessibility/src/utils/get-calculated-price.test.ts
+++ apps/testingaccessibility/src/utils/get-calculated-price.test.ts
@@ -1,6 +1,6 @@
 import {getCalculatedPriced} from './get-calculated-price'
 
-test('does the math right', async () => {
+test('does the math right!', async () => {
   const quantity = 5
   const discountMultiplier = 0.05
   const unitPrice = 100
