4f4cf2ffe215cb9f098230fd8ff7207e2dad1796
Joel Hooks <joelhooks@gmail.com>
2022-04-02 11:49:43 -0700

    fix: sentry auth token

--- apps/testingaccessibility/next.config.js
+++ apps/testingaccessibility/next.config.js
@@ -38,7 +38,7 @@ const sentryWebpackPluginOptions = {
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
-
+  authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
