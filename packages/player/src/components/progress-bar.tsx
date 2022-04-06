0391d92878b037d96d36ae9e0be1fc7ebc627c9e
Vojta Holik <vojta@egghead.io>
2022-04-06 13:55:40 +0200

    update(player): progress bar (#209)

--- packages/player/src/components/progress-bar.tsx
+++ packages/player/src/components/progress-bar.tsx
@@ -1,38 +1,16 @@
 import * as React from 'react'
-import {useSelector} from '@xstate/react'
 import cx from 'classnames'
 import {SeekBar} from './seek-bar'
-import {useVideo} from '../context/video-context'
-import {findElPosition, getPointerPosition} from '../utils'
-import {selectDuration} from '../selectors'
 
 export const ProgressBar: React.FC<any> = (props) => {
-  const videoService = useVideo()
-  const duration = useSelector(videoService, selectDuration)
-  const [mouseTime, setMouseTime] = React.useState({time: 0, position: 0})
-  const seekBar = React.useRef(null)
-
-  function handleMouseMove(event: React.MouseEvent) {
-    if (!event.pageX) {
-      return
-    }
-
-    const node = seekBar.current
-    const newTime = getPointerPosition(node, event).x * duration
-    const position = event.pageX - findElPosition(node).left
-
-    setMouseTime({
-      time: newTime,
-      position,
-    })
-  }
-
   return (
     <div
-      onMouseMove={handleMouseMove}
-      className={cx('cueplayer-react-progress-bar', props.className)}
+      className={cx(
+        'cueplayer-react-progress-control cueplayer-react-progress-bar',
+        props.className,
+      )}
     >
-      <SeekBar mouseTime={mouseTime} ref={seekBar} {...props} />
+      <SeekBar />
     </div>
   )
 }
