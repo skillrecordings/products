736937a7b621ab666cd22b66c2d18927a3f7f54e
Vojta Holik <vojta@egghead.io>
2022-04-20 20:29:52 +0200

    update(badass): twitter callout on confirmed page

--- apps/badass/src/pages/confirmed.tsx
+++ apps/badass/src/pages/confirmed.tsx
@@ -18,12 +18,34 @@ const Confirmed = () => {
           placeholder="blur"
           priority
         />
-        <motion.h1
-          className="font-heading text-3xl max-w-lg pt-8"
+        <motion.p
+          className="font-condensed sm:text-3xl text-2xl pt-8 pb-6 text-badass-pink-500"
           animate={{opacity: [0, 1], y: [-10, 0]}}
           transition={{type: 'spring'}}
         >
-          Thanks for signing up!
+          Success! Thank you for signing up
+        </motion.p>
+        <motion.h1
+          className="font-heading text-3xl max-w-xl"
+          animate={{opacity: [0, 1], y: [-10, 0]}}
+          transition={{type: 'spring', delay: 0.1}}
+        >
+          We're working on some badass stuff and occasionally share sneak peeks
+          on{' '}
+          <a
+            href="https://twitter.com/badass_courses"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="text-badass-green-400"
+          >
+            <span className="font-symbol" aria-hidden="true">
+              #
+            </span>
+            <span className="underline underline-offset-4 decoration-badass-green-400/60 hover:decoration-badass-green-400">
+              Twitter
+            </span>{' '}
+            <sup className="text-badass-gray text-lg">@badass_courses</sup>
+          </a>
         </motion.h1>
       </div>
     </Layout>
