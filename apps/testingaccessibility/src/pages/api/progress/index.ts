919b184c898db9c8b0b6da7a882befb66c576597
Joel Hooks <joelhooks@gmail.com>
2022-04-22 15:59:48 -0700

    update: use prisma instead of supabase

--- apps/testingaccessibility/src/pages/api/progress/index.ts
+++ apps/testingaccessibility/src/pages/api/progress/index.ts
@@ -1,30 +1,33 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {getDecodedToken} from 'utils/get-decoded-token'
-import {progressTable, supabase} from 'utils/progress'
+import prisma from '../../../db'
+import {serialize} from '../../../utils/prisma-next-serializer'
+import {withSentry} from '@sentry/nextjs'
+import {setupHttpTracing} from '@vercel/tracing-js'
+import {tracer} from '../../../utils/honeycomb-tracer'
 
 const getProgress = async (req: NextApiRequest, res: NextApiResponse) => {
+  setupHttpTracing({
+    name: getProgress.name,
+    tracer,
+    req,
+    res,
+  })
   const sessionToken = await getDecodedToken(req)
 
   if (req.method === 'GET') {
     try {
       if (!sessionToken || !sessionToken.sub) {
-        return res.status(401).end('Not Authorized')
-      }
-      if (!supabase) {
-        throw new Error('Unable to set progress.')
-      }
-      const {data = [], error} = await supabase
-        .from(progressTable)
-        .select('completed')
-        .eq('user_id', sessionToken.sub)
-
-      if (error) {
-        console.error(error)
-        throw new Error('Data not loaded')
+        return res.status(403).end('Not Authorized')
       }
 
-      const progress = data
-      res.status(200).json(progress)
+      const userLessonProgress = await prisma.lessonProgress.findMany({
+        where: {
+          userId: sessionToken.sub,
+        },
+      })
+
+      res.status(200).json(userLessonProgress.map(serialize))
     } catch (error: any) {
       console.error(error.message)
       res.status(400).end(error.message)
@@ -32,4 +35,10 @@ const getProgress = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 }
 
-export default getProgress
+export default withSentry(getProgress)
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
