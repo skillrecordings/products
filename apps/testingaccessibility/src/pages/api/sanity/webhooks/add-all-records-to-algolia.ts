79aadd471f99efaf3152854233567d57d28083c0
Joel Hooks <joelhooks@gmail.com>
2022-05-01 11:11:53 -0500

    tweak(testingaccessibility): use try catch for async/await
    goal here is primarily to get the error messaging in the console so we can get more insight into what
    might be going wrong with the data

--- apps/testingaccessibility/src/pages/api/sanity/webhooks/add-all-records-to-algolia.ts
+++ apps/testingaccessibility/src/pages/api/sanity/webhooks/add-all-records-to-algolia.ts
@@ -4,9 +4,9 @@ import {sanityAlgolia} from 'utils/algolia'
 import {setupHttpTracing} from '@vercel/tracing-js'
 import {withSentry} from '@sentry/nextjs'
 import {tracer} from 'utils/honeycomb-tracer'
-import get from 'lodash/get'
+import {isValidRequest} from '@sanity/webhook'
 
-const secret = process.env.NEXT_PUBLIC_ALGOLIA_API_WRITE_KEY as string
+const secret = process.env.NEXT_PUBLIC_ALGOLIA_API_WRITE_KEY
 
 export const addAllRecordsToAlgolia = async (
   req: NextApiRequest,
@@ -18,30 +18,27 @@ export const addAllRecordsToAlgolia = async (
     req,
     res,
   })
-  // Basic security to prevent others from hitting this API
-  const s = get(req.query, 's')
-  if (s !== secret) {
-    res.status(403).json({success: false, message: 'Invalid signature'})
-    return
-  }
 
-  // Fetch the _id of all the documents we want to index
-  const types = ['section', 'lesson']
-  const query = `* [_type in $types][]._id`
+  try {
+    if (!isValidRequest(req, secret)) {
+      res.status(403).json({success: false, message: 'Invalid signature'})
+    } else {
+      // Fetch the _id of all the documents we want to index
+      const types = ['section', 'lesson']
+      const query = `* [_type in $types][]._id`
 
-  await sanityClient
-    .fetch(query, {types})
-    .then((ids) => {
-      sanityAlgolia.webhookSync(sanityClient, {
-        ids: {created: ids, updated: [], deleted: []},
+      await sanityClient.fetch(query, {types}).then((ids) => {
+        sanityAlgolia.webhookSync(sanityClient as any, {
+          ids: {created: ids, updated: [], deleted: []},
+        })
       })
-    })
-    .then(() => {
+
       res.status(200).send('Success!')
-    })
-    .catch((error) => {
-      res.status(500).json({success: false, message: error})
-    })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({success: false, message: error.message})
+  }
 }
 
 export default withSentry(addAllRecordsToAlgolia)
