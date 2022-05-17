0371a0f139a4ecb5a302dac2a38b8f2c727b21f5
Joel Hooks <joelhooks@gmail.com>
2022-05-16 22:07:29 -0600

    fix: delete generated files

--- packages/skill-api/core/pages/test.d.ts
+++
@@ -1,6 +0,0 @@
-import {Theme} from '../types'
-export declare type TestErrorTypes = 'default'
-export interface TestServerPageParams {
-  theme: Theme
-}
-export default function TestPage(props: TestServerPageParams): JSX.Element
--- packages/skill-api/core/services/send-feedback-from-user.d.ts
+++
@@ -1,8 +0,0 @@
-import {SendFeedbackFromUserOptions} from '../types'
-import {OutgoingResponse} from '../index'
-export declare function sendFeedbackFromUser({
-  userId,
-  feedbackText,
-  context,
-  prisma,
-}: SendFeedbackFromUserOptions): Promise<OutgoingResponse>
