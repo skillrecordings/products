b122001e36c65d8fdfdc7721fcc76b5bae2cfc84
Vojta Holik <vojta@egghead.io>
2022-05-31 19:49:44 +0200

    fix(team): dynamic code eval in middleware

--- apps/testingaccessibility/src/pages/team/_middleware.ts
+++ apps/testingaccessibility/src/pages/team/_middleware.ts
@@ -1,16 +1,16 @@
-import {getCurrentAbility} from 'server/ability'
 import type {NextFetchEvent} from 'next/server'
 import {NextResponse} from 'next/server'
 import {getToken} from 'next-auth/jwt'
 import {NextApiRequest} from 'next'
+import {hasBulkPurchase} from 'utils/purchase-validators'
 
 export async function middleware(req: NextApiRequest, ev: NextFetchEvent) {
   const session: any = await getToken({req})
   if (!session)
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`)
 
-  const ability = getCurrentAbility(session)
-  if (!ability.can('invite', 'Team'))
+  const canViewTeam = hasBulkPurchase(session.purchases)
+  if (!canViewTeam)
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/learn`)
 
   // If user is authenticated and can invite team, continue.
