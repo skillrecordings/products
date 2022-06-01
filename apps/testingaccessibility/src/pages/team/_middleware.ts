a21f3340919f2ffdfbe145c1cfe95b27a9243f38
Vojta Holik <vojta@egghead.io>
2022-06-01 09:40:07 +0200

    fix(team): ordinary js check for bulk purchase

--- apps/testingaccessibility/src/pages/team/_middleware.ts
+++ apps/testingaccessibility/src/pages/team/_middleware.ts
@@ -2,13 +2,20 @@ import type {NextFetchEvent} from 'next/server'
 import {NextResponse} from 'next/server'
 import {getToken} from 'next-auth/jwt'
 import {NextApiRequest} from 'next'
-import {hasBulkPurchase} from 'utils/purchase-validators'
 
 export async function middleware(req: NextApiRequest, ev: NextFetchEvent) {
   const session: any = await getToken({req})
+
   if (!session)
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`)
 
-  // If user is authenticated and can invite team, continue.
+  const hasBulkPurchase = session.purchases.some((purchase: any) => {
+    return purchase.bulkCoupon !== null
+  })
+
+  if (!hasBulkPurchase)
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/learn`)
+
+  // If user is authenticated and has bulk purchase, continue.
   return NextResponse.next()
 }
