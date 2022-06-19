b54e2285f81e969dbed8e37e27ce4c196d35d6b3
Vojta Holik <vojta@egghead.io>
2022-06-19 12:10:43 +0200

    update(testingaccessibility): style login email
    - use mjml2html
    - add logo and brand colors
    - force light color scheme fixing contrast issues

--- apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
+++ apps/testingaccessibility/src/pages/api/auth/[...nextauth].ts
@@ -8,6 +8,7 @@ import {createTransport} from 'nodemailer'
 import {withSentry} from '@sentry/nextjs'
 import {getSdk} from '../../../lib/prisma-api'
 import {defineRulesForPurchases} from '../../../server/ability'
+import mjml2html from 'mjml'
 
 export const nextAuthOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -109,48 +110,51 @@ function html({url, host, email}: Record<'url' | 'host' | 'email', string>) {
   const escapedHost = `${host.replace(/\./g, '&#8203;.')}`
 
   // Some simple styling options
-  const backgroundColor = '#f9f9f9'
-  const textColor = '#444444'
+  const backgroundColor = '#F9FAFB'
+  const textColor = '#3E3A38'
   const mainBackgroundColor = '#ffffff'
-  const buttonBackgroundColor = '#346df1'
-  const buttonBorderColor = '#346df1'
+  const buttonBackgroundColor = '#218345'
   const buttonTextColor = '#ffffff'
 
-  return `
-<body style="background: ${backgroundColor};">
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
-        <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-  </table>
-  <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
-        Sign in as <strong>${escapedEmail}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`
+  const {html} = mjml2html(`
+<mjml>
+  <mj-head>
+    <mj-font name="Inter" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600" />
+    <mj-attributes>
+      <mj-all font-family="Inter, Helvetica, sans-serif" line-height="1.5" />
+    </mj-attributes>
+    <mj-raw>
+      <meta name="color-scheme" content="light" />
+      <meta name="supported-color-schemes" content="light" />
+    </mj-raw>
+  </mj-head>
+  <mj-body background-color="${backgroundColor}">
+    <mj-section padding="10px">
+      <mj-column background-color="${backgroundColor}">
+        <mj-image width="180px" src="https://res.cloudinary.com/testing-accessibility/image/upload/v1655584147/logo-email_2x_e0n8tn.png" />
+      </mj-column>
+    </mj-section>
+    <mj-section padding-top="0">
+      <mj-column background-color="${mainBackgroundColor}" padding="16px 10px">
+        <mj-text font-size="18px" color="${textColor}" align="center" padding-bottom="20px">
+          Log in as <strong color="${textColor}">${escapedEmail}</strong> to Testing Accessibility.
+        </mj-text>
+        <mj-button href="${url}" background-color="${buttonBackgroundColor}" color="${buttonTextColor}" target="_blank" border-radius="6px" font-size="18px" font-weight="bold">
+          Log in
+        </mj-button>
+        <mj-text color="gray" align="center" padding-top="20px">
+          If you did not request this email you can safely ignore it.
+        </mj-text>
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+`)
+
+  return html
 }
 
 // Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
 function text({url, host}: Record<'url' | 'host', string>) {
-  return `Sign in to ${host}\n${url}\n\n`
+  return `Log in to ${host}\n${url}\n\n`
 }
