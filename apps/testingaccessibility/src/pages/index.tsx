43a97f4470a47dc321029ce6b5c2e4476061bff3
Vojta Holik <vojta@egghead.io>
2022-07-06 13:03:50 +0200

    update(pricing): sale state design + PPP box

--- apps/testingaccessibility/src/pages/index.tsx
+++ apps/testingaccessibility/src/pages/index.tsx
@@ -67,7 +67,7 @@ const Home: React.FC<CommerceProps> = ({
                 <div className="pb-80 pt-24 text-white">
                   <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
                     <div className="max-w-3xl mx-auto space-y-4 lg:max-w-none">
-                      <h2 className="text-3xl  font-extrabold sm:text-4xl lg:text-5xl">
+                      <h2 className="text-3xl font-heading font-bold sm:text-4xl lg:text-5xl">
                         Pricing
                       </h2>
                       <p className="text-xl max-w-lg mx-auto">
