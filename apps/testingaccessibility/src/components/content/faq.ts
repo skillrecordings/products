f5546d84a757a47e26e39ae809d33bee37509af2
Taylor Bell <taylorbell@gmail.com>
2022-07-07 15:44:02 -0600

    update TA faq

--- apps/testingaccessibility/src/components/content/faq.ts
+++ apps/testingaccessibility/src/components/content/faq.ts
@@ -4,21 +4,35 @@ export const faq = [
     answer: `I guarantee it will, but if it doesn’t, send an email to [team@testingaccessibility.com](mailto:team@testingaccessibility.com) within 30 days for a full refund.`,
   },
   {
-    question: `Something has gone horribly wrong, how can I get help?`,
-    answer: `Oh no! Please send an email to [team@testingaccessibility.com](mailto:team@testingaccessibility.com) and we'll help you as soon as possible.`,
+    question: `How do “seats” work for team purchases?`,
+    answer: `When purchasing team seats, you will receive a link that can be used to register one account for every seat you buy. The license is non-transferable (you cannot reassign a licensed seat from one person to another).`,
   },
   {
-    question: `Can I buy a lower tier and upgrade later?`,
-    answer: `Yes you can! Once you purchase the workshops and login, you can come back to this page which will allow you to update. (You only pay the difference between your tier and the tier you upgrade to). Note that if you purchased a tier with a regional discount, this regional discount will not be applied to your upgrade.`,
+    question: `Does my company own the team licenses?`,
+    answer: `The team licenses are actually single use coupon codes that are distributed to users individually. Companies are not meant to own or keep the licenses. They belong to the individual who claims the coupon code, and cannot be passed around or transferred.`,
   },
   {
-    question: `What frameworks do you use in the workshops?`,
-    answer: `
-The Manual Testing workshop doesn’t use any frameworks at all when remediating issues— strictly vanilla HTML, CSS, and JS.
-
-The other development-specific workshops do use some basic React in the demos, but the lessons and tools you learn will be applicable to other frameworks as well. Think of React as an implementation detail.
-    
-The material in the Foundations and Design & People Skills workshops have lessons that talk about code, but there’s not much writing code.
-`,
+    question: `Can I customize the invoice with VAT or other company details?`,
+    answer: `Yes, there is a text area for you to add any arbitrary information into a printable PDF.`,
+  },
+  {
+    question: `Do you support Purchasing Power Parity (PPP) / regional licenses?`,
+    answer: `Yes! This is automated based on the IP address of the computer you are using to make the purchase. This system isn’t perfect, so the discount does not always display. Note that purchases made with a PPP discount will be restricted to the country they were purchased from and future content and additional materials may not be available.`,
+  },
+  {
+    question: `Can I give a license as a gift?`,
+    answer: `Yes! Just enter their email address instead of your own at time of purchase. They may receive a verification code that you would have to request from them. If you are purchasing with a PPP discount, you must both be in the same country or the person receiving the gift will not be able to access the content.`,
+  },
+  {
+    question: `If I buy a lower tier, can I upgrade later?`,
+    answer: `Yes! Unless there’s still a discount on the highest tier, you can upgrade by paying the difference at the time you upgrade.`,
+  },
+  {
+    question: `Can I change my email address from the one I bought the course with?`,
+    answer: `Yes, by contacting the support team at team@testingaccessibility.com. You cannot transfer your license to another person.`,
+  },
+  {
+    question: `I have a question not covered here. Can I contact someone?`,
+    answer: `Yes! You can email the support team at [team@testingaccessibility.com](mailto:team@testingaccessibility.com).`,
   },
 ]
