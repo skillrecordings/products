077c68580b23560eda6578d9c89c5528971165c5
Vojta Holik <vojta@egghead.io>
2022-07-07 20:02:31 +0200

    update(testingaccessibility): landing page (#297)
    * update(testingaccessibility): landing page
    
    * feat: guarantee seal + faq section
    
    * fix: add key
    
    * fix: add key

---
+++ apps/testingaccessibility/src/components/content/faq.ts
@@ -0,0 +1,24 @@
+export const faq = [
+  {
+    question: `What if this material doesn’t suit my needs?`,
+    answer: `I guarantee it will, but if it doesn’t, send an email to [team@testingaccessibility.com](mailto:team@testingaccessibility.com) within 30 days for a full refund.`,
+  },
+  {
+    question: `Something has gone horribly wrong, how can I get help?`,
+    answer: `Oh no! Please send an email to [team@testingaccessibility.com](mailto:team@testingaccessibility.com) and we'll help you as soon as possible.`,
+  },
+  {
+    question: `Can I buy a lower tier and upgrade later?`,
+    answer: `Yes you can! Once you purchase the workshops and login, you can come back to this page which will allow you to update. (You only pay the difference between your tier and the tier you upgrade to). Note that if you purchased a tier with a regional discount, this regional discount will not be applied to your upgrade.`,
+  },
+  {
+    question: `What frameworks do you use in the workshops?`,
+    answer: `
+The Manual Testing workshop doesn’t use any frameworks at all when remediating issues— strictly vanilla HTML, CSS, and JS.
+
+The other development-specific workshops do use some basic React in the demos, but the lessons and tools you learn will be applicable to other frameworks as well. Think of React as an implementation detail.
+    
+The material in the Foundations and Design & People Skills workshops have lessons that talk about code, but there’s not much writing code.
+`,
+  },
+]
