1739cc5eb867376192c330a9b07d30ab267e9184
Lauro <hey@laurosilva.com>
2022-07-08 00:13:06 -0700

    adding Taxonomy Manager

--- apps/typescriptcourse/studio/deskStructure.js
+++ apps/typescriptcourse/studio/deskStructure.js
@@ -2,9 +2,17 @@ import S from '@sanity/desk-tool/structure-builder'
 import articles from './src/structure/articles'
 
 const hiddenDocTypes = (listItem) =>
-  !['tag', 'article'].includes(listItem.getId())
+  !['tag', 'article', 'skosConcept', 'skosConceptScheme'].includes(
+    listItem.getId(),
+  )
 
 export default () =>
   S.list()
     .title('TypeScript Course')
-    .items([...S.documentTypeListItems().filter(hiddenDocTypes), articles])
+    .items([
+      ...S.documentTypeListItems().filter(hiddenDocTypes),
+      articles,
+      S.divider(),
+      S.documentTypeListItem('skosConcept').title('Concepts'),
+      S.documentTypeListItem('skosConceptScheme').title('Taxonomy Schemes'),
+    ])
