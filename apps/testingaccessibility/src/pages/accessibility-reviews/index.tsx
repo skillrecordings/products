ba0b315adb589a98faa5422d71ada750e46593fd
Marcy Sutton <holla@marcysutton.com>
2022-07-09 12:49:47 -0700

    Wrap global nav in header, adjust other landmarks

--- apps/testingaccessibility/src/pages/accessibility-reviews/index.tsx
+++ apps/testingaccessibility/src/pages/accessibility-reviews/index.tsx
@@ -16,55 +16,57 @@ const meta = {
 const Reviews: React.FC<any> = ({reviews}) => {
   return (
     <Layout meta={meta}>
-      <header className="relative px-5 py-28 overflow-hidden text-white bg-green-700 bg-noise">
-        <h1 className="max-w-screen-md font-heading font-bold mx-auto leading-none text-center text-3xl sm:text-4xl lg:text-5xl">
-          Accessibility Reviews
-        </h1>
-      </header>
-      <main className="bg-gray-50 px-5 flex-grow">
-        <div className="pb-16 mx-auto max-w-lg w-full pt-16 gap-16">
-          <div className="grid grid-cols-1 gap-5">
-            {reviews.map(({title, description, slug, image, date}: any) => {
-              return (
-                <div
-                  key={slug}
-                  className="flex sm:flex-row flex-col sm:text-left text-center items-center gap-5"
-                >
-                  <div className="flex-shrink-0">
-                    <Image
-                      src={image}
-                      alt={title}
-                      width={60}
-                      height={60}
-                      className="rounded-md"
-                    />
-                  </div>
-                  <div className="flex w-full sm:justify-between justify-center">
-                    <div>
+      <main>
+        <header className="relative px-5 py-28 overflow-hidden text-white bg-green-700 bg-noise">
+          <h1 className="max-w-screen-md font-heading font-bold mx-auto leading-none text-center text-3xl sm:text-4xl lg:text-5xl">
+            Accessibility Reviews
+          </h1>
+        </header>
+        <div className="bg-gray-50 px-5 flex-grow">
+          <div className="pb-16 mx-auto max-w-lg w-full pt-16 gap-16">
+            <div className="grid grid-cols-1 gap-5">
+              {reviews.map(({title, description, slug, image, date}: any) => {
+                return (
+                  <div
+                    key={slug}
+                    className="flex sm:flex-row flex-col sm:text-left text-center items-center gap-5"
+                  >
+                    <div className="flex-shrink-0">
+                      <Image
+                        src={image}
+                        alt={title}
+                        width={60}
+                        height={60}
+                        className="rounded-md"
+                      />
+                    </div>
+                    <div className="flex w-full sm:justify-between justify-center">
+                      <div>
+                        <Link href={`/accessibility-reviews/${slug}`} passHref>
+                          <a className="text-green-600 group-hover:underline sm:text-3xl font-heading text-xl font-bold">
+                            {title.replace('Accessibility Review of ', '')}
+                          </a>
+                        </Link>
+                        <time dateTime={date} className="block font-medium">
+                          {format(new Date(date), 'MMMM d, yyyy')}
+                        </time>
+                        <p>{description}</p>
+                      </div>
+                    </div>
+                    {slug && (
                       <Link href={`/accessibility-reviews/${slug}`} passHref>
-                        <a className="text-green-600 group-hover:underline sm:text-3xl font-heading text-xl font-bold">
-                          {title.replace('Accessibility Review of ', '')}
+                        <a className="flex-shrink-0 px-3 py-2 rounded-md hover:bg-moss-200/50 transition bg-moss-100 text-green-600 font-semibold inline-flex">
+                          Watch the review
+                          <i aria-hidden className="pl-2">
+                            →
+                          </i>
                         </a>
                       </Link>
-                      <time dateTime={date} className="block font-medium">
-                        {format(new Date(date), 'MMMM d, yyyy')}
-                      </time>
-                      <p>{description}</p>
-                    </div>
+                    )}
                   </div>
-                  {slug && (
-                    <Link href={`/accessibility-reviews/${slug}`} passHref>
-                      <a className="flex-shrink-0 px-3 py-2 rounded-md hover:bg-moss-200/50 transition bg-moss-100 text-green-600 font-semibold inline-flex">
-                        Watch the review
-                        <i aria-hidden className="pl-2">
-                          →
-                        </i>
-                      </a>
-                    </Link>
-                  )}
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
         </div>
       </main>
--- apps/testingaccessibility/src/pages/learn/index.tsx
+++ apps/testingaccessibility/src/pages/learn/index.tsx
@@ -102,145 +102,147 @@ const Learn: React.FC<{purchases: Purchase[]; product: SanityDocument}> = ({
       className="bg-green-700 bg-noise"
       meta={{title: 'Testing Accessibility Workshops'}}
     >
-      <header className="w-full bg-green-800/40">
-        <div className="mx-auto py-[2px] w-full max-w-screen-lg flex">
-          <Search product={product} />
-        </div>
-      </header>
-      <main className="max-w-screen-md mx-auto w-full lg:p-1 p-5 lg:pt-10 pt-10 lg:pb-32 pb-16">
-        <header className="pt-10 pb-24 text-white flex flex-col items-center justify-center">
-          <h1 className="font-heading text-5xl font-bold text-center max-w-lg">
-            Testing Accessibility Workshops
-          </h1>
-          <Link
-            href={
-              nextUpLesson
-                ? {
-                    pathname: '/learn/[module]/[section]/[lesson]',
-                    query: getPathForLesson(nextUpLesson.slug, product.modules),
-                  }
-                : {
-                    pathname: '/learn/[module]',
-                    query: {module: 'foundations-of-accessibility'},
-                  }
-            }
-          >
-            <a className="flex items-center font-medium gap-2 mt-8 px-4 py-2.5 rounded-md border border-white/20 hover:bg-white/5 transition focus-visible:ring-white">
-              {isEmpty(progress) ? 'Start' : 'Continue'} Learning{' '}
-              <ChevronRightIcon aria-hidden="true" className="w-4 h-4 mt-0.5" />
-            </a>
-          </Link>
-        </header>
-        {/* <h1 className="font-nav text-center text-2xl leading-none font-bold text-white py-10">
-          {title}
-        </h1> */}
-        <div className="grid grid-cols-1 gap-16 w-full">
-          {modules.map((module: SanityDocument, i: number) => {
-            const {title, slug, image, sections} = module
-            const {completedSections, isCompleted} = getModuleProgressForUser(
-              progress,
-              sections,
-            )
-            return (
-              <ol key={slug} className="text-white">
-                <li className="flex md:flex-row flex-col md:items-start items-center justify-center">
-                  <div
-                    className={cx(
-                      'flex items-center justify-center flex-shrink-0 hover:rotate-0 transition ease-in-out duration-300',
-                      {'md:-rotate-2': i % 2 === 0},
-                      {'md:rotate-2': i % 2 === 1},
-                    )}
-                  >
-                    <Image
-                      className="drop-shadow-xl"
-                      src={image.url}
-                      alt={image.alt}
-                      width={350}
-                      height={350}
-                      quality={100}
-                    />
-                  </div>
-                  <div className="w-full md:pt-16">
-                    <Link
-                      href={{
-                        pathname: '/learn/[module]',
-                        query: {module: slug},
-                      }}
-                      passHref
-                    >
-                      <a className="hover:underline sm:text-4xl text-3xl leading-tight font-bold mt-2 inline-flex justify-center w-full font-heading md:text-left text-center focus-visible:ring-white">
-                        <h2>{title}</h2>
-                      </a>
-                    </Link>
-                    <ol className="pt-5 list-none">
-                      {sections?.map((section: SanityDocument, i: number) => {
-                        const {title} = section
-                        const {isCompleted: isSectionCompleted} =
-                          getSectionProgressForUser(progress, section.lessons)
-
-                        const isCompleted =
-                          isSectionCompleted ??
-                          find(progress, {lessonSlug: section.slug})
-                            ?.completedAt
-
-                        return (
-                          <li
-                            key={title}
-                            className="group md:-ml-4 relative flex items-baseline before:opacity-60 before:absolute before:content-[attr(data-index)] before:text-xs marker:text-gray-400 before:pl-2"
-                          >
-                            <Link
-                              href={{
-                                pathname: '/learn/[module]/[section]',
-                                query: {
-                                  module: module.slug,
-                                  section: section.slug,
-                                },
-                              }}
-                              passHref
-                            >
-                              <a
-                                aria-label={`${title} ${
-                                  isCompleted ? '(completed)' : ''
-                                }`}
-                                data-index={isCompleted ? '✓' : i + 1}
-                                className={cx(
-                                  `after:content-[""] after:absolute after:left-[21.5px] after:top-0 group-first-of-type:after:top-1/2 group-first-of-type:after:h-1/2 after:w-px after:bg-white/5 after:h-full group rounded-md pl-3 group-hover:bg-green-800/20 text-sand-100 hover:text-white focus-visible:ring-white w-full font-medium py-4 transition relative items-center inline-flex before:font-semibold before:z-10 before:flex before:items-center before:justify-center before:font-mono before:content-[attr(data-index)] before:w-5 before:h-5 before:left-0 before:rounded-full before:flex-shrink-0`,
-                                  {
-                                    'before:text-[0.55em] before:text-sans-500/50 before:border before:border-white/20 before:bg-green-700 before:bg-noise':
-                                      !isCompleted,
-                                    'before:text-sm before:text-white  before:bg-green-500':
-                                      isCompleted,
-                                  },
-                                )}
-                              >
-                                <span className="inline-flex pl-2">
-                                  {title}
-                                </span>
-                              </a>
-                            </Link>
-                          </li>
-                        )
-                      })}
-
-                      {sections?.length > 0 && (
-                        <li
-                          key={`certificate-${title}`}
-                          className={cx(
-                            `w-full group mt-7 after:content-[""] after:absolute after:left-[21.5px] after:bottom-1/2 after:w-px after:bg-white/5 after:h-full md:-ml-4 relative flex items-baseline group`,
-                          )}
-                        >
-                          <GetCertificate
-                            isAvailable={isCompleted ?? false}
-                            module={module}
-                          />
-                        </li>
+      <main>
+        <nav className="w-full bg-green-800/40" aria-label="Search">
+          <div className="mx-auto py-[2px] w-full max-w-screen-lg flex">
+            <Search product={product} />
+          </div>
+        </nav>
+        <div className="max-w-screen-md mx-auto w-full lg:p-1 p-5 lg:pt-10 pt-10 lg:pb-32 pb-16">
+          <header className="pt-10 pb-24 text-white flex flex-col items-center justify-center">
+            <h1 className="font-heading text-5xl font-bold text-center max-w-lg">
+              Testing Accessibility Workshops
+            </h1>
+            <Link
+              href={
+                nextUpLesson
+                  ? {
+                      pathname: '/learn/[module]/[section]/[lesson]',
+                      query: getPathForLesson(nextUpLesson.slug, product.modules),
+                    }
+                  : {
+                      pathname: '/learn/[module]',
+                      query: {module: 'foundations-of-accessibility'},
+                    }
+              }
+            >
+              <a className="flex items-center font-medium gap-2 mt-8 px-4 py-2.5 rounded-md border border-white/20 hover:bg-white/5 transition focus-visible:ring-white">
+                {isEmpty(progress) ? 'Start' : 'Continue'} Learning{' '}
+                <ChevronRightIcon aria-hidden="true" className="w-4 h-4 mt-0.5" />
+              </a>
+            </Link>
+          </header>
+          {/* <h1 className="font-nav text-center text-2xl leading-none font-bold text-white py-10">
+            {title}
+          </h1> */}
+          <div className="grid grid-cols-1 gap-16 w-full">
+            {modules.map((module: SanityDocument, i: number) => {
+              const {title, slug, image, sections} = module
+              const {completedSections, isCompleted} = getModuleProgressForUser(
+                progress,
+                sections,
+              )
+              return (
+                <ol key={slug} className="text-white">
+                  <li className="flex md:flex-row flex-col md:items-start items-center justify-center">
+                    <div
+                      className={cx(
+                        'flex items-center justify-center flex-shrink-0 hover:rotate-0 transition ease-in-out duration-300',
+                        {'md:-rotate-2': i % 2 === 0},
+                        {'md:rotate-2': i % 2 === 1},
                       )}
-                    </ol>
-                  </div>
-                </li>
-              </ol>
-            )
-          })}
+                    >
+                      <Image
+                        className="drop-shadow-xl"
+                        src={image.url}
+                        alt={image.alt}
+                        width={350}
+                        height={350}
+                        quality={100}
+                      />
+                    </div>
+                    <div className="w-full md:pt-16">
+                      <Link
+                        href={{
+                          pathname: '/learn/[module]',
+                          query: {module: slug},
+                        }}
+                        passHref
+                      >
+                        <a className="hover:underline sm:text-4xl text-3xl leading-tight font-bold mt-2 inline-flex justify-center w-full font-heading md:text-left text-center focus-visible:ring-white">
+                          <h2>{title}</h2>
+                        </a>
+                      </Link>
+                      <ol className="pt-5 list-none">
+                        {sections?.map((section: SanityDocument, i: number) => {
+                          const {title} = section
+                          const {isCompleted: isSectionCompleted} =
+                            getSectionProgressForUser(progress, section.lessons)
+
+                          const isCompleted =
+                            isSectionCompleted ??
+                            find(progress, {lessonSlug: section.slug})
+                              ?.completedAt
+
+                          return (
+                            <li
+                              key={title}
+                              className="group md:-ml-4 relative flex items-baseline before:opacity-60 before:absolute before:content-[attr(data-index)] before:text-xs marker:text-gray-400 before:pl-2"
+                            >
+                              <Link
+                                href={{
+                                  pathname: '/learn/[module]/[section]',
+                                  query: {
+                                    module: module.slug,
+                                    section: section.slug,
+                                  },
+                                }}
+                                passHref
+                              >
+                                <a
+                                  aria-label={`${title} ${
+                                    isCompleted ? '(completed)' : ''
+                                  }`}
+                                  data-index={isCompleted ? '✓' : i + 1}
+                                  className={cx(
+                                    `after:content-[""] after:absolute after:left-[21.5px] after:top-0 group-first-of-type:after:top-1/2 group-first-of-type:after:h-1/2 after:w-px after:bg-white/5 after:h-full group rounded-md pl-3 group-hover:bg-green-800/20 text-sand-100 hover:text-white focus-visible:ring-white w-full font-medium py-4 transition relative items-center inline-flex before:font-semibold before:z-10 before:flex before:items-center before:justify-center before:font-mono before:content-[attr(data-index)] before:w-5 before:h-5 before:left-0 before:rounded-full before:flex-shrink-0`,
+                                    {
+                                      'before:text-[0.55em] before:text-sans-500/50 before:border before:border-white/20 before:bg-green-700 before:bg-noise':
+                                        !isCompleted,
+                                      'before:text-sm before:text-white  before:bg-green-500':
+                                        isCompleted,
+                                    },
+                                  )}
+                                >
+                                  <span className="inline-flex pl-2">
+                                    {title}
+                                  </span>
+                                </a>
+                              </Link>
+                            </li>
+                          )
+                        })}
+
+                        {sections?.length > 0 && (
+                          <li
+                            key={`certificate-${title}`}
+                            className={cx(
+                              `w-full group mt-7 after:content-[""] after:absolute after:left-[21.5px] after:bottom-1/2 after:w-px after:bg-white/5 after:h-full md:-ml-4 relative flex items-baseline group`,
+                            )}
+                          >
+                            <GetCertificate
+                              isAvailable={isCompleted ?? false}
+                              module={module}
+                            />
+                          </li>
+                        )}
+                      </ol>
+                    </div>
+                  </li>
+                </ol>
+              )
+            })}
+          </div>
         </div>
       </main>
     </Layout>
