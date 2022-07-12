7fdc2a687cb7c85e0d59985d41ff8428cff38502
Joel Hooks <joelhooks@gmail.com>
2022-07-12 08:53:42 -0700

    fix(quiz): when finding ID, don't rely on reference to question object

--- packages/quiz/src/utils/submit-answer.ts
+++ packages/quiz/src/utils/submit-answer.ts
@@ -10,14 +10,10 @@ export default async function handleSubmitAnswer(context: QuizContext) {
     answer,
   } = context
 
-  // TODO check Sanity API structure versus static JSON structure
-  //      they should be the same! It appears as though that might not be the case
-  let id =
-    findKey(questionSet, currentQuestion) ||
-    findKey(
-      questionSet,
-      (question) => question.question === currentQuestion.question,
-    )
+  const id = findKey(
+    questionSet,
+    (question) => question.question === currentQuestion.question,
+  )
 
   return axios.post('/api/answer', {
     tagId,
