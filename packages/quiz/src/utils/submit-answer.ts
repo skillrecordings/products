05d786d39f1646e111c00d31ca16f5b446ced880
Joel Hooks <joelhooks@gmail.com>
2022-07-12 08:22:54 -0700

    fix(protailwind): convertkit forms succeed

--- packages/quiz/src/utils/submit-answer.ts
+++ packages/quiz/src/utils/submit-answer.ts
@@ -9,10 +9,20 @@ export default async function handleSubmitAnswer(context: QuizContext) {
     questionSet,
     answer,
   } = context
+
+  // TODO check Sanity API structure versus static JSON structure
+  //      they should be the same! It appears as though that might not be the case
+  let id =
+    findKey(questionSet, currentQuestion) ||
+    findKey(
+      questionSet,
+      (question) => question.question === currentQuestion.question,
+    )
+
   return axios.post('/api/answer', {
     tagId,
     survey: {
-      id: findKey(questionSet, currentQuestion),
+      id,
       answer,
     },
   })
