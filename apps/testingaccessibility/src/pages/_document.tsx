71d36e071e7a849b5f42e57daefca90aeef44bc6
Vojta Holik <vojta@egghead.io>
2022-07-13 17:18:05 +0200

    update(testingaccessibility): favicon

--- apps/testingaccessibility/src/pages/_document.tsx
+++ apps/testingaccessibility/src/pages/_document.tsx
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.ico" />
           <link
             rel="mask-icon"
             href="/mask-icon.svg"
