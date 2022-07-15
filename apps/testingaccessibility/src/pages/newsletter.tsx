8542ebb5b8838334474f2cb302e1b55e60b9e9b7
Taylor Bell <taylorbell@gmail.com>
2022-07-15 17:02:34 -0600

    conditonal form display

--- apps/testingaccessibility/src/pages/newsletter.tsx
+++ apps/testingaccessibility/src/pages/newsletter.tsx
@@ -1,14 +1,16 @@
 import Layout from 'components/app/layout'
 import {getOgImage} from 'utils/get-og-image'
 import {
-  redirectUrlBuilder,
   SubscribeToConvertkitForm,
+  useConvertkit,
+  redirectUrlBuilder,
 } from '@skillrecordings/convertkit'
 import {PrimaryNewsletterCta} from 'components/primary-newsletter-cta'
 
 const NewsletterPage = () => {
-  const title = 'Newsletter Signup'
+  const title = 'Newsletter'
   const ogImage = getOgImage(title)
+  const {subscriber, loadingSubscriber} = useConvertkit()
   return (
     <Layout meta={{title, ogImage}} className="w-full">
       <main className="bg-green-700 bg-noise text-white w-full mx-auto py-10 lg:pb-32 sm:pb-20">
@@ -16,7 +18,13 @@ const NewsletterPage = () => {
           {title}
         </h1>
         <div className="w-full">
-          <PrimaryNewsletterCta />
+          {!loadingSubscriber && !subscriber ? (
+            <PrimaryNewsletterCta />
+          ) : (
+            <div className="flex items-center justify-center not-prose max-w-screen-sm mx-auto px-5 mt-5">
+              <p>It looks like you're already signed up!</p>
+            </div>
+          )}
         </div>
       </main>
     </Layout>
