8ddc60a94e5526726e7ae3975da7b0f80ba7a585
Joel Hooks <joelhooks@gmail.com>
2022-07-18 16:27:07 -0700

    feat(testingaccessibility): show nav faster :zap:

--- apps/testingaccessibility/src/hooks/use-current-ability.ts
+++ apps/testingaccessibility/src/hooks/use-current-ability.ts
@@ -1,14 +1,19 @@
-import {useSession} from 'next-auth/react'
 import {getCurrentAbility} from '../server/ability'
+import {useQuery} from 'react-query'
 
 /**
  * Uses next-auth session data to return a viewer's current ability.
  */
 export function useCurrentAbility() {
-  const {data: sessionData, status} = useSession()
+  // const session = useSession()
+
+  const {data, status} = useQuery('/api/auth/session', async ({queryKey}) => {
+    return await fetch(queryKey[0]).then((res) => res.json())
+  })
 
   return {
     status,
-    ability: getCurrentAbility({rules: sessionData?.rules}),
+    ability: getCurrentAbility({rules: data?.rules}),
+    user: data,
   }
 }
--- apps/testingaccessibility/src/hooks/use-nav-state.ts
+++ apps/testingaccessibility/src/hooks/use-nav-state.ts
@@ -1,13 +1,14 @@
 import {useCurrentAbility} from './use-current-ability'
+import {isEmpty} from 'lodash'
 
 /**
  * TODO: consider removing this and using abilities?
  */
 export function useNavState() {
-  const {status, ability} = useCurrentAbility()
+  const {status, ability, user} = useCurrentAbility()
 
   return {
-    isSignedIn: status === 'authenticated',
+    isSignedIn: ability.can('view', 'Account'),
     isLoadingUser: status === 'loading',
     canViewTeam: ability.can('view', 'Team'),
     canViewInvoice: ability.can('view', 'Invoice'),
