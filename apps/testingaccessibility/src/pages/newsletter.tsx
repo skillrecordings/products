87a9ee2c78b8f8a263fd34b9ea151226c90c3486
Vojta Holik <vojta@egghead.io>
2022-07-18 09:40:59 +0200

    update: conditional nav and headings

--- apps/testingaccessibility/src/pages/newsletter.tsx
+++ apps/testingaccessibility/src/pages/newsletter.tsx
@@ -1,27 +1,24 @@
 import Layout from 'components/app/layout'
 import {getOgImage} from 'utils/get-og-image'
-import {useConvertkit} from '@skillrecordings/convertkit'
 import {PrimaryNewsletterCta} from 'components/primary-newsletter-cta'
 
 const NewsletterPage = () => {
   const title = 'Newsletter'
   const ogImage = getOgImage(title)
-  const {subscriber, loadingSubscriber} = useConvertkit()
+
   return (
     <Layout meta={{title, ogImage}} className="w-full">
-      <main className="bg-green-700 bg-noise text-white w-full mx-auto py-10 lg:pb-32 sm:pb-20">
-        <h1 className="text-center font-extrabold lg:text-4xl sm:text-3xl text-2xl font-heading">
-          {title}
-        </h1>
-        <div className="w-full">
-          {!loadingSubscriber && !subscriber ? (
-            <PrimaryNewsletterCta />
-          ) : (
-            <div className="flex items-center justify-center not-prose max-w-screen-sm mx-auto px-5 mt-5">
-              <p>It looks like you're already signed up!</p>
-            </div>
-          )}
-        </div>
+      <main className="bg-green-700 bg-noise text-white w-full mx-auto">
+        <PrimaryNewsletterCta>
+          <h1 className="max-w-xl font-heading mx-auto -mt-4 sm:text-4xl text-3xl leading-none text-center md:text-5xl font-bold sm:mt-0">
+            Join my exclusive, free{' '}
+            <span className="whitespace-nowrap">6-part</span> email course
+          </h1>
+          <h2 className="max-w-md leading-tight pt-6 pb-16 text-xl text-center text-orange-200">
+            And learn more about building and testing accessible web
+            applications.
+          </h2>
+        </PrimaryNewsletterCta>
       </main>
     </Layout>
   )
