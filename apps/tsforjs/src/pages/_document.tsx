5074142e0786c4ed68af72c05657439044d52762
Joel Hooks <joelhooks@gmail.com>
2022-07-19 14:33:20 -0700

    fix: use a custom document to ssr the stylesheets

--- apps/tsforjs/src/pages/_document.tsx
+++ apps/tsforjs/src/pages/_document.tsx
@@ -1,27 +1,28 @@
-import * as React from 'react'
-import {Html, Head, Main, NextScript} from 'next/document'
+import Document, {DocumentContext} from 'next/document'
+import {ServerStyleSheet} from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+  render(): JSX.Element {
+    return super.render()
+  }
 }
