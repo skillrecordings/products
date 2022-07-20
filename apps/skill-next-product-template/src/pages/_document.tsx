d1698d1001a3c11d8e0b1f2c6ff6f13062a00b0d
Vojta Holik <vojta@egghead.io>
2022-07-20 16:03:23 +0200

    feat: skill favicon ⚡️

--- apps/skill-next-product-template/src/pages/_document.tsx
+++ apps/skill-next-product-template/src/pages/_document.tsx
@@ -4,20 +4,7 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
