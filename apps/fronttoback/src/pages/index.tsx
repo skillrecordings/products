25beb7e078482c6fab9698dbf60dcbf92671ad69
DrShpongle <evgeniy.nagalskiy@gmail.com>
2022-07-20 19:28:05 -0700

    add bg

--- apps/fronttoback/src/pages/index.tsx
+++ apps/fronttoback/src/pages/index.tsx
@@ -1,11 +1,23 @@
 import Layout from 'components/layout'
 import type {NextPage} from 'next'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <Layout>
+    <Layout className="bg-[#03021a]">
       <h1 className="text-4xl text-primary-500 font-bold flex items-center justify-center flex-grow">
-        Hi! 👋
+        <div className="relative w-full">
+          <div className="absolute inset-0">
+            <Image
+              alt="Mountains"
+              src="/images/background.webp"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+            />
+          </div>
+          <div className="relative min-h-screen">123</div>
+        </div>
       </h1>
     </Layout>
   )
