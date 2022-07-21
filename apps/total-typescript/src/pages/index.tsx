03825d6a759af81506ee95e0a1581eea1451c162
Vojta Holik <vojta@egghead.io>
2022-07-21 20:00:31 +0200

    fix: import

--- apps/total-typescript/src/pages/index.tsx
+++ apps/total-typescript/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Layout from 'components/layout'
+import Layout from 'components/app/layout'
 import LandingCopy from 'components/landing-copy.mdx'
 import {
   redirectUrlBuilder,
