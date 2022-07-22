6ef9af32ebb2799c4f5d6d6647c4a1b7a9e71a97
Lauro <hey@laurosilva.com>
2022-07-22 06:48:12 -0700

    add ck pages

---
+++ apps/total-typescript/src/pages/confirmed.tsx
@@ -0,0 +1,13 @@
+const Confirmed = () => {
+  return (
+    <div className="p-0 min-h-[calc(100vh-96px)] flex flex-col">
+      <div className="flex flex-col items-center justify-center flex-grow p-5 text-center">
+        <p className="pt-8 pb-6 text-2xl font-condensed sm:text-3xl text-badass-pink-500">
+          Success! Thank you for signing up
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Confirmed
