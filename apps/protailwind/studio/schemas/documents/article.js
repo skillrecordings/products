950d8bb5495090ae14ba6dd7d3235e847a1e95b5
Vojta Holik <vojta@egghead.io>
2022-07-27 12:43:05 +0200

    feat: sanity studio ready for articles

---
+++ apps/protailwind/studio/schemas/documents/article.js
@@ -0,0 +1,125 @@
+import React from 'react'
+
+export default {
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      title: 'Published',
+      name: 'published',
+      type: 'boolean',
+      initialValue: true,
+    },
+    {
+      title: 'Subscribers Only',
+      name: 'subscribersOnly',
+      type: 'boolean',
+      readOnly: true,
+      initialValue: false,
+    },
+    {
+      name: 'preview',
+      title: 'Preview',
+      type: 'body',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'body',
+      title: 'Full Article',
+      type: 'body',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Call to Action',
+      name: 'cta',
+      type: 'object',
+      description: 'Displayed at the bottom of the article. Not required.',
+      fields: [
+        {
+          title: 'Text',
+          type: 'body',
+          name: 'body',
+        },
+        {
+          title: 'Form',
+          name: 'ckFormId',
+          type: 'string',
+          of: [{type: 'string'}],
+          options: {
+            list: [
+              {
+                title: 'Initial Landing Page: Pro Tailwind (3179174)',
+                value: '3179174',
+              },
+            ],
+            sortable: false,
+          },
+        },
+        {
+          title: 'Action Label',
+          name: 'actionLabel',
+          type: 'string',
+          initialValue: 'Learn Tailwind',
+        },
+      ],
+    },
+    {name: 'image', title: 'Image', type: 'externalImage'},
+    {
+      name: 'ogImage',
+      title: 'Share Card',
+      type: 'externalImage',
+      description: '1200x628',
+    },
+    {
+      name: 'description',
+      title: 'SEO Description',
+      type: 'text',
+      validation: (Rule) => Rule.max(160),
+    },
+    {
+      name: 'date',
+      title: 'Published Date',
+      type: 'date',
+      validation: (Rule) => Rule.required(),
+    },
+    // {
+    //   name: 'tags',
+    //   title: 'Tags',
+    //   type: 'array',
+    //   of: [{type: 'reference', to: [{type: 'tag'}]}],
+    //   options: {
+    //     layout: 'tags',
+    //   },
+    // },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'image.url',
+    },
+    prepare(selection) {
+      const {media, title} = selection
+      return {
+        title: title,
+        media: media && <img src={media} alt={title} />,
+      }
+    },
+  },
+}
---
+++ apps/protailwind/studio/src/components/logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './styles.css'
+
+const Logo = () => (
+  <img className={styles.logo} src="/static/logo@2x.png" alt="Pro Tailwind" />
+)
+
+export default Logo
