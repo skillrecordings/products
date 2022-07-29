74ad9e449af7c22529bd5db010d3baf76e4e191d
Nicoll <nicoll.g4@gmail.com>
2022-07-29 13:03:11 -0500

    initial commit - header

--- apps/tsforjs/src/pages/index.tsx
+++ apps/tsforjs/src/pages/index.tsx
@@ -1,8 +1,17 @@
 import type {NextPage} from 'next'
-import MainContent from '../components/original/MainContent/MainContent'
 
 const Home: NextPage = () => {
-  return <MainContent />
+  return (
+    <>
+      <div className="relative w-full">
+        <div className="flex:auto flex-col text-center items-center bg-black">
+          <h1 className="pt-52 h-screen text-4xl md:text-6xl xl:text-7xl 2xl:text-9xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-500">
+            Type errors are ✨annoying✨
+          </h1>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
