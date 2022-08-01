ab61ead5e50774fe60603e5800265f5ffcc003e3
Joel Hooks <joelhooks@gmail.com>
2022-08-01 13:15:19 -0700

    Revert "Revert "feat(skill-api): use tracer package""
    This reverts commit da782571e37f6f7bd807c5a45ec29bdf76d8be18.

--- packages/skill-api/src/next/index.ts
+++ packages/skill-api/src/next/index.ts
@@ -8,7 +8,7 @@ import {SkillRecordingsHandler} from '../core'
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {PrismaClient} from '@skillrecordings/database'
 import {setupHttpTracing} from '@vercel/tracing-js'
-import {tracer} from '../server'
+import {tracer} from '@skillrecordings/honeycomb-tracer'
 
 /** Extract the host from the environment */
 export function detectHost(forwardedHost: any) {
--- packages/skill-api/src/server/index.ts
+++ packages/skill-api/src/server/index.ts
@@ -1,6 +1,5 @@
 export * from './prices/format-prices-for-product'
 export * from './post-to-slack'
-export * from './utils/honeycomb-tracer'
 export * from './prices/parity-coupon'
 export * from './prices/get-calculated-price'
 export * from './db/context'
