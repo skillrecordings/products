e9e05eb4fb7c2915450fb34df29cdf67e4d0d34b
Joel Hooks <joelhooks@gmail.com>
2022-08-01 16:55:20 -0700

    chore: delete viewer package

--- packages/viewer/src/index.ts
+++
@@ -1,5 +0,0 @@
-export {
-  ViewerContext,
-  ViewerProvider,
-  useViewer,
-} from './contexts/viewer-context'
--- packages/viewer/src/utils/get-dev-access-token.ts
+++
@@ -1,8 +0,0 @@
-export default function getDevAccessToken() {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'test'
-  ) {
-    return process.env.NEXT_PUBLIC_DEV_USER_TOKEN
-  }
-}
