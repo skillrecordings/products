362eb033d4e03adfa4e332bde3c77c2dc7a532e7
Nicoll <nicoll.g4@gmail.com>
2022-08-02 12:48:44 -0500

    tailwind design

--- apps/tsforjs/src/pages/confirm.tsx
+++ apps/tsforjs/src/pages/confirm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from 'components/layout'
+import config from 'config'
 
 const ConfirmSubscriptionPage = () => {
   return (
@@ -29,12 +30,12 @@ const ConfirmSubscriptionPage = () => {
     </Layout>
   )
 }
-
 export default ConfirmSubscriptionPage
 
 export const Signature = () => {
   //TODO: add a signature
-  return null
+  // return null
+  return <>{config.author}</>
 }
 
 const Image = () => {
