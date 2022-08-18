2a431d361f5ee082acd4c712049049ac96e63c0b
Joel Hooks <joelhooks@gmail.com>
2022-08-17 18:49:43 -0700

    feat(em): download endpoint for book using cloudfront signed url

---
+++ apps/engmanagement/src/pages/api/download.ts
@@ -0,0 +1,43 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import AWS from 'aws-sdk'
+
+const accessKeyID = process.env.AWS_CLOUDFRONT_ACCESS_KEY_ID
+const privateKeyContents = process.env.AWS_CLOUDFRONT_SIGNING_PEM
+const cfDomainName = process.env.AWS_CLOUDFRONT_DOMAIN
+const s3ContentPath = process.env.AWS_CLOUDFRONT_BOOK_PATH
+const cfFullPath = `${cfDomainName}/${s3ContentPath}`
+let signer = new AWS.CloudFront.Signer(accessKeyID, privateKeyContents)
+
+const option = {
+  url: cfFullPath,
+  expires: Math.floor(new Date().getTime()) + 60 * 60 * 24, // 24 hours from now
+}
+
+async function getSignedUrl() {
+  return new Promise((resolve, reject) => {
+    signer.getSignedUrl(option, (error, url) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(url)
+      }
+    })
+  })
+}
+
+const subscriber = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    try {
+      const url = await getSignedUrl()
+      res.status(200).json({url})
+    } catch (error) {
+      console.log(error)
+      res.status(200).end()
+    }
+  } else {
+    console.error('non-get request made')
+    res.status(404).end()
+  }
+}
+
+export default subscriber
