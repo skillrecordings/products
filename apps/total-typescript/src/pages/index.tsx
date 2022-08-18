6d94f8694def161c1a4badd92be391a7e56734ba
Vojta Holik <vojta@egghead.io>
2022-08-18 11:58:27 +0200

    update(tts): landing page design (#422)
    * update(design): images
    
    * feat: add sanity
    
    * update(tts): landing page design

--- apps/total-typescript/src/pages/index.tsx
+++ apps/total-typescript/src/pages/index.tsx
@@ -6,12 +6,13 @@ import {
   SubscribeToConvertkitForm,
 } from '@skillrecordings/convertkit'
 import {useRouter} from 'next/router'
+import {MDXComponents} from 'components/mdx'
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout meta={{titleAppendSiteName: false}}>
       <Header />
-      <main className="pt-16">
+      <main>
         <Copy />
         <SubscribeToNewsletter />
       </main>
@@ -21,7 +22,7 @@ const HomePage = () => {
 
 const Header = () => {
   return (
-    <header className="flex flex-col justify-center items-center px-5 overflow-hidden">
+    <header className="flex flex-col justify-center items-center overflow-hidden px-5 relative">
       <div className="max-w-screen-lg flex lg:flex-row flex-col-reverse items-center w-full relative lg:min-h-[80vh]">
         <div className="relative z-10 max-w-2xl lg:py-48 lg:pb-48 pb-10">
           <h1 className="sm:mt-0 mt-16 font-heading xl:text-6xl lg:text-5xl sm:text-5xl text-4xl font-normal xl:leading-[1.15] lg:leading-[1.15] sm:leading-[1.15] leading-[1.25] max-w-[14ch]">
@@ -49,13 +50,20 @@ const Header = () => {
           />
         </div>
       </div>
+      <Image
+        src={require('../../public/assets/landing/bg-divider-1.png')}
+        layout="fill"
+        className="object-contain object-bottom translate-y-48 select-none pointer-events-none"
+        alt=""
+        aria-hidden="true"
+      />
     </header>
   )
 }
 
 const Copy = () => {
   return (
-    <article className="md:prose-h2:py-16 prose-h2:text-3xl prose-h2:py-5 md:prose-h2:text-5xl md:prose-xl sm:prose-lg prose-base opacity-90 prose-p:font-light w-full prose-pre:overflow-auto prose-p:max-w-2xl prose-p:mx-auto prose-headings:max-w-3xl prose-headings:mx-auto prose-pre:max-w-2xl prose-pre:mx-auto prose-ul:max-w-2xl prose-ul:mx-auto prose-ul:list-disc marker:text-cyan-400 prose-headings:font-bold prose-p:px-5 prose-headings:px-5 prose-headings:font-text prose-h2:text-center">
+    <article className="prose-h2:text-3xl md:prose-h2:text-5xl md:prose-xl sm:prose-lg prose-base opacity-90 prose-p:font-light w-full prose-pre:overflow-auto prose-p:max-w-2xl prose-p:mx-auto prose-headings:max-w-2xl prose-headings:mx-auto prose-pre:max-w-2xl prose-pre:mx-auto prose-ul:max-w-2xl prose-ul:mx-auto prose-ul:list-disc marker:text-cyan-400 prose-headings:font-bold prose-p:px-5 prose-headings:px-5 prose-headings:font-text prose-h2:text-center">
       <LandingCopy />
     </article>
   )
@@ -64,34 +72,49 @@ const Copy = () => {
 const SubscribeToNewsletter = () => {
   const router = useRouter()
   return (
-    <section className="sm:mt-32 mt-20 pt-24 pb-32 bg-black/20 text-white flex flex-col items-center justify-center px-5">
-      <Image
-        src={require('../../public/assets/ts-scroll@2x.png')}
-        quality={100}
-        width={650 / 2}
-        height={650 / 2}
-        alt=""
-        aria-hidden="true"
-        placeholder="blur"
-      />
-      <h2 className="xl:text-6xl lg:text-5xl sm:text-5xl text-4xl font-heading font-bold max-w-[15ch] text-center">
-        Become a TypeScript Wizard
-      </h2>
-      <div className="py-16 w-full max-w-sm" id="newsletter">
-        <SubscribeToConvertkitForm
-          actionLabel="Subscribe"
-          onSuccess={(subscriber: any) => {
-            if (subscriber) {
-              const redirectUrl = redirectUrlBuilder(subscriber, '/confirm')
-              router.push(redirectUrl)
-            }
-          }}
+    <MDXComponents.Section
+      className="py-40 bg-[#081021] flex flex-col items-center"
+      slot={
+        <Image
+          src="/assets/landing/bg-divider-7.png"
+          layout="fill"
+          className="pointer-events-none object-contain select-none"
+          objectPosition="top center"
+          quality={100}
+        />
+      }
+    >
+      <div className="flex items-center justify-center">
+        <Image
+          src={require('../../public/assets/ts-scroll@2x.png')}
+          quality={100}
+          width={650 / 2}
+          height={650 / 2}
+          alt=""
+          aria-hidden="true"
+          placeholder="blur"
         />
       </div>
-      <p className="text-gray-400">
+      <h2 className="xl:text-6xl lg:text-5xl sm:text-5xl text-4xl font-heading font-bold max-w-[15ch] text-center mx-auto">
+        Become a TypeScript Wizard
+      </h2>
+      <div className="py-16 w-full mx-auto relative" id="newsletter">
+        <div className="max-w-sm mx-auto">
+          <SubscribeToConvertkitForm
+            actionLabel="Subscribe"
+            onSuccess={(subscriber: any) => {
+              if (subscriber) {
+                const redirectUrl = redirectUrlBuilder(subscriber, '/confirm')
+                router.push(redirectUrl)
+              }
+            }}
+          />
+        </div>
+      </div>
+      <p className="text-gray-400 text-center">
         I respect your privacy. Unsubscribe at any time.
       </p>
-    </section>
+    </MDXComponents.Section>
   )
 }
 
