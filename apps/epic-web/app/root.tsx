c22f1407b36d1c70c5a8b493531cf519cdb2b033
Joel Hooks <joelhooks@gmail.com>
2022-08-22 13:19:52 -0700

    feat(ew): add tailwind

--- apps/epic-web/app/root.tsx
+++ apps/epic-web/app/root.tsx
@@ -1,4 +1,8 @@
-import type {MetaFunction} from '@remix-run/node'
+import type {
+  HeadersFunction,
+  LinksFunction,
+  MetaFunction,
+} from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -8,16 +12,52 @@ import {
   ScrollRestoration,
 } from '@remix-run/react'
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-})
+import styles from './tailwind.css'
+
+export const meta: MetaFunction = () => {
+  return {
+    title: 'Epic Web Dev by Kent C. Dodds',
+    description: 'Learn full-stack web development with Kent C. Dodds',
+    'twitter:site': '@kentcdodds',
+    'twitter:creator': '@kentcdodds',
+    'twitter:title': 'Epic Web by Kent C. Dodds',
+    'twitter:description':
+      'Learn full-stack web development with Kent C. Dodds',
+    'twitter:image': 'https://mattpocock.com/og-image.jpg',
+    'og:title': 'Epic Web by Kent C. Dodds',
+    'og:description': 'Learn full-stack web development with Kent C. Dodds',
+    'og:url': 'https://epicweb.dev',
+    'og:type': 'website',
+    'og:site_name': 'Epic Web by Kent C. Dodds',
+  }
+}
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+    {
+      rel: 'icon',
+      href: '/favicon.png',
+      type: 'image/png',
+    },
+  ]
+}
+
+export const headers: HeadersFunction = () => {
+  return {
+    'Cache-Control': 'max-age=1800',
+  }
+}
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
