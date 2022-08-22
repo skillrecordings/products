37bd05572e5415d505eac66e8e36a7b1f12d9f38
Joel Hooks <joelhooks@gmail.com>
2022-08-22 15:23:38 -0700

    feat(ew): improve form and add cookie

---
+++ apps/epic-web/app/cookies.ts
@@ -0,0 +1,11 @@
+import {createCookie} from '@remix-run/node' // or cloudflare/deno
+
+const hour = 3600000
+export const oneYear = 365 * 24 * hour
+
+export const convertkitSubscriberCookie = createCookie('ck_subscriber', {
+  secure: process.env.NODE_ENV === 'production',
+  httpOnly: true,
+  path: '/',
+  maxAge: 31556952,
+})
---
+++ apps/epic-web/app/lib/convertkit.server.ts
@@ -0,0 +1,1 @@
+export * from '@skillrecordings/convertkit-sdk'
