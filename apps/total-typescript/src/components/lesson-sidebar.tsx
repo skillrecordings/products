8243b99d606b350c592df490c824e0c3ab11abdb
Vojta Holik <vojta@egghead.io>
2022-09-01 17:52:58 +0200

    update(tts): scrollable lesson navigator

--- apps/total-typescript/src/components/lesson-sidebar.tsx
+++ apps/total-typescript/src/components/lesson-sidebar.tsx
@@ -3,6 +3,7 @@ import {SanityDocument} from '@sanity/client'
 import Link from 'next/link'
 import LessonNavigator from './lesson-navigator'
 import cx from 'classnames'
+import SimpleBar from 'simplebar-react'
 
 type SidebarProps = {
   module: SanityDocument
@@ -22,37 +23,40 @@ const LessonSidebar: React.FC<SidebarProps> = ({
       />
       <nav
         className={cx(
-          'bg-slate-900 lg:max-w-xs lg:fixed  w-full top-0 border-r border-gray-800',
+          'bg-slate-900 lg:max-w-xs lg:fixed  w-full top-0 border-r border-gray-800 lg:border-b border-b',
           className,
         )}
       >
-        <div>
-          <div className="flex items-center gap-5 px-3 lg:pt-16 pt-2 pb-2 bg-blue-600">
-            {module?.image && <img src={module.image} className="w-20" />}
-            <div>
-              <Link href="/tutorials">
-                <a className="uppercase text-sm font-semibold opacity-80 hover:underline">
-                  {module.moduleType}s<span className="pl-1">/</span>
-                </a>
-              </Link>
-              <h1 className="text-2xl font-bold leading-tight font-text">
-                <Link
-                  href={{
-                    pathname: `${path}/[module]`,
-                    query: {module: module.slug},
-                  }}
-                  passHref
-                >
-                  <a className="hover:underline">{module.title}</a>
+        <SimpleBar className="relative lg:max-h-screen max-h-[580px] lg:pb-16">
+          <div className="sticky top-0 z-10 bg-gray-900/80 backdrop-blur-lg">
+            <div className="flex items-center gap-5 px-3 lg:pt-16 pt-2 pb-2 bg-blue-600">
+              {module?.image && <img src={module.image} className="w-20" />}
+              <div>
+                <Link href="/tutorials">
+                  <a className="uppercase text-sm font-semibold opacity-80 hover:underline">
+                    {module.moduleType}s<span className="pl-1">/</span>
+                  </a>
                 </Link>
-              </h1>
+                <h1 className="text-2xl font-bold leading-tight font-text">
+                  <Link
+                    href={{
+                      pathname: `${path}/[module]`,
+                      query: {module: module.slug},
+                    }}
+                    passHref
+                  >
+                    <a className="hover:underline">{module.title}</a>
+                  </Link>
+                </h1>
+              </div>
             </div>
+            <h3 className="pt-5 pb-2 text-sm opacity-80 font-semibold uppercase px-5">
+              Lessons
+            </h3>
           </div>
-          <h3 className="pt-5 pb-2 text-sm opacity-80 font-semibold uppercase px-5">
-            Lessons
-          </h3>
           <LessonNavigator module={module} path={path} />
-        </div>
+        </SimpleBar>
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-gray-900 to-transparent h-24 z-20 pointer-events-none" />
       </nav>
     </>
   )
