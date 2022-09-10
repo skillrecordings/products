b026a4864926069480ab8faaf76c231de0f6244a
Joel Hooks <joelhooks@gmail.com>
2022-09-10 10:26:33 -0700

    update(tt): add rss link in head

--- apps/total-typescript/src/pages/_document.tsx
+++ apps/total-typescript/src/pages/_document.tsx
@@ -4,7 +4,14 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed for TotalTypeScript.com"
+          href="/rss.xml"
+        />
+      </Head>
       <body className="bg-gray-900 text-white antialiased">
         <Main />
         <NextScript />
