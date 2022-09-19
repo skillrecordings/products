e14fe20f160b9ae8cc477301b3a891915f9574c0
Creeland <cree@provinsal.com>
2022-09-19 17:39:46 -0500

    test(tt): add passing test expectations

--- apps/total-typescript/src/lib/castingwords.test.ts
+++ apps/total-typescript/src/lib/castingwords.test.ts
@@ -6,13 +6,14 @@ test('should build correct castingwords order url', () => {
     originalMediaUrl: 'https://example.com/video.mp4',
   })
 
-  expect(url).toEqual(`correct castingwords url here`)
+  expect(url).toEqual(
+    `https://castingwords.com/store/API4/order_url?sku=test1&test=1&url=https%3A%2F%2Fexample.com%2Fvideo.mp4&api_key=castingwords-api-key`,
+  )
 })
 
 test('orderTranscript should call castingwords url and return order', async () => {
   const order = await orderTranscript(
     'https://www.dropbox.com/s/bssbwpz4khvvosr/beginners-01.problem.mp4?dl=1',
   )
-
-  expect(order.order).toEqual(`wat is this`)
+  expect(order.order).toEqual(`teSt`)
 })
