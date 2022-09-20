ebc38ad9eeee44914d82f9e0b8f392a0b13ae4cb
Joel Hooks <joelhooks@gmail.com>
2022-09-20 12:54:39 -0700

    fix(tt): CK type schema

--- apps/total-typescript/src/lib/convertkit.ts
+++ apps/total-typescript/src/lib/convertkit.ts
@@ -2,10 +2,10 @@ import z from 'zod'
 
 export const SubscriberSchema = z.object({
   id: z.number(),
-  first_name: z.string().optional(),
+  first_name: z.string().nullish(),
   email_address: z.string(),
   state: z.string(),
-  fields: z.object({last_name: z.string().optional()}),
+  fields: z.object({last_name: z.string().nullish()}),
 })
 
 export type Subscriber = z.infer<typeof SubscriberSchema>
