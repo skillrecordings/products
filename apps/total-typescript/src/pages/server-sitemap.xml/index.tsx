3a60e320c802720de73c5648845c2a0930fd9939
Vojta Holik <vojta@egghead.io>
2022-09-22 15:34:06 +0200

    feat(tt): tips sitemap

--- apps/total-typescript/src/pages/server-sitemap.xml/index.tsx
+++ apps/total-typescript/src/pages/server-sitemap.xml/index.tsx
@@ -1,6 +1,7 @@
 import {getServerSideSitemap} from 'next-sitemap'
 import {GetServerSideProps} from 'next'
 import {getAllTutorials} from '../../lib/tutorials'
+import {getAllTips} from 'lib/tips'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   ctx.res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
@@ -35,7 +36,20 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       }),
     ]
   })
-  return getServerSideSitemap(ctx, [...tutorialSitemap])
+
+  const tips = await getAllTips()
+  const tipsSitemap = tips.flatMap((tip: any) => {
+    const tipRootUrl = `${process.env.NEXT_PUBLIC_URL}/tips/${tip.slug.current}`
+    return [
+      {
+        loc: tipRootUrl, // Absolute url
+        lastmod: new Date(tip._updatedAt).toISOString(),
+        changefreq: 'weekly',
+        priority: 0.7,
+      },
+    ]
+  })
+  return getServerSideSitemap(ctx, [...tutorialSitemap, ...tipsSitemap])
 }
 
 // Default export to prevent next.js errors
