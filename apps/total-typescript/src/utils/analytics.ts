a99633e0925b7f3dc77c1faaa555446ebc3ae9d2
Joel Hooks <joelhooks@gmail.com>
2022-10-10 11:25:47 -0700

    feat(tt): identify with amplitude

--- apps/total-typescript/src/utils/analytics.ts
+++ apps/total-typescript/src/utils/analytics.ts
@@ -1,11 +1,35 @@
 import {track as defaultTrack} from '@skillrecordings/analytics'
 import {
   identify as amplitudeIdentify,
+  Identify,
+  setUserId,
   track as amplitudeTrack,
 } from '@amplitude/analytics-browser'
+import {Subscriber} from '../lib/convertkit'
+import {isEmpty} from 'lodash'
 
 export async function track(event: string, params?: any) {
   console.debug(`track ${event}`, params)
   amplitudeTrack(event, params)
   return defaultTrack(event, params)
+}
+
+export async function identify(subscriber: Subscriber): Promise<unknown> {
+  try {
+    const identify = new Identify()
+
+    setUserId(subscriber.email_address)
+
+    for (const field in subscriber.fields) {
+      if (!isEmpty(subscriber.fields[field])) {
+        identify.set(field, subscriber.fields[field] as any)
+      } else {
+        identify.unset(field)
+      }
+    }
+
+    return amplitudeIdentify(identify).promise
+  } catch (e) {
+    return Promise.resolve(e)
+  }
 }
