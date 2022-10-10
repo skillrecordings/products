fcef0a14c23f74de8eb8c2a669b699e22d2181da
Vojta Holik <vojta@egghead.io>
2022-10-10 19:48:03 +0200

    update(tc): sanity deskStructure

--- apps/typescriptcourse/studio/deskStructure.js
+++ apps/typescriptcourse/studio/deskStructure.js
@@ -8,8 +8,6 @@ const hiddenDocTypes = (listItem) =>
     'article',
     'skosConcept',
     'skosConceptScheme',
-    'skosConcept',
-    'skosConceptScheme',
     'podcast',
     'podcastEpisode',
     'podcastSeason',
