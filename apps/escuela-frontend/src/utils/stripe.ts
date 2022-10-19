868fe353fe320412a663af1f686df2be0df156ec
Lauro <hey@laurosilva.com>
2022-10-19 13:58:25 -0700

    update type

--- apps/escuela-frontend/src/utils/stripe.ts
+++ apps/escuela-frontend/src/utils/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_TOKEN, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_TOKEN as string, {
   apiVersion: '2020-08-27',
 })
