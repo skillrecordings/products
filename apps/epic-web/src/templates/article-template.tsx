d2ec17a7a781641706c6ff69ed1ce33cfc0a9f32
Vojta Holik <vojta@egghead.io>
2022-10-19 21:02:58 +0200

    feat(ew): add code syntax highlighting and prep next article (#536)
    * feat(ew): code syntax highlighting
    
    * feat(ew): code syntax highlighting

--- apps/epic-web/src/templates/article-template.tsx
+++ apps/epic-web/src/templates/article-template.tsx
@@ -91,7 +91,7 @@ const Header: React.FC<HeaderProps> = ({
       )}
       <div
         className={cx(
-          'w-full md:min-h-screen flex items-center justify-center flex-col relative',
+          'w-full min-h-screen flex items-center justify-center flex-col relative',
           {
             'bg-gray-800/40': !image,
           },
@@ -102,7 +102,7 @@ const Header: React.FC<HeaderProps> = ({
             {title}
           </h1>
         </div>
-        <div className="md:absolute bottom-16 md:pb-0 pb-16 w-full max-w-screen-md px-5 text-gray-300 grid md:grid-cols-4 grid-cols-2 md:gap-16 gap-10 items-center">
+        <div className="md:absolute bottom-16 md:pb-0 pb-40 w-full max-w-screen-md px-5 text-gray-300 grid md:grid-cols-4 grid-cols-2 md:gap-16 gap-10 items-center">
           <div className="flex items-center md:justify-start justify-center gap-3 col-span-2">
             <div className="flex items-center justify-center rounded-full overflow-hidden bg-black/50 flex-shrink-0">
               <Image
@@ -205,7 +205,7 @@ const AboutKent = () => {
 
 const Body: React.FC<{value: any[]}> = ({value}) => {
   return (
-    <main className="prose md:prose-xl text-lg mx-auto md:py-16 py-8 max-w-3xl px-5">
+    <main className="prose md:prose-xl text-lg md:py-16 py-8 max-w-3xl px-5 w-full mx-auto">
       <PortableText value={value} components={PortableTextComponents} />
     </main>
   )
