8846d86decc3da56a7654ec03b215231922a85f2
Joel Hooks <joelhooks@gmail.com>
2022-10-28 11:50:14 -0700

    feat(mp): monorepo.party site added to monorepo

---
+++ apps/monorepo-party/src/config.mjs
@@ -0,0 +1,12 @@
+export const SITE = {
+  name: "Awesome Monorepo Resources",
+  domain: "https://monorepo.party",
+
+  title: "Monorepo Party 🎉",
+  description: "A collection of resources for building monorepos.",
+
+  postsPerPage: 6,
+  
+  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+  googleSiteVerificationId: "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
+};
---
+++ apps/monorepo-party/src/utils/getRelativeUrlByFilePath.js
@@ -0,0 +1,12 @@
+import path from 'path'
+import {getProjectRootDir} from './getProjectRootDir'
+
+const __srcFolder = path.join(getProjectRootDir(), '/src')
+
+export const getRelativeUrlByFilePath = (filepath) => {
+  if (filepath) {
+    return filepath.replace(__srcFolder, '')
+  }
+
+  return null
+}
