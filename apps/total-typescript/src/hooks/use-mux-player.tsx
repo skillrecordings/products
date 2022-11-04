af21b8bcd766b9ff97a084f8ab8bcbbf3f94ef30
Joel Hooks <joelhooks@gmail.com>
2022-11-04 12:09:41 -0700

    feat(tt): adds abilities to provider

--- apps/total-typescript/src/hooks/use-mux-player.tsx
+++ apps/total-typescript/src/hooks/use-mux-player.tsx
@@ -9,6 +9,8 @@ import {track} from '../utils/analytics'
 import {type Exercise, ExerciseSchema} from 'lib/exercises'
 import {type Tip, TipSchema} from 'lib/tips'
 import {useConvertkit} from './use-convertkit'
+import {AppAbility, getCurrentAbility} from 'ability/ability'
+import {useSession} from 'next-auth/react'
 
 type VideoResource = Exercise | Tip
 
@@ -25,6 +27,8 @@ type VideoContextType = {
   module: SanityDocument
   path: string
   video?: {muxPlaybackId: string | null | undefined}
+  canShowVideo: boolean
+  ability: AppAbility
 }
 
 export const VideoContext = React.createContext({} as VideoContextType)
@@ -32,6 +36,7 @@ export const VideoContext = React.createContext({} as VideoContextType)
 type VideoProviderProps = {
   module: SanityDocument
   lesson: VideoResource
+  section?: SanityDocument
   path?: string
   muxPlayerRef: any
   onEnded?: () => Promise<any>
@@ -46,10 +51,15 @@ export const VideoProvider: React.FC<
   children,
   path = '',
   onEnded = async () => {},
+  section,
 }) => {
   const router = useRouter()
   const {subscriber} = useConvertkit()
-  const nextExercise = getNextExercise(module, lesson as Exercise)
+  const nextExercise = getNextExercise({
+    module,
+    section,
+    currentLesson: lesson as Exercise,
+  })
   const {setPlayerPrefs, playbackRate, autoplay, getPlayerPrefs} =
     usePlayerPrefs()
   const [autoPlay, setAutoPlay] = React.useState(getPlayerPrefs().autoplay)
@@ -91,6 +101,18 @@ export const VideoProvider: React.FC<
       muxPlayerRef.current.autoplay = autoplay
     }
   }, [subscriber, muxPlayerRef, playbackRate, autoPlay, video])
+
+  const {data: userSession} = useSession()
+
+  const ability = getCurrentAbility({
+    user: userSession?.user,
+    subscriber,
+    module,
+    lesson,
+    section,
+  })
+
+  const canShowVideo = ability.can('view', 'Content')
 
   const context = {
     muxPlayerProps: {
@@ -143,6 +165,8 @@ export const VideoProvider: React.FC<
     module,
     video,
     path,
+    canShowVideo,
+    ability,
   }
   return (
     <VideoContext.Provider value={context}>{children}</VideoContext.Provider>
