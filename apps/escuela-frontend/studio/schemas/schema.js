4e35586569a40c5e6476cb4e3f9bbd1c1c43591d
Lauro <hey@laurosilva.com>
2022-11-09 10:54:19 -0800

    adding collaborator to schema

--- apps/escuela-frontend/studio/schemas/schema.js
+++ apps/escuela-frontend/studio/schemas/schema.js
@@ -60,6 +60,7 @@ export default createSchema({
     muxVideo,
     stackblitz,
     castingwordsTranscript,
+    collaborator,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
   ]),
