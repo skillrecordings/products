f24350b8d97b054ca335ffd7b01bccfa3abca89b
Nicoll <nicoll.g4@gmail.com>
2022-11-17 12:05:45 -0600

    fixing footer

--- apps/typescriptcourse/src/components/bio.tsx
+++ apps/typescriptcourse/src/components/bio.tsx
@@ -3,27 +3,35 @@ import Image from 'next/image'
 
 const Bio = () => {
   return (
-    <div className="flex items-start gap-6 max-w-2xl rounded-t-lg">
-      <div className="flex-shrink-0 border-2 border-white border-opacity-90 flex items-center justify-center rounded-full overflow-hidden">
-        <Image
-          src={require('../../public/images/joe-previte.jpeg')}
-          alt="Joe Previte"
-          quality={100}
-          width={90}
-          height={90}
-          loading="eager"
-        />
+    <footer className="flex flex-col items-center justify-center w-full pt-16 pb-40 bg-black/30">
+      <div className="flex flex-col items-center justify-center w-full max-w-screen-lg px-5 md:flex-row  md:items-start lg:px-0 md:px-10">
+        <div className="pt-16 md:pt-0">
+          <div className="flex items-start gap-6 max-w-2xl rounded-t-lg">
+            <div className="flex-shrink-0 border-2 border-white border-opacity-90 flex items-center justify-center rounded-full overflow-hidden">
+              <Image
+                src={require('../../public/images/joe-previte.jpeg')}
+                alt="Joe Previte"
+                quality={100}
+                width={90}
+                height={90}
+                loading="eager"
+              />
+            </div>
+            <div className="pt-1 flex flex-col gap-5 opacity-80 sm:text-lg text-base leading-relaxed">
+              <p>
+                Hi! I'm Joe, and I will be your TypeScript Course instructor.
+              </p>
+              <p>
+                I’m an Open Source TypeScript Engineer with a passion for
+                teaching and learning. I help developers learn faster through
+                interactive courses, and in my free time, I get people excited
+                about webdev and indie hacking.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="pt-1 flex flex-col gap-5 opacity-80 sm:text-lg text-base leading-relaxed">
-        <p>Hi! I'm Joe, and I will be your TypeScript Course instructor.</p>
-        <p>
-          I’m an Open Source TypeScript Engineer with a passion for teaching and
-          learning. I help developers learn faster through interactive courses,
-          and in my free time, I get people excited about webdev and indie
-          hacking.
-        </p>
-      </div>
-     </div>
+    </footer>
   )
 }
 
