b266424eaf4562ab2abbbf143078fdeba472cc1b
Joel Hooks <joelhooks@gmail.com>
2022-12-01 10:56:35 -0800

    feat(tauto): hello 👋

---
+++ apps/transcript-automation/src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {AppProps} from 'next/app'
+import '../styles/globals.css'
+import 'focus-visible'
+import {ConvertkitProvider} from '@skillrecordings/convertkit'
+import {usePageview} from '@skillrecordings/analytics'
+import {initNProgress} from '@skillrecordings/react'
+import {DefaultSeo} from '@skillrecordings/next-seo'
+
+import config from '../config'
+
+initNProgress()
+
+function MyApp({Component, pageProps}: AppProps) {
+  usePageview()
+  return (
+    <>
+      <DefaultSeo {...config} />
+      <ConvertkitProvider>
+        <Component {...pageProps} />
+      </ConvertkitProvider>
+    </>
+  )
+}
+
+export default MyApp
