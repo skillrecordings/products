ccdc60ce1fbaa9406a77e7744604695b121a18e6
Joel Hooks <joelhooks@gmail.com>
2022-12-06 11:40:02 -0800

    feat(tt): adds pricing tiers to root page

--- apps/total-typescript/src/templates/home-template.tsx
+++ apps/total-typescript/src/templates/home-template.tsx
@@ -5,11 +5,33 @@ import {SubscribeToNewsletter} from 'components/home/home-newsletter-cta'
 import React from 'react'
 import {useSkillLevel} from 'components/home/use-skill-level'
 import {useCoupon} from 'path-to-purchase-react/use-coupon'
-import {CouponForCode} from '@skillrecordings/commerce-server/dist/@types'
+import {
+  CommerceProps,
+  CouponForCode,
+} from '@skillrecordings/commerce-server/dist/@types'
+import {Element} from 'react-scroll'
+import {PricingTiers} from '../path-to-purchase-react/product-tiers'
+import Image from 'next/image'
+import {
+  redirectUrlBuilder,
+  SubscribeToConvertkitForm,
+} from '@skillrecordings/convertkit'
+import {setUserId} from '@amplitude/analytics-browser'
+import {track} from '../utils/analytics'
+import {MDXComponents} from '../components/mdx'
 
 export const HomeTemplate: React.FC<
-  React.PropsWithChildren<{couponFromCode?: CouponForCode; level?: string}>
-> = ({level, couponFromCode}) => {
+  React.PropsWithChildren<CommerceProps & {level?: string}>
+> = ({
+  level,
+  couponFromCode,
+  purchases = [],
+  userId,
+  products,
+  couponIdFromCoupon,
+  defaultCoupon,
+}) => {
+  console.log({products})
   const skillLevel = useSkillLevel(level)
   const {redeemableCoupon, RedeemDialogForCoupon} = useCoupon(couponFromCode)
   return (
@@ -21,11 +43,37 @@ export const HomeTemplate: React.FC<
           alt: 'Golden Ticket',
         },
       }}
+      defaultCoupon={defaultCoupon}
     >
       <Header level={skillLevel} />
       <main>
         <Copy level={skillLevel} />
-        <SubscribeToNewsletter level={skillLevel} />
+        <MDXComponents.Section
+          className="flex flex-col items-center bg-[#081021] py-40"
+          slot={
+            <Image
+              src="/assets/landing/bg-divider-7.png"
+              layout="fill"
+              className="pointer-events-none select-none object-contain"
+              objectPosition="top center"
+              quality={100}
+            />
+          }
+        >
+          <h2 className="mx-auto max-w-[15ch] text-center font-heading text-4xl font-bold sm:text-5xl lg:text-5xl xl:text-6xl">
+            Become a TypeScript Wizard
+          </h2>
+          <div className="flex w-full flex-col items-center pt-16">
+            <Element name="buy" aria-hidden="true" />
+            <PricingTiers
+              products={products}
+              userId={userId}
+              purchases={purchases}
+              couponIdFromCoupon={couponIdFromCoupon}
+              couponFromCode={couponFromCode}
+            />
+          </div>
+        </MDXComponents.Section>
         {redeemableCoupon ? <RedeemDialogForCoupon /> : null}
       </main>
     </Layout>
