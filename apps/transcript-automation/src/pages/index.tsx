4f4225c24479ad05ccacd885822aec3ab15a6d17
Joel Hooks <joelhooks@gmail.com>
2022-12-10 10:54:46 -0800

    feat(TsA): minor copy updates

--- apps/transcript-automation/src/pages/index.tsx
+++ apps/transcript-automation/src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Layout from 'components/layout'
 import type {NextPage} from 'next'
 import {PrimaryNewsletterCta} from '../components/primary-newsletter-cta'
+import HomeCopy from 'content/home.md'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1 className="text-4xl text-primary-500 font-bold flex items-center justify-center flex-grow">
+      <h1 className="text-4xl text-primary-500 font-bold flex items-center justify-center flex-grow pt-16">
         Transcript Automation Using AI Tools 😎
       </h1>
-      <main className="w-full mx-auto">
+      <article className="prose prose-lg w-full mx-auto pt-16">
+        <HomeCopy />
+      </article>
+      <section className="w-full mx-auto">
         <PrimaryNewsletterCta />
-      </main>
+      </section>
     </Layout>
   )
 }
