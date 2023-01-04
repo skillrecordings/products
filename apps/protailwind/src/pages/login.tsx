420169d70003770add9bc03065a63c6df1f152fd
Vojta Holik <vojta@egghead.io>
2023-01-04 15:54:46 +0100

    feat(pt): style login

--- apps/protailwind/src/pages/login.tsx
+++ apps/protailwind/src/pages/login.tsx
@@ -28,12 +28,20 @@ const Login: React.FC<React.PropsWithChildren<{csrfToken: string}>> = ({
     }
   }, [router])
 
+  const title = `Log in to ${process.env.NEXT_PUBLIC_SITE_TITLE}`
+
   return (
-    <Layout footer={null} meta={{title: 'Log in to Total TypeScript'}}>
-      <div className="relative mx-auto flex w-full flex-grow flex-col items-center justify-center pb-16 pt-0 sm:p-5 md:pb-40 md:pt-16">
-        <main className="relative z-10 rounded-lg border border-gray-800 p-5 shadow-2xl shadow-black/60 sm:mx-auto sm:p-10">
-          <h1 className="font-text pt-4 text-center text-3xl font-extrabold leading-9 sm:text-4xl">
-            Log in to {process.env.NEXT_PUBLIC_SITE_TITLE}
+    <Layout footer={null} meta={{title}} className="bg-blue-600">
+      <div className="relative mx-auto flex w-full flex-grow flex-col items-center justify-center p-5 py-40">
+        <main className="relative z-10 mb-10 flex w-full max-w-lg flex-col items-center rounded-xl border border-gray-100 bg-white p-8 shadow-2xl shadow-blue-800/50 sm:mx-auto sm:p-14">
+          <div className="-mt-40 w-32">
+            <Image
+              src={require('../../public/assets/waving-corgi.svg')}
+              alt="waving corgi"
+            />
+          </div>
+          <h1 className="font-text pt-4 text-center font-heading text-3xl font-extrabold leading-9 sm:text-4xl">
+            {title}
           </h1>
           {query?.error === 'Verification' ? (
             <p className="mt-4 max-w-sm text-center sm:mx-auto sm:mt-8 sm:w-full">
@@ -49,16 +57,20 @@ const Login: React.FC<React.PropsWithChildren<{csrfToken: string}>> = ({
               if you need help.
             </p>
           ) : null}
-          <div className="pt-6 sm:mx-auto sm:w-full sm:max-w-md sm:pt-14">
-            <form method="post" action="/api/auth/signin/email">
-              <label htmlFor="email" className="block pb-1 text-base leading-5">
+          <div className="w-full pt-6 sm:mx-auto sm:max-w-md sm:pt-10">
+            <form
+              method="post"
+              action="/api/auth/signin/email"
+              className="flex flex-col items-center"
+            >
+              <label htmlFor="email" className="block w-full pb-1 leading-5">
                 Email address
               </label>
               <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-              <div className="relative mt-1 rounded-md shadow-sm">
+              <div className="relative w-full">
                 <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                   <svg
-                    className="h-5 w-5 text-black/50"
+                    className="h-5 w-5 text-blue-500"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -73,11 +85,11 @@ const Login: React.FC<React.PropsWithChildren<{csrfToken: string}>> = ({
                   type="email"
                   required={true}
                   placeholder="you@example.com"
-                  className="mb-3 block w-full rounded-md border border-white border-opacity-20  py-4 pl-10 text-white placeholder-gray-400 focus:border-cyan-300 focus:ring-0"
+                  className="block w-full rounded-lg border border-gray-300 border-opacity-20 bg-gray-100 py-3.5 pl-10 placeholder-gray-500 shadow-inner shadow-gray-200 focus:border-blue-500 focus:ring-0"
                   {...register('email', {required: true})}
                 />
               </div>
-              <button className="group group mt-5 inline-block gap-2 rounded bg-gradient-to-b from-cyan-300 to-cyan-400 py-3 pl-5 pr-8 font-medium text-black transition hover:brightness-110">
+              <button className="group group mt-5 inline-block gap-2 rounded-full bg-blue-600 py-3 px-8 font-medium text-white shadow-md ring-offset-1 transition focus-visible:ring-blue-500 hover:brightness-110">
                 Email me a login link
               </button>
             </form>
