6d3bbb70c1528f8faa708846651a7127f91f3aef
Vojta Holik <vojta@egghead.io>
2023-01-06 09:24:08 +0100

    fix(skill-lesson): global shortcuts should ignore content editable elements (#733)
    * fix(skill-lesson): shortcuts ignore editable els
    
    * feat(skill-lesson): press f to trigger fullscreen

--- packages/skill-lesson/hooks/use-global-player-shortcut.ts
+++ packages/skill-lesson/hooks/use-global-player-shortcut.ts
@@ -1,5 +1,6 @@
 import React from 'react'
 import {MuxPlayerRefAttributes} from '@mux/mux-player-react/*'
+import get from 'lodash/get'
 
 const ignoredInputs = ['input', 'select', 'button', 'textarea', 'mux-player']
 
@@ -9,9 +10,12 @@ export const useGlobalPlayerShortcuts = (muxPlayerRef: {
   const handleUserKeyPress = React.useCallback(
     (e: any) => {
       const activeElement = document.activeElement
+      const isContentEditable = get(activeElement, 'contentEditable') === 'true'
+
       if (
         activeElement &&
-        ignoredInputs.indexOf(activeElement.tagName.toLowerCase()) === -1
+        ignoredInputs.indexOf(activeElement.tagName.toLowerCase()) === -1 &&
+        !isContentEditable
       ) {
         if (muxPlayerRef.current) {
           if (e.key === ' ') {
@@ -31,6 +35,12 @@ export const useGlobalPlayerShortcuts = (muxPlayerRef: {
             muxPlayerRef.current.currentTime =
               muxPlayerRef.current.currentTime -
               (muxPlayerRef.current.forwardSeekOffset || 10)
+          }
+          if (e.key === 'f') {
+            e.preventDefault()
+            document.fullscreenElement
+              ? document.exitFullscreen()
+              : muxPlayerRef.current.requestFullscreen()
           }
         }
       }
