c9564e20f09db036163546aa919a3b712af02995
jbranchaud <jbranchaud@gmail.com>
2023-01-09 11:49:29 -0600

    feat(tjs): initial commit for create-skill-app

---
+++ apps/testing-javascript/seed_data/User.00001.sql
@@ -0,0 +1,6 @@
+INSERT INTO `User`(`id`,`name`,`email`,`emailVerified`,`image`,`roles`) VALUES
+("6705ff06-bc08-461c-93ae-883433c13abc","Joel Hooks","joel@egghead.io","2022-07-19 00:54:12.622",NULL,"SUPERADMIN"),
+("aeefc55c-313e-4fc5-9dd0-1eec5b33f425","Creeland Provinsal","cree@egghead.io","2022-06-24 21:57:07.321",NULL,"ADMIN"),
+("b08f5ceb-dc7f-4e23-bad8-c63f08ebce67","Nicoll Guarnizo","nicoll@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN"),
+("7eaebcea-2479-4c54-b9d1-c8c99dc3e1ce","Taylor Bell","taylor@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN"),
+("1a101306-6d51-4551-955f-19226bf88dc5","Vojta Holik","vojta@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN")
