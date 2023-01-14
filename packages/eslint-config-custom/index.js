13b587bb0297fb124715a523897a3966003c9977
Joel Hooks <joelhooks@gmail.com>
2023-01-13 17:01:15 -0800

    next 13 upgrade, eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,8 +1,18 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
-  extends: ['react-app', 'prettier'],
+  extends: ['prettier', 'next/core-web-vitals'],
+  ignorePatterns: ['node_modules', 'dist'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
   },
 }
