3ae585ba345db36c75df8d2391ace3306d3f6b88
Vojta Holik <vojta@egghead.io>
2023-01-18 18:26:58 +0100

    feat(pt): make sandpack read from custom css file

--- apps/protailwind/src/video/exercise/sandpack/parcel/app.js
+++ apps/protailwind/src/video/exercise/sandpack/parcel/app.js
@@ -1,6 +1,12 @@
 import fs from 'fs'
 const sourceCode = fs.readFileSync(__dirname + '/lesson.html', 'utf8')
+const tailwindcss = fs.readFileSync(__dirname + '/tailwind.css', 'utf8')
 import './tailwind.config'
+
+document.head.insertAdjacentHTML(
+  'beforeend',
+  `<style type="text/tailwindcss">${tailwindcss}</style>`,
+)
 
 const app = document.createElement('div')
 app.innerHTML = sourceCode
