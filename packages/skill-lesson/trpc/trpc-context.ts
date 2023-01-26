7bfdeef9a2e0539cfeaee9068520ea3e8cb2cc70
Joel Hooks <joelhooks@gmail.com>
2023-01-25 21:56:24 -0800

    fixes build and types
    made sure that the library versions were all aligned and explicitly set a type for the trpc context in skill-lesson
    
    I created an entire type for the `createContext` but to appease the type checker it actually only required that `Session` was imported in trpc-context.ts
    
    This is beyond my understanding but it often works for this type of error along with aligning versions

--- packages/skill-lesson/trpc/trpc-context.ts
+++ packages/skill-lesson/trpc/trpc-context.ts
@@ -1,7 +1,18 @@
 import {PrismaClient} from '@skillrecordings/database'
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
+import {NextApiRequest, NextApiResponse} from 'next'
+import {Session} from 'next-auth'
 import {getSession} from 'next-auth/react'
+
+type SkillTrpcCreateContext = (
+  options: trpcNext.CreateNextContextOptions,
+) => Promise<{
+  req: NextApiRequest
+  res: NextApiResponse
+  session: Session | null
+  prisma: PrismaClient<{log: ('query' | 'warn' | 'error')[]}, never, false>
+}>
 
 const prisma = new PrismaClient({
   log:
@@ -13,12 +24,11 @@ const prisma = new PrismaClient({
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = async ({
+export const createContext: SkillTrpcCreateContext = async ({
   req,
   res,
 }: trpcNext.CreateNextContextOptions) => {
   const session = await getSession({req})
-  console.debug('createContext for', session?.user?.name ?? 'unknown user')
   return {
     req,
     res,
