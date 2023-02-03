756187f89db1e7de155f5354d8aefe4f65747082
Joel Hooks <joelhooks@gmail.com>
2023-02-03 12:45:46 -0800

    fix: all the missing things

--- packages/create-skill-app/templates/next/src/pages/_app.tsx
+++ packages/create-skill-app/templates/next/src/pages/_app.tsx
@@ -2,25 +2,62 @@ import React from 'react'
 import {AppProps} from 'next/app'
 import '../styles/globals.css'
 import 'focus-visible'
-import {ConvertkitProvider} from '@skillrecordings/convertkit-react-ui'
+import {ConvertkitProvider} from '@skillrecordings/skill-lesson/hooks/use-convertkit'
 import {usePageview} from '@skillrecordings/analytics'
 import {initNProgress} from '@skillrecordings/react'
 import {DefaultSeo} from '@skillrecordings/next-seo'
-
+import {MDXProvider} from '@mdx-js/react'
+import {MDXComponents} from 'components/mdx'
+import {SessionProvider} from 'next-auth/react'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import * as amplitude from '@amplitude/analytics-browser'
+import {FeedbackProvider} from 'feedback-widget/feedback-context'
 import config from '../config'
 
 import {trpc} from 'trpc/trpc.client'
+import Script from 'next/script'
+import {Session} from 'next-auth'
 
-initNProgress()
+const queryClient = new QueryClient()
 
-function MyApp({Component, pageProps}: AppProps) {
+if (process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY) {
+  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY)
+}
+
+function MyApp({Component, pageProps}: AppProps<{session: Session}>) {
   usePageview()
+  initNProgress()
   return (
     <>
       <DefaultSeo {...config} />
-      <ConvertkitProvider>
-        <Component {...pageProps} />
-      </ConvertkitProvider>
+      <FeedbackProvider>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <QueryClientProvider client={queryClient}>
+            <ConvertkitProvider>
+              <MDXProvider components={MDXComponents}>
+                <Component {...pageProps} />
+              </MDXProvider>
+            </ConvertkitProvider>
+          </QueryClientProvider>
+        </SessionProvider>
+      </FeedbackProvider>
+      {process.env.NODE_ENV !== 'development' && (
+        <>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          ></Script>
+          <Script id="google-inline">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+          </Script>
+        </>
+      )}
     </>
   )
 }
