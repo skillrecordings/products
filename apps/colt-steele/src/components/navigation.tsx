05ed910741b3761024219f223f62ac6415e04d6d
Vojta Holik <vojta@egghead.io>
2023-03-13 11:06:20 +0100

    feat(cs): landing page design (#842)

---
+++ apps/colt-steele/src/components/navigation.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const Navigation = () => {
+  return (
+    <nav aria-label="top" className="p-14 absolute z-50 w-full left-0 top-0">
+      <Link href="/" className="inline-flex flex-col">
+        <div className="text-2xl font-semibold">Colt Steele</div>
+        <div className="font-mono text-xs opacity-60 -translate-y-1">
+          Digital Garden
+        </div>
+      </Link>
+    </nav>
+  )
+}
+
+export default Navigation
--- apps/colt-steele/tailwind.config.js
+++ apps/colt-steele/tailwind.config.js
@@ -15,8 +15,17 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [...defaultTheme.fontFamily.sans],
+        serif: ['Marbach', ...defaultTheme.fontFamily.serif],
+        heading: ['Coranto Headline', ...defaultTheme.fontFamily.serif],
       },
-      colors: {},
+      colors: {
+        brand: {
+          red: '#DC6D53',
+          bone: '#E0DEC5',
+          cultured: '#F8F7F3',
+          cola: '#383529',
+        },
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {},
