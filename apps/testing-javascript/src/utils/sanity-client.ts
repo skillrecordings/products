8ec5f0d6379a4a5ed4722cfd6c530f59a99887d2
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-03-22 23:36:26 -0700

    add sanity api token to client config

--- apps/testing-javascript/src/utils/sanity-client.ts
+++ apps/testing-javascript/src/utils/sanity-client.ts
@@ -2,8 +2,9 @@ import client from '@sanity/client'
 import type {SanityClient} from '@sanity/client'
 
 export const sanityClient: SanityClient = client({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  useCdn: true, // `false` if you want to ensure fresh data
-  apiVersion: process.env.SANITY_API_VERSION,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: false, // `false` if you want to ensure fresh data
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  token: process.env.SANITY_API_TOKEN,
 })
