a39cdf3cbacd34e33de7970712e8cacccca69676
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-03-24 16:50:50 -0700

    workshop template

--- apps/testing-javascript/src/pages/workshops/index.tsx
+++ apps/testing-javascript/src/pages/workshops/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {isEmpty} from 'lodash'
+import {PortableText} from '@portabletext/react'
 
-import Layout from 'components/layout'
 import {getProductById} from '../../lib/resources'
+import Layout from 'components/layout'
+import PortableTextComponents from 'components/portable-text'
 
 export async function getStaticProps() {
-  const product = await getProductById('product-273900')
+  const product = await getProductById('product-273899')
   return {
     props: {product},
     revalidate: 10,
@@ -18,40 +20,62 @@ const WorkshopsIndex: React.FC<{product: any}> = ({product}) => {
   return (
     <Layout>
       <main className="relative z-10 flex flex-col items-center justify-center py-20">
-        <h1 className="text-4xl mb-8 text-primary-500 font-bold">Workshops:</h1>
-        {!isEmpty(product[0]) && (
-          <div className="space-y-8">
-            {product[0].modules.map((module: any) => {
-              return (
-                <div
-                  key={module.slug}
-                  className="flex flex-col md:flex-row border border-gray-400 p-4"
-                >
-                  <div>
-                    <Image
-                      src={module.image.url}
-                      width={150}
-                      height={150}
-                      alt={module.title}
-                    />
+        <div className="container">
+          <h2 className="text-4xl mb-4 text-primary-500 font-bold">
+            Product: {product[0].title}
+          </h2>
+          <h3 className="mb-20 text-xl">{product[0].description}</h3>
+          <h2 className="text-4xl mb-8 text-primary-500 font-bold">
+            Workshops:
+          </h2>
+          {!isEmpty(product[0]) && (
+            <div className="space-y-8">
+              {product[0].modules.map((module: any) => {
+                console.log({module})
+                return (
+                  <div
+                    key={module.slug}
+                    className="flex flex-col md:flex-row border border-gray-400 p-4 space-x-4"
+                  >
+                    <div className="shrink-0">
+                      <Image
+                        src={module.image.url}
+                        width={150}
+                        height={150}
+                        alt={module.title}
+                      />
+                    </div>
+                    <div className="space-y-3">
+                      <h3 className="font-bold text-xl">
+                        <Link
+                          href={{
+                            pathname: '/workshops/[workshop]',
+                            query: {
+                              workshop: module.slug,
+                            },
+                          }}
+                          className="hover:underline"
+                        >
+                          {module.title}
+                        </Link>
+                      </h3>
+                      <div className="text-gray-600 font-mono text-sm">
+                        {module?.lessons.length} lessons
+                      </div>
+                      <div>{module.description}</div>
+                      <article className="prose w-full max-w-none pb-10 text-gray-900 lg:max-w-xl">
+                        <PortableText
+                          value={module.body}
+                          components={PortableTextComponents}
+                        />
+                      </article>
+                    </div>
                   </div>
-                  <div>
-                    <Link
-                      href={{
-                        pathname: '/workshops/[workshop]',
-                        query: {
-                          workshop: module.slug,
-                        },
-                      }}
-                    >
-                      <h3 className="font-bold text-xl">{module.title}</h3>
-                    </Link>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        )}
+                )
+              })}
+            </div>
+          )}
+        </div>
       </main>
     </Layout>
   )
--- apps/testing-javascript/src/templates/workshop-template.tsx
+++ apps/testing-javascript/src/templates/workshop-template.tsx
@@ -1,19 +1,46 @@
 import * as React from 'react'
+import Link from 'next/link'
+import {PortableText} from '@portabletext/react'
+
+import Layout from 'components/layout'
+import PortableTextComponents from 'components/portable-text'
 
 const WorkshopTemplate: React.FC<any> = ({workshop}) => {
   console.log({workshop})
-  // const router = useRouter()
-  // const {data: commerceProps} = trpc.pricing.propsForCommerce.useQuery(
-  //   router.query,
-  // )
-  // TODO: Load subscriber, find user via Prisma/api using USER_ID_QUERY_PARAM_KEY
   return (
-    // <ModuleProgressProvider moduleSlug={workshop.slug.current}>
-    //   <WorkshopTemplate workshop={workshop} commerceProps={commerceProps} />
-    // </ModuleProgressProvider>
-    <div>
-      <div>Workshop {workshop.title}</div>
-    </div>
+    <Layout>
+      <main className="relative z-10 flex flex-col items-center justify-center py-20">
+        <h1 className="text-4xl mb-4 text-primary-500 font-bold">
+          {workshop.title}
+        </h1>
+        <div className="mb-4">{workshop.lessons.length} lessons</div>
+        <article className="prose w-full max-w-none pb-10 text-gray-900 lg:max-w-xl mb-4">
+          <PortableText
+            value={workshop.body}
+            components={PortableTextComponents}
+          />
+        </article>
+        <ul className="list list-decimal">
+          {workshop.lessons.map((lesson: any) => {
+            return (
+              <li key={lesson.id}>
+                <Link
+                  href={{
+                    pathname: `/workshops/[workshop]/[lesson]`,
+                    query: {
+                      workshop: workshop.slug,
+                      lesson: lesson.slug,
+                    },
+                  }}
+                >
+                  {lesson.title}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+    </Layout>
   )
 }
 
