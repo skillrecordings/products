eccd660fc29e6a125f4af42bf71f8352a29a3613
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-03-24 17:54:24 -0700

    initial lesson template and page

--- apps/testing-javascript/src/pages/workshops/[module]/[lesson].tsx
+++ apps/testing-javascript/src/pages/workshops/[module]/[lesson].tsx
@@ -1,11 +1,75 @@
-import * as React from 'react'
+import React from 'react'
+import {GetStaticPaths, GetStaticProps} from 'next'
+// import {getAllTutorials, getTutorial} from 'lib/tutorials'
+// import {getExercise} from 'lib/exercises'
+// import path from 'path'
+// import {walk} from 'utils/code-editor-content'
+import {LessonProvider} from '@skillrecordings/skill-lesson/hooks/use-lesson'
+import {VideoResourceProvider} from '@skillrecordings/skill-lesson/hooks/use-video-resource'
+import {
+  getAllWorkshops,
+  getLessonBySlug,
+  getWorkshopBySlug,
+} from '../../../lib/resources'
+// import {getSection} from '@skillrecordings/skill-lesson/lib/sections'
+import {ModuleProgressProvider} from 'utils/module-progress'
 
-const LessonIndex: React.FC<any> = () => {
+import LessonTemplate from 'templates/lesson-template'
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const lessonSlug = params?.lesson as string
+  const module = await getWorkshopBySlug(params?.module as string)
+  const lesson = await getLessonBySlug(lessonSlug)
+
+  return {
+    props: {
+      lesson,
+      module,
+      videoResourceId: lesson.videoResourceId,
+      transcript: lesson.transcript,
+    },
+    revalidate: 10,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  const workshops = await getAllWorkshops()
+
+  // flatMap to extract lessons in sections from workshops
+  const paths = workshops.flatMap((workshop: any) => {
+    return (
+      workshop.lessons?.map((lesson: any) => ({
+        params: {
+          module: workshop.slug,
+          lesson: lesson.slug,
+        },
+      })) || []
+    )
+  })
+
+  return {paths, fallback: 'blocking'}
+}
+
+const LessonPage: React.FC<any> = ({
+  lesson,
+  module,
+  videoResourceId,
+  transcript,
+}) => {
+  console.log({videoResourceId})
   return (
-    <div>
-      <div>LessonIndex</div>
-    </div>
+    <ModuleProgressProvider moduleSlug={module.slug.current}>
+      <LessonProvider lesson={lesson} module={module}>
+        <VideoResourceProvider videoResourceId={videoResourceId}>
+          <LessonTemplate
+            transcript={transcript}
+            lesson={lesson}
+            module={module}
+          />
+        </VideoResourceProvider>
+      </LessonProvider>
+    </ModuleProgressProvider>
   )
 }
 
-export default LessonIndex
+export default LessonPage
--- apps/testing-javascript/src/templates/lesson-template.tsx
+++ apps/testing-javascript/src/templates/lesson-template.tsx
@@ -1,1 +1,26 @@
 import * as React from 'react'
+import Link from 'next/link'
+import {PortableText} from '@portabletext/react'
+
+import Layout from 'components/layout'
+import PortableTextComponents from 'components/portable-text'
+
+const WorkshopTemplate: React.FC<any> = ({lesson, module, transcript}) => {
+  return (
+    <Layout>
+      <main className="relative z-10 flex flex-col items-center justify-center py-20">
+        <h1 className="text-4xl mb-4 text-primary-500 font-bold">
+          {lesson.title}
+        </h1>
+        <article className="prose w-full max-w-none pb-10 text-gray-900 lg:max-w-xl mb-4">
+          <PortableText
+            value={lesson.body}
+            components={PortableTextComponents}
+          />
+        </article>
+      </main>
+    </Layout>
+  )
+}
+
+export default WorkshopTemplate
