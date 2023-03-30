9adfd8c462e0fca428f14a76d69317935b4444ce
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-03-29 18:56:51 -0700

    update env vars

--- apps/testing-javascript/src/utils/sanity-client.ts
+++ apps/testing-javascript/src/utils/sanity-client.ts
@@ -3,7 +3,7 @@ import type {SanityClient} from '@sanity/client'
 
 export const sanityClient: SanityClient = client({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET_ID,
   useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   token: process.env.SANITY_API_TOKEN,
