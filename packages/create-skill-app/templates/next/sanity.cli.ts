05b64d397cd470a3ccc34a2c68e2a67e967ea1ba
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2023-04-05 08:40:38 -0700

    make env vars names consistent (#860)
    Co-authored-by: Joel Hooks <joelhooks@gmail.com>

--- packages/create-skill-app/templates/next/sanity.cli.ts
+++ packages/create-skill-app/templates/next/sanity.cli.ts
@@ -1,7 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET_ID
 
 export default defineCliConfig({
   api: {
