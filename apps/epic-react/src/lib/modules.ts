f4f70ec39a4a07866ea3937456ef069ff52467ab
Joel Hooks <joelhooks@gmail.com>
2023-04-05 09:20:38 -0700

    quick sketch of routing structure and data loading
    https://www.loom.com/share/706ef6f2a8704c499e360008355bd37c

---
+++ apps/epic-react/src/lib/modules.ts
@@ -0,0 +1,105 @@
+import groq from 'groq'
+import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
+
+const modulesQuery = groq`*[_type == "module" && moduleType == 'workshop'] | order(_createdAt desc) {
+  _id,
+  _type,
+  title,
+  slug,
+  "image": image.asset->url,
+  _updatedAt,
+  _createdAt,
+  description,
+  state,
+  "sections": resources[@->._type == 'section']->{
+    _id,
+    _type,
+    _updatedAt,
+    title,
+    description,
+    "slug": slug.current,
+    "lessons": resources[@->._type in ['exercise', 'explainer']]->{
+      _id,
+      _type,
+      _updatedAt,
+      title,
+      description,
+      "slug": slug.current,
+      "solution": resources[@._type == 'solution'][0]{
+        _key,
+        _type,
+        "_updatedAt": ^._updatedAt,
+        title,
+        description,
+        "slug": slug.current,
+      }
+    },
+    "resources": resources[@->._type in ['linkResource']]->
+  }
+}`
+
+export const getAllModules = async () => await sanityClient.fetch(modulesQuery)
+
+export const getModule = async (slug: string) =>
+  await sanityClient.fetch(
+    groq`*[_type == "module" && moduleType == 'workshop' && slug.current == $slug][0]{
+        "id": _id,
+        _type,
+        title,
+        state,
+        slug,
+        body[]{
+          ...,
+          _type == "bodyTestimonial" => {
+            "body": testimonial->body,
+            "author": testimonial->author {
+              "image": image.asset->url,
+              name
+            }
+          }
+        },
+        moduleType,
+        _id,
+        github,
+        ogImage,
+        description,
+        _updatedAt,
+        "testimonials": resources[@->._type == 'testimonial']->{
+          _id,
+          _type,
+          _updatedAt,
+          body,
+          author {
+            name,
+            "image": image.asset->url
+          }
+        },
+        "sections": resources[@->._type == 'section']->{
+          _id,
+          _type,
+          _updatedAt,
+          title,
+          description,
+          "slug": slug.current,
+          "lessons": resources[@->._type in ['exercise', 'explainer']]->{
+            _id,
+            _type,
+            _updatedAt,
+            title,
+            description,
+            "slug": slug.current,
+            "solution": resources[@._type == 'solution'][0]{
+              _key,
+              _type,
+              "_updatedAt": ^._updatedAt,
+              title,
+              description,
+              "slug": slug.current,
+            }
+          },
+          "resources": resources[@->._type in ['linkResource']]->
+        },
+        "image": image.asset->url
+    }`,
+    {slug: `${slug}`},
+  )
