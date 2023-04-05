4698f183c64a782cd31b037a80c2bb7a6cf2d07a
Joel Hooks <joelhooks@gmail.com>
2023-04-04 20:02:08 -0700

    playlist and lesson routes for testing javascript

---
+++ apps/testing-javascript/src/lib/lessons.ts
@@ -0,0 +1,67 @@
+import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
+import groq from 'groq'
+import z from 'zod'
+import {ResourceSchema} from '@skillrecordings/skill-lesson/schemas/resource'
+
+export const getLesson = async (
+  slug: string,
+  includeMedia: boolean = true,
+): Promise<any> => {
+  const exercise = await sanityClient.fetch(
+    `*[_type in ['exercise', 'explainer', 'interview'] && slug.current == $slug][0]{
+      _id,
+      _type,
+      _updatedAt,
+      title,
+      description,
+      "slug": slug.current,
+      body,
+      "stackblitz": resources[@._type == 'stackblitz'][0].openFile,
+      "videoResourceId": resources[@->._type == 'videoResource'][0]->_id,
+      "transcript": resources[@->._type == 'videoResource'][0]-> castingwords.transcript,
+      "solution": resources[@._type == 'solution'][0]{
+        _key,
+        _type,
+        "_updatedAt": ^._updatedAt,
+        title,
+        description,
+        body,
+        "stackblitz": resources[@._type == 'stackblitz'][0].openFile,
+        "videoResourceId": resources[@->._type == 'videoResource'][0]->_id,
+        "transcript": resources[@->._type == 'videoResource'][0]-> castingwords.transcript,
+        "slug": slug.current,
+      }
+    }`,
+    {slug},
+  )
+
+  return exercise
+}
+
+export const getAllLessons = async (): Promise<any[]> => {
+  const lessons =
+    await sanityClient.fetch(groq`*[_type in ['exercise', 'explainer', 'interview']]{
+      _id,
+      _type,
+      _updatedAt,
+      title,
+      description,
+      body,
+      "slug": slug.current,
+      "stackblitz": resources[@._type == 'stackblitz'][0].openFile,
+      "videoResourceId": resources[@->._type == 'videoResource'][0],
+      "solution": resources[@._type == 'solution'][0]{
+        _key,
+        _type,
+        _updatedAt,
+        title,
+        description,
+        body,
+        "stackblitz": resources[@._type == 'stackblitz'][0].openFile,
+        "videoResourceId": resources[@->._type == 'videoResource'][0],
+       "slug": slug.current
+       }
+    }`)
+
+  return lessons
+}
