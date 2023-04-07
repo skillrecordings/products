e077a358c3341d7e4f59f0680cd63f12a8d782f2
Nicoll <nicoll.g4@gmail.com>
2023-04-07 15:16:56 -0500

    quick copy fix

--- apps/epic-web/studio/schemas/objects/github-repo.js
+++ apps/epic-web/studio/schemas/objects/github-repo.js
@@ -11,7 +11,7 @@ export default {
       name: 'repo',
       title: 'Repository',
       type: 'string',
-      description: 'without https://github.com/pro-tailwind/',
+      description: 'without https://github.com/epicweb-dev/',
     },
   ],
 }
