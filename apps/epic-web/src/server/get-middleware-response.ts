86aabc74033278a4ddff6ec6f876f9ea74599b83
Joel Hooks <joelhooks@gmail.com>
2023-04-06 18:57:42 -0700

    add an admin video uploader

--- apps/epic-web/src/server/get-middleware-response.ts
+++ apps/epic-web/src/server/get-middleware-response.ts
@@ -1,36 +1,32 @@
 import {rewriteToPath} from './rewrite-next-response-to-path'
 import {NextRequest, NextResponse} from 'next/server'
+
+import {getToken} from 'next-auth/jwt'
 import {
-  getCookiesForRequest,
-  setCookiesForResponse,
-} from './process-customer-cookies'
+  getCurrentAbility,
+  UserSchema,
+} from '@skillrecordings/skill-lesson/utils/ability'
 
 export const SITE_ROOT_PATH = '/'
 
-/**
- * with this approach, logged in users can be shown
- * '/' and anon users '/signup' IN PLACE
- *
- * This looks a lot like the i18n example:
- * https://github.com/vercel/examples/tree/main/edge-functions/i18n
- *
- * an the split testing example that puts them in a bucket:
- * https://github.com/vercel/examples/edge-functions/ab-testing-simple
- *
- */
 export async function getMiddlewareResponse(req: NextRequest) {
   let response = NextResponse.next()
-  const subscriber = await getCookiesForRequest(req)
+  const token = await getToken({req})
 
-  if (subscriber && req.nextUrl.pathname === SITE_ROOT_PATH) {
-    switch (true) {
-      case Boolean(subscriber.fields?.level):
-        response = rewriteToPath(`/home/level/${subscriber.fields?.level}`, req)
-        break
+  if (
+    req.nextUrl.pathname.includes('/admin') ||
+    req.nextUrl.pathname.includes('/tips')
+  ) {
+    try {
+      const user = UserSchema.parse(token)
+      const ability = getCurrentAbility({user})
+      if (!ability.can('create', 'Content')) {
+        response = rewriteToPath('/login', req)
+      }
+    } catch (error) {
+      response = rewriteToPath('/login', req)
     }
   }
-
-  response = setCookiesForResponse(response, subscriber)
 
   return response
 }
