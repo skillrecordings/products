711db0fdce9311a606749f0646fbb41874b46c5d
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-04-07 20:48:18 -0700

    flex-grow --> grow

--- apps/testing-javascript/src/pages/confirmed.tsx
+++ apps/testing-javascript/src/pages/confirmed.tsx
@@ -5,7 +5,7 @@ import {Signature} from './confirm'
 const ConfirmedSubscriptionPage = () => {
   return (
     <Layout>
-      <main className="flex-grow flex items-center justify-center flex-col px-5">
+      <main className="grow flex items-center justify-center flex-col px-5">
         <Image />
         <div className="max-w-lg text-center font-light">
           <h1 className="font-bold lg:text-5xl text-4xl py-8 font-heading">
