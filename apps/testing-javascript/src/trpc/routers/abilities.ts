e94790ba6bed03cfb4e664a06b1a6b9c72375f0e
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-04-10 14:25:32 -0700

    ModuleProgress (not functioning yet)

--- apps/testing-javascript/src/trpc/routers/abilities.ts
+++ apps/testing-javascript/src/trpc/routers/abilities.ts
@@ -8,7 +8,7 @@ import {getSubscriberFromCookie} from '@skillrecordings/skill-lesson/utils/ck-su
 import {convertkitSetSubscriberCookie} from '@skillrecordings/skill-lesson/utils/ck-set-subscriber-cookie'
 import {publicProcedure, router} from '@skillrecordings/skill-lesson'
 
-export const abilities = router({
+export const abilitiesRouter = router({
   getAbilities: publicProcedure.query(async ({ctx, input}) => {
     const token = await getToken({req: ctx.req})
     const convertkitSubscriber = await getSubscriberFromCookie(ctx.req)
