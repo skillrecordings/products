bd971d4cbebabefa96d9ca2db90ffc67768b7c46
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-04-10 17:49:31 -0700

    add user

--- apps/testing-javascript/seed_data/User.00001.sql
+++ apps/testing-javascript/seed_data/User.00001.sql
@@ -3,4 +3,5 @@ INSERT INTO `User`(`id`,`name`,`email`,`emailVerified`,`image`,`roles`) VALUES
 ("aeefc55c-313e-4fc5-9dd0-1eec5b33f425","Creeland Provinsal","cree@egghead.io","2022-06-24 21:57:07.321",NULL,"ADMIN"),
 ("b08f5ceb-dc7f-4e23-bad8-c63f08ebce67","Nicoll Guarnizo","nicoll@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN"),
 ("7eaebcea-2479-4c54-b9d1-c8c99dc3e1ce","Taylor Bell","taylor@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN"),
-("1a101306-6d51-4551-955f-19226bf88dc5","Vojta Holik","vojta@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN")
+("1a101306-6d51-4551-955f-19226bf88dc5","Vojta Holik","vojta@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN"),
+("7de13e0e-fa7c-4704-80a0-a38580f067f0","Yevhen Nahalskyi","evgeniy@egghead.io","2022-04-12 15:51:01.388",NULL,"ADMIN")
