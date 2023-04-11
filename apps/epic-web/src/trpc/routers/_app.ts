e9d26fe52fb42e163c97adc9946a6c36d91ac57b
Joel Hooks <joelhooks@gmail.com>
2023-04-11 09:19:34 -0700

    fix build with missing packages and routers

--- apps/epic-web/src/trpc/routers/_app.ts
+++ apps/epic-web/src/trpc/routers/_app.ts
@@ -5,11 +5,13 @@ import {
 } from '@skillrecordings/skill-lesson'
 import {tipResourcesRouter} from './tip-resources'
 import {moduleProgressRouter} from './module-progress'
+import {purchasesRouter} from './purchases'
 
 export const appRouter = mergeRouters(
   router({
     tipResources: tipResourcesRouter,
     moduleProgress: moduleProgressRouter,
+    purchases: purchasesRouter,
   }),
   skillLessonRouter,
 )
