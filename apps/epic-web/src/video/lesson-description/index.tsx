073d082adb6322967df777af5a4fbe789d58f84b
Joel Hooks <joelhooks@gmail.com>
2023-04-12 00:28:35 -0700

    feat(ew): tutorial routes (#881)
    * feat(ew): tutorial routes
    
    * make changes to tutorial index so it will function
    
    * certificate is required
    
    * individual tutorial page renders
    
    * missing trpc
    * divider images always break
    
    * modules load, but not lesson in the table of contents
    
    * update the query so that it loads the correct type for project
    
    * start on exercise template with missing deps
    
    * remove extra stuff from exercise template
    
    * team and commerce bits add fully
    
    lesson page renders, but no video is present
    
    * video displays!
    
    canShowVideo is hard coded to "true" here to get the video displaying
    
    * fix build with missing packages and routers
    
    * missing image breaks build

---
+++ apps/epic-web/src/video/lesson-description/index.tsx
@@ -0,0 +1,45 @@
+import {PortableText} from '@portabletext/react'
+import {take} from 'lodash'
+import Link from 'next/link'
+import {portableTextComponents} from '@skillrecordings/skill-lesson/portable-text'
+import {useMuxPlayer} from '@skillrecordings/skill-lesson/hooks/use-mux-player'
+import {useLesson} from '@skillrecordings/skill-lesson/hooks/use-lesson'
+
+export const LessonDescription: React.FC<{
+  productName: string
+  loadingIndicator: React.ReactElement
+}> = ({productName, loadingIndicator}) => {
+  const {canShowVideo, loadingUserStatus} = useMuxPlayer()
+  const {lesson} = useLesson()
+  const {body} = lesson
+
+  const displayedBody = canShowVideo ? body : take(body, 3)
+  return (
+    <div data-lesson-description="">
+      <div data-content="" data-content-visible={canShowVideo.toString()}>
+        <PortableText
+          value={displayedBody}
+          components={portableTextComponents({loadingIndicator})}
+        />
+      </div>
+      {!canShowVideo && loadingUserStatus && (
+        <div role="status">
+          {new Array(6).fill(0).map((_, index) => (
+            <div key={index} />
+          ))}
+          <span className="sr-only">Loading {lesson._type}</span>
+        </div>
+      )}
+      {!canShowVideo && !loadingUserStatus && (
+        <div data-cta="">
+          <p>
+            This {lesson._type} is part of{' '}
+            <Link href={'/buy'}>{productName}</Link> and can be unlocked
+            immediately after purchase. Already purchased?{' '}
+            <Link href="/login">Log in here.</Link>
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
