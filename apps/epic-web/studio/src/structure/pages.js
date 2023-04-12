3d8220a9267848bcc317b02f27f194e0249a6694
Vojta Holik <vojta@egghead.io>
2023-04-12 13:36:45 +0200

    chore(ew): upgrade sanity to v3

--- apps/epic-web/studio/src/structure/pages.js
+++
@@ -1,9 +0,0 @@
-import S from '@sanity/desk-tool/structure-builder'
-import {FcHome} from 'react-icons/fc'
-
-const pages = S.listItem()
-  .title('Pages')
-  .icon(FcHome)
-  .child(S.documentTypeList('page').title('All Pages'))
-
-export default pages
