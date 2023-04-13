7c6f15d8e7de704c6acd6c428bcc60075d6bb5d8
Joel Hooks <joelhooks@gmail.com>
2023-04-13 11:06:09 -0700

    add prisma structures

---
+++ apps/scriptkit/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,46 @@
+import NextAuth, {type NextAuthOptions, Theme} from 'next-auth'
+import {withSentry} from '@sentry/nextjs'
+import {createOptions} from '@skillrecordings/skill-api'
+import {NextApiRequest, NextApiResponse} from 'next'
+import GitHubProvider from 'next-auth/providers/github'
+
+const productTheme: Theme = {
+  colorScheme: 'auto',
+  brandColor: '#10172a',
+}
+
+const providers = [
+  GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    allowDangerousEmailAccountLinking: true,
+  }),
+]
+
+export const nextAuthOptions: NextAuthOptions = createOptions({
+  theme: productTheme,
+  providers,
+})
+
+export default async function NextAuthEndpoint(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  return withSentry(
+    NextAuth(
+      req,
+      res,
+      createOptions({
+        req,
+        theme: productTheme,
+        providers,
+      }),
+    ),
+  )
+}
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+}
