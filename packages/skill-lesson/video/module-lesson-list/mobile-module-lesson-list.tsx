562fd8fcfbe0ed1352fe43823174148a6a4e24ea
Vojta Holik <vojta@egghead.io>
2023-04-18 16:31:30 +0200

    feat(skill-lesson): add video

---
+++ packages/skill-lesson/video/module-lesson-list/mobile-module-lesson-list.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import capitalize from 'lodash/capitalize'
+import Balancer from 'react-wrap-balancer'
+import ModuleLessonListHeader from './module-lesson-list-header'
+import {LessonList} from './lesson-list'
+import {Module} from 'schemas/module'
+import {Section} from 'schemas/section'
+import {Lesson} from 'schemas/lesson'
+
+export const MobileModuleLessonList: React.FC<{
+  lessonResourceRenderer: (
+    path: string,
+    module: Module,
+    lesson: Lesson,
+    section?: Section,
+  ) => void
+  module: Module
+  section?: Section
+  path: string
+}> = ({module, path, section, lessonResourceRenderer}) => {
+  const exerciseCount = section
+    ? section.lessons && section.lessons.length
+    : module.lessons && module.lessons.length
+  return (
+    <details data-mobile-module-lesson-list="">
+      <summary>
+        <Balancer>
+          {section ? section.title : module.title}{' '}
+          {section ? null : capitalize(module.moduleType)}{' '}
+        </Balancer>
+        <span data-byline="">{exerciseCount || 0} exercises</span>
+      </summary>
+      <ModuleLessonListHeader module={module} path={path}>
+        <LessonList
+          path={path}
+          lessonResourceRenderer={lessonResourceRenderer}
+        />
+      </ModuleLessonListHeader>
+    </details>
+  )
+}
---
+++ packages/skill-lesson/video/module-lesson-list/module-lesson-list-header.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/legacy/image'
+import {track} from 'utils/analytics'
+import {Module} from 'schemas/module'
+import pluralize from 'pluralize'
+
+type SidebarProps = {
+  module: Module
+  path: string
+  children?: React.ReactNode
+}
+const ModuleLessonListHeader: React.FC<SidebarProps> = ({
+  module,
+  path = '',
+  children,
+}) => {
+  return (
+    <>
+      <div data-module-lesson-list-header="">
+        <div data-sticky-container="">
+          <aside>
+            <div data-content-wrapper="">
+              <div data-content="">
+                {module?.image && (
+                  <Image
+                    src={module.image}
+                    width={120}
+                    height={120}
+                    alt={module.title}
+                    quality={100}
+                    data-image=""
+                  />
+                )}
+                <div data-title="">
+                  <Link
+                    href={`/${pluralize(module.moduleType)}`}
+                    data-type=""
+                    onClick={() => {
+                      track(
+                        `clicked return to ${pluralize(module.moduleType)}`,
+                        {
+                          module: module.slug.current,
+                        },
+                      )
+                    }}
+                  >
+                    {pluralize(module.moduleType)}
+                  </Link>
+                  <span>/</span>
+                  <h2>
+                    <Link
+                      href={{
+                        pathname: `${path}/[module]`,
+                        query: {module: module.slug.current},
+                      }}
+                      passHref
+                      onClick={() => {
+                        track('clicked return to module', {
+                          module: module.slug.current,
+                        })
+                      }}
+                    >
+                      {module.title}
+                    </Link>
+                  </h2>
+                </div>
+              </div>
+            </div>
+            {children}
+            <div data-fadeout="" />
+          </aside>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ModuleLessonListHeader
