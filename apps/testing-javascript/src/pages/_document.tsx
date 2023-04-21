67c2c4fca8724dcdf76d0a83b10e8c73a215ac7b
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-04-20 19:07:48 -0700

    woff --> woff2

--- apps/testing-javascript/src/pages/_document.tsx
+++ apps/testing-javascript/src/pages/_document.tsx
@@ -7,28 +7,28 @@ export default function Document() {
       <Head>
         <link
           rel="preload"
-          href="/fonts/499f3b52-6c48-40e5-9a4a-7d76f57bc82d.woff"
+          href="/fonts/c0fdf756-27c7-46b6-ade5-d04ede3fbf9b.woff2"
           as="font"
           type="font/woff"
           crossOrigin=""
         />
         <link
           rel="preload"
-          href="/fonts/941bd4ef-6d96-4cc3-b891-b967fb693919.woff"
+          href="/fonts/56184c9a-0155-4961-a836-516c2a37949d.woff2"
           as="font"
           type="font/woff"
           crossOrigin=""
         />
         <link
           rel="preload"
-          href="/fonts/5df2ffcf-6190-4c99-adea-e678cb3fcad8.woff"
+          href="/fonts/ad9be1db-493d-445e-aeb8-16a5cca6cde9.woff2"
           as="font"
           type="font/woff"
           crossOrigin=""
         />
         <link
           rel="preload"
-          href="/fonts/9a718670-1649-4925-a033-3a8b409e3a39.woff"
+          href="/fonts/c7e2cf7d-6add-43b2-8bd9-f68faa219cb6.woff2"
           as="font"
           type="font/woff"
           crossOrigin=""
