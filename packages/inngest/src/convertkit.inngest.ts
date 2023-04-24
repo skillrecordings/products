a21bc79b93fd946769232aaae30aa02a0b550ef4
Joel Hooks <joelhooks@gmail.com>
2023-04-24 16:24:51 -0700

    fix(tt): update inngest

--- packages/inngest/src/convertkit.inngest.ts
+++ packages/inngest/src/convertkit.inngest.ts
@@ -1,10 +1,10 @@
 import {inngest} from './inngest.server'
 
-export const convertkitSurveyAnswered = inngest.createStepFunction(
-  'ConvertKit Survey Answered',
-  'convertkit/survey-answered',
-  ({event, tools}) => {
-    tools.sleep('1 second')
+export const convertkitSurveyAnswered = inngest.createFunction(
+  {name: 'ConvertKit Survey Answered'},
+  {event: 'convertkit/survey.answered'},
+  ({event, step}) => {
+    step.sleep('1 second')
     return {event, body: 'now you can do stuff!'}
   },
 )
