99f083fba47109846e36b6fcbd0f84545e22d672
Joel Hooks <joelhooks@gmail.com>
2023-04-28 15:29:09 -0700

    get tip video resources

--- apps/total-typescript/src/pages/tips/[tip]/edit.tsx
+++ apps/total-typescript/src/pages/tips/[tip]/edit.tsx
@@ -3,20 +3,20 @@ import * as React from 'react'
 import VideoUploader from 'module-builder/video-uploader'
 import {NextPageContext} from 'next'
 import {sanityWriteClient} from '../../../utils/sanity-server'
-import {
-  EditorSelection,
-  Patch,
-  PortableTextEditor,
-} from '@sanity/portable-text-editor'
-import {Editor} from '../../../components/editor/editor'
-import {PortableTextBlock} from '@sanity/types'
-import {Subject} from 'rxjs'
+import Link from 'next/link'
+import MuxPlayer from '@mux/mux-player-react'
+import {isEmpty} from 'lodash'
+
+const SANITY_STUDIO_TIPS_BASE_URL = `https://totaltypescript.sanity.studio/desk`
 
 export async function getServerSideProps(context: NextPageContext) {
   const {query} = context
   const {tip} = query
   const tipData = await sanityWriteClient.fetch(
-    `*[_type == "tip" && slug.current == $slug][0]`,
+    `*[_type == "tip" && slug.current == $slug][0]{
+      ...,
+      "videoResources": resources[@->._type == 'videoResource']->
+    }`,
     {slug: tip},
   )
 
@@ -34,47 +34,75 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Adminpage({tip}: {tip: any}) {
-  const patches$ = React.useMemo<any>(
-    () =>
-      new Subject<{
-        patches: Patch[]
-        snapshot: PortableTextBlock[] | undefined
-      }>(),
-    [],
-  )
   console.log({tip})
-  const [selection, setSelection] = React.useState<EditorSelection | null>(null)
-  const [value, setValue] = React.useState<
-    PortableTextBlock[] | undefined | null
-  >(tip.body)
-  const editorId = tip._id
-  const handleMutation = React.useCallback(
-    (mutatingPatches: Patch[]) => {
-      console.log({mutatingPatches, editorId})
-    },
-    [editorId],
-  )
+
   return (
     <div>
       <header className="relative flex flex-col items-center justify-center overflow-hidden px-5 pt-12">
         <div className="relative z-10 flex w-full max-w-screen-lg flex-col-reverse items-center  lg:flex-row">
           <div className="relative z-10 max-w-2xl pb-10 lg:py-12 lg:pb-12">
             <h1 className="w-full max-w-[14ch] font-heading text-4xl font-normal leading-[1.25] sm:mt-0 sm:text-5xl sm:leading-[1.15] lg:text-5xl lg:leading-[1.15] xl:text-6xl xl:leading-[1.15]">
-              Admin Scratch Pages
+              {tip.title}
             </h1>
+
+            <Link
+              href={`${SANITY_STUDIO_TIPS_BASE_URL}/tips;${tip._id}`}
+              className="group mt-5 inline-block gap-2 rounded bg-gray-800 px-4 py-2 font-medium transition hover:bg-gray-700"
+            >
+              edit tip in Sanity Studio{' '}
+              <span
+                aria-hidden="true"
+                className="text-gray-300 transition group-hover:text-white"
+              >
+                →
+              </span>
+            </Link>
           </div>
         </div>
       </header>
+      <div>
+        <VideoResources tip={tip} />
+      </div>
       <div className="flex min-h-full items-center">
         <VideoUploader />
-        <Editor
-          value={value}
-          onMutation={handleMutation}
-          editorId={editorId}
-          patches$={patches$}
-          selection={selection}
-        />
       </div>
+    </div>
+  )
+}
+
+/**
+ * 1. Create a component that lists all videoResources within a tip
+ */
+const VideoResources = ({tip}: {tip: {videoResources: any[]}}) => {
+  return !isEmpty(tip.videoResources) ? (
+    <div>
+      <ul>
+        {tip.videoResources.map((videoResource: any) => {
+          console.log({videoResource})
+          return (
+            <li key={videoResource._id}>
+              <h2 className="text-2xl">{videoResource.title}</h2>
+              <MuxPlayer playbackId={videoResource.muxAsset.muxPlaybackId} />
+              <Link
+                href={`${SANITY_STUDIO_TIPS_BASE_URL}/videoResource;${videoResource._id}`}
+                className="group mt-5 inline-block gap-2 rounded bg-gray-800 px-4 py-2 font-medium transition hover:bg-gray-700"
+              >
+                edit {videoResource.title} in Sanity Studio{' '}
+                <span
+                  aria-hidden="true"
+                  className="text-gray-300 transition group-hover:text-white"
+                >
+                  →
+                </span>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div>
+      <p>No video resources yet</p>
     </div>
   )
 }
