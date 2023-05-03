ac30aebf8889b3015a01174df4fe9c2da8ad15b7
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-05-02 19:21:05 -0700

    revert changes

--- apps/testing-javascript/src/pages/thanks/purchase.tsx
+++ apps/testing-javascript/src/pages/thanks/purchase.tsx
@@ -98,6 +98,18 @@ const InlineTeamInvite = ({
         <p className="pb-2 font-semibold">
           You have purchased {seatsPurchased} seats.
         </p>
+        <p className="pb-2">
+          Invite your team to claim seats right away with this invite link.
+          Don't worry about saving this anywhere, it will always be available on
+          your{' '}
+          <a
+            className="font-semibold underline"
+            href={`${process.env.NEXT_PUBLIC_URL}/team`}
+          >
+            Team page
+          </a>{' '}
+          once you sign in.
+        </p>
         <CopyInviteLink bulkCouponId={bulkCouponId} />
       </div>
     </div>
