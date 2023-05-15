f8844b18e5a69fe24c86901265365315584f9a76
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-05-15 16:12:46 -0700

    styles and tweaks

--- apps/testing-javascript/src/components/pricing-section.tsx
+++ apps/testing-javascript/src/components/pricing-section.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 import cx from 'classnames'
 import type {CommerceProps} from '@skillrecordings/commerce-server/dist/@types'
 import {useCoupon} from '@skillrecordings/skill-lesson/path-to-purchase/use-coupon'
 import {PriceCheckProvider} from '@skillrecordings/skill-lesson/path-to-purchase/pricing-check-context'
+import Balancer from 'react-wrap-balancer'
 
 import {Pricing} from 'path-to-purchase/pricing'
 
@@ -30,18 +32,18 @@ const PricingSection: React.FC<{
 
   return (
     <div className={cx(className)}>
-      <div className="flex flex-col">
+      <div className="container max-w-6xl flex flex-col">
         <header>
-          <h2 className="px-5 text-center font-heading text-4xl sm:text-5xl lg:text-6xl">
-            Start testing like a pro
+          <h2 className="text-center font-heading text-4xl sm:text-5xl lg:text-6xl">
+            <Balancer>Start testing like a pro</Balancer>
           </h2>
-          <h3 className="text-center mb-10 mt-4 font-tt-regular text-2xl opacity-80">
+          <h3 className="text-center mb-10 mt-4 font-tt-regular text-xl sm:text-2xl opacity-80">
             Buy once. Forever yours.
           </h3>
         </header>
         <PriceCheckProvider purchasedProductsIds={purchasedProductsIds}>
           {redeemableCoupon ? <RedeemDialogForCoupon /> : null}
-          <div className="flex flex-col lg:flex-row justify-center gap-6 mt-32 items-start">
+          <div className="flex flex-col items-center lg:flex-row justify-center gap-6 mt-24 md:mt-28 lg:mt-32 lg:items-start">
             {products.map((product, i) => {
               return (
                 <Pricing
@@ -58,6 +60,14 @@ const PricingSection: React.FC<{
             })}
           </div>
         </PriceCheckProvider>
+        <div className="w-36 top-[50px] md:w-44 relative md:top-[62px] mx-auto">
+          <Image
+            src="https://res.cloudinary.com/epic-web/image/upload/v1684186307/testingjavascript.com/illos/30-day-money.back.svg"
+            width={314}
+            height={218}
+            alt="30-Day Money-Back Guarantee"
+          />
+        </div>
       </div>
     </div>
   )
