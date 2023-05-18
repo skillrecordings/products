55077abda5a4202a080357977746d5bb0f0bdf0a
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-05-17 20:45:15 -0700

    styles tweaks

--- apps/testing-javascript/src/components/playlist/playlist-item.tsx
+++ apps/testing-javascript/src/components/playlist/playlist-item.tsx
@@ -22,7 +22,7 @@ const PlaylistItem: React.FC<{
   const nextLessonSlug = moduleProgress?.nextLesson?.slug
   return (
     <li className="flex flex-col items-center md:items-start space-y-6 md:space-y-0 md:flex-row md:space-x-8 lg:space-x-12">
-      <div className="flex flex-col item-center shrink-0 space-y-8">
+      <div className="flex flex-col item-center shrink-0 space-y-4 md:space-y-6 lg:space-y-8">
         <div className="w-48 lg:w-60">
           {purchased ? (
             <Link
@@ -66,7 +66,7 @@ const PlaylistItem: React.FC<{
         )}
       </div>
       <div>
-        <h3 className="text-3xl md:text-4xl">
+        <h3 className="text-3xl md:text-4xl text-center md:text-start">
           {purchased ? (
             <Link
               href={`/playlists/${playlist.slug.current}`}
@@ -80,15 +80,15 @@ const PlaylistItem: React.FC<{
         </h3>
         {!purchased && (
           <div className="space-y-4">
-            <div className="flex items-center space-x-3 mt-4">
-              <span className="rounded-full text-white uppercase bg-tjs-green text-base px-2 py-px font-tt-medium">
+            <div className="flex items-center space-x-2 md:space-x-3 mt-4 justify-center md:justify-start">
+              <span className="rounded-full text-white uppercase bg-tjs-green text-xs sm:text-sm md:text-base px-2 py-px font-tt-demibold">
                 updated
               </span>
-              <span className="text-tjs-green text-2xl font-tt-medium">
+              <span className="text-tjs-green text-base sm:text-xl md:text-2xl font-tt-medium">
                 Includes brand new content!
               </span>
             </div>
-            <div>
+            <div className="text-center md:text-start">
               <Link
                 href="/buy"
                 className="font-tt-demibold duration-200 hover:text-[#ffa82e]"
@@ -101,40 +101,59 @@ const PlaylistItem: React.FC<{
         )}
         <div
           className={cx(
-            'flex items-center space-x-5',
-            purchased ? 'mt-4' : 'mt-8',
+            'flex items-center justify-center md:justify-start space-x-4 sm:space-x-5',
+            purchased ? 'mt-4' : 'mt-5 lg:mt-8',
           )}
         >
           {purchased && (
             <a
               href="/"
-              className="py-1 px-2 space-x-2 flex items-center text-base rounded-sm bg-gray-100 hover:bg-gray-200 duration-200"
+              className="py-1 px-2 space-x-2 sm:space-x-2 flex items-center text-base rounded-sm bg-gray-100 hover:bg-gray-200 duration-200"
             >
-              <Icon name="download" className="w-[14px] h-[14px]" />
+              <Icon
+                name="download"
+                className="w-[12px] h-[12px] sm:w-[14px] sm:h-[14px]"
+              />
               <span>Download</span>
             </a>
           )}
-          <div className="space-x-2 flex items-center text-base">
-            <Icon name="lesson" className="w-[20px] h-[20px]" />
+          <div className="space-x-1 sm:space-x-2 flex items-center text-base">
+            <Icon
+              name="lesson"
+              className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+            />
             <span>
-              {playlist?.sections?.[0]?.lessons?.length} video lessons
+              {playlist?.sections?.[0]?.lessons?.length}{' '}
+              <span className="hidden sm:inline">video</span> lessons
             </span>
           </div>
           {!purchased && (
             <>
-              <div className="space-x-2 flex items-center text-base">
-                <Icon name="code" className="w-[20px] h-[20px]" />
+              <div className="space-x-1 sm:space-x-2 flex items-center text-base">
+                <Icon
+                  name="code"
+                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+                />
                 <span>code</span>
               </div>
-              <div className="space-x-2 flex items-center text-base">
-                <Icon name="transcript" className="w-[20px] h-[20px]" />
+              <div className="space-x-1 sm:space-x-2 flex items-center text-base">
+                <Icon
+                  name="transcript"
+                  className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+                />
                 <span>transcript</span>
               </div>
             </>
           )}
-          <div className="space-x-2 flex items-center text-base">
-            <Icon name="duration" className="w-[20px] h-[20px]" />
-            <span>Xh XXm of learning material</span>
+          <div className="space-x-1 sm:space-x-2 flex items-center text-base">
+            <Icon
+              name="duration"
+              className="w-[18px] h-[18px] sm:w-[20px] sm:h-[20px]"
+            />
+            <span>
+              Xh XXm{' '}
+              <span className="hidden lg:inline">of learning material</span>
+            </span>
           </div>
         </div>
         {purchased && (
@@ -156,7 +175,7 @@ const PlaylistItem: React.FC<{
           </Link>
         )}
         {playlist?.body && (
-          <div className="mt-7 prose md:prose-md">
+          <div className="mt-6 lg:mt-7 prose md:prose-md">
             <PortableText
               value={playlist.body}
               components={{
