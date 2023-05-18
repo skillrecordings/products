98a69d6f03c25398df3b9b5ac1fc58a224a27774
Matt Pocock <mattpocockvoice@gmail.com>
2023-05-18 11:41:34 +0100

    fix(tt): Fix Total TypeScript build

---
+++ apps/skillrecordings/globals.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Used to indicate that there's a type that needs to be fixed.
+ */
+type $TSFixMe = any
---
+++ apps/total-typescript/globals.d.ts
@@ -0,0 +1,5 @@
+namespace React {
+  interface HTMLAttributes<T> {
+    tw?: string
+  }
+}
