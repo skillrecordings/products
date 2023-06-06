e86357e960aac694187ba72cfa1ae50c4761e3c4
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-06-06 10:22:55 -0700

    defaultProduct trpc route

--- apps/testing-javascript/src/trpc/routers/_app.ts
+++ apps/testing-javascript/src/trpc/routers/_app.ts
@@ -4,10 +4,12 @@ import {
   skillLessonRouter,
 } from '@skillrecordings/skill-lesson'
 import {abilitiesRouter} from './abilities'
+import {productsRouter} from './products'
 
 export const appRouter = mergeRouters(
   router({
     abilities: abilitiesRouter,
+    products: productsRouter,
   }),
   skillLessonRouter,
 )
