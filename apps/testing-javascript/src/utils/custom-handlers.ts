e14de6bb36d89cada171cca9824b8fd8e4253914
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-06-07 11:16:03 -0700

    clean up

--- apps/testing-javascript/src/utils/custom-handlers.ts
+++ apps/testing-javascript/src/utils/custom-handlers.ts
@@ -1,42 +1,8 @@
-import {type NextRouter, useRouter} from 'next/router'
+import {type NextRouter} from 'next/router'
 import {type Section} from '@skillrecordings/skill-lesson/schemas/section'
 import {type Module} from '@skillrecordings/skill-lesson/schemas/module'
 import {type Lesson} from '@skillrecordings/skill-lesson/schemas/lesson'
-import {type Exercise} from '@skillrecordings/skill-lesson/schemas/exercise'
 
-// export const pathnameForPath = ({
-//   section,
-//   path,
-// }: {
-//   section?: Section
-//   path: string
-// }) => {
-//   return section
-//     ? `/${path}/[module]/[section]/[lesson]`
-//     : `/${path}/[module]/[lesson]`
-// }
-
-// export const getRouteQuery = ({
-//   section,
-//   module,
-// }: {
-//   section?: Section
-//   module: Module
-// }) => {
-//   return section
-//     ? {
-//         module: module.slug.current,
-//         section: module.sections && module.sections[0].slug,
-//         lesson:
-//           module.sections &&
-//           module.sections[0].lessons &&
-//           module.sections[0].lessons[0].slug,
-//       }
-//     : {
-//         module: module.slug.current,
-//         lesson: module.lessons && module.lessons[0].slug,
-//       }
-// }
 export const customPlayFromBeginningHandler = ({
   router,
   section,
