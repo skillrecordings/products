653048c29eb1d2e466fa64a31cbed5d86e23ef97
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-06-19 10:06:39 -0700

    tweaks

--- apps/badass/src/components/mdx/components/tweet-embed.tsx
+++ apps/badass/src/components/mdx/components/tweet-embed.tsx
@@ -8,20 +8,22 @@ export type TweetEmbedProps = {
 
 const TweetEmbed: React.FC<TweetEmbedProps> = ({tweetId}) => {
   return (
-    <TwitterTweetEmbed
-      tweetId={tweetId}
-      options={{
-        theme: 'dark',
-        cards: 'hidden',
-        width: 550,
-        align: 'center',
-      }}
-      placeholder={
-        <div className="flex aspect-square h-full w-full items-center justify-center">
-          <Spinner className="h-5 w-5" />
-        </div>
-      }
-    />
+    <div data-tweet-embed>
+      <TwitterTweetEmbed
+        tweetId={tweetId}
+        options={{
+          theme: 'dark',
+          cards: 'hidden',
+          width: 550,
+          align: 'center',
+        }}
+        placeholder={
+          <div className="flex aspect-square h-full w-full items-center justify-center">
+            <Spinner className="h-5 w-5" />
+          </div>
+        }
+      />
+    </div>
   )
 }
 
