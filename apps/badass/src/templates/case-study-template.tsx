190faadb29f7c818c33f730c6c6e4f1cbdc7360e
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-06-20 17:14:58 -0700

    clean up

--- apps/badass/src/templates/case-study-template.tsx
+++ apps/badass/src/templates/case-study-template.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import {toPlainText} from '@portabletext/react'
-import {LinkedIn, Twitter} from '@skillrecordings/react'
-import {useRouter} from 'next/router'
-import JoelHooksHeadshotImage from '../../public/joel-hooks.jpg'
 import Layout from 'components/layout'
 import Image from 'next/image'
 import {SmallCallToActionForm} from '../components/call-to-action-form'
@@ -127,38 +124,5 @@ const Header: React.FC<
         </div>
       </div>
     </header>
-  )
-}
-
-const Share: React.FC<React.PropsWithChildren<{title: string}>> = ({title}) => {
-  const router = useRouter()
-  const url = process.env.NEXT_PUBLIC_URL + router.asPath
-  const className =
-    'p-3 hover:bg-white hover:bg-opacity-10 transition rounded-full focus-visible:ring-white'
-  const message = `${title} by @jhooks`
-
-  return (
-    <div className="flex">
-      <Twitter className={className} link={url} message={message} />
-      <LinkedIn className={className} link={url} message={message} />
-    </div>
-  )
-}
-
-export const Author = () => {
-  return (
-    <div className="flex items-center">
-      <Image
-        src={JoelHooksHeadshotImage}
-        alt="Joel Hooks"
-        width={48}
-        height={48}
-        placeholder="blur"
-        priority
-        loading="eager"
-        className="rounded-full"
-      />
-      <span className="pl-2 font-medium leading-tight">Joel Hooks</span>
-    </div>
   )
 }
