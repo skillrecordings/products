a9c079a46a55ca242d150b418e617db5e5de40de
Joel Hooks <joelhooks@gmail.com>
2023-06-26 17:41:22 -0600

    add purchase routes

---
+++ apps/epic-web/src/components/page-headline.tsx
@@ -0,0 +1,24 @@
+import cx from 'classnames'
+
+type Props = {
+  className?: string
+  children: React.ReactNode
+} & React.ComponentProps<'h1'>
+
+export default function PageHeadline({
+  className,
+  children,
+  ...restProps
+}: Props) {
+  return (
+    <h1
+      className={cx(
+        'font-heading text-center text-4xl font-black sm:text-5xl lg:text-6xl',
+        className,
+      )}
+      {...restProps}
+    >
+      {children}
+    </h1>
+  )
+}
