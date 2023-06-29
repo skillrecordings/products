4f21e8026bf48ebc08961d5aed4bafc5dd180813
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-06-29 16:17:21 -0700

    tweaks

--- apps/badass/src/pages/partners/index.tsx
+++ apps/badass/src/pages/partners/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
+import Image from 'next/image'
 import Layout from 'components/layout'
 import Link from 'next/link'
-import Markdown from 'react-markdown'
+import Balancer from 'react-wrap-balancer'
+import cx from 'classnames'
 import {GetStaticProps} from 'next'
-import {format} from 'date-fns'
 import {type CaseStudy, getAllCaseStudies} from 'lib/case-studies'
-import Image from 'next/legacy/image'
-import Stars from '../../../public/assets/stars-1@2x.png'
-import JoelHooksHeadshotImage from '../../../public/joel-hooks.jpg'
+import {SmallCallToActionForm} from 'components/call-to-action-form'
+import {genericCallToActionContent} from 'components/landing-content'
 
 const meta = {
   title: 'Badass Partners',
@@ -25,81 +25,84 @@ const CaseStudies: React.FC<React.PropsWithChildren<CaseStudiesProps>> = ({
 }) => {
   return (
     <Layout meta={meta} className="overflow-hidden">
-      <header className="sm:py-10 py-24 px-4 md:px-16 relative">
-        <Image
-          src={Stars}
-          alt=""
-          aria-hidden="true"
-          layout="fill"
-          className="sm:object-contain object-scale-down sm:scale-100 scale-50 pointer-events-none select-none"
-        />
-        <h1 className="relative z-10 max-w-screen-md font-expanded bg-gradient-to-tr from-badass-pink-500/80 via-badass-pink-400/50 to-badass-black bg-clip-text text-transparent font-bold mx-auto leading-none text-center text-3xl sm:text-4xl lg:text-5xl">
-          Badass Partners
-        </h1>
-      </header>
-      <main className="px-3 flex-grow">
-        <div className="pb-16 mx-auto max-w-3xl w-full sm:pt-20 gap-16">
-          <div className="grid grid-cols-1 gap-20">
+      <main className="px-3 sm:px-6 flex-grow">
+        <div className="pb-16 mx-auto max-w-7xl w-full sm:pt-10">
+          <div className="space-y-10">
             {caseStudies.map(
-              ({title, slug, description, _createdAt: date}, i: number) => {
+              (
+                {
+                  title,
+                  slug,
+                  description,
+                  _createdAt: date,
+                  heroImage,
+                  partnerName,
+                },
+                i: number,
+              ) => {
                 return (
-                  <div key={slug} className="sm:gap-5 gap-3 flex">
-                    <i
-                      aria-hidden="true"
-                      className="font-symbol sm:text-5xl text-4xl text-badass-yellow-300"
-                    >
-                      o
-                    </i>
-                    <div className="flex w-full sm:justify-between justify-left">
-                      <div>
-                        <Link
-                          href={`/partners/${slug}`}
-                          passHref
-                          className="group block"
-                        >
-                          <h2 className="group-hover:underline md:text-4xl sm:text-3xl text-xl font-heading">
-                            {title}
-                          </h2>
-                        </Link>
-                        <div className="flex items-center pt-5 gap-8">
-                          <Author />
-                          <time dateTime={date} className="block opacity-80">
-                            {format(new Date(date), 'dd MMMM, y')}
-                          </time>
-                        </div>
-                        {description && (
-                          <Markdown className="prose pt-3 pb-6">
-                            {description}
-                          </Markdown>
-                        )}
-                      </div>
-                    </div>
-                  </div>
+                  <CaseStudyCard
+                    key={slug}
+                    image={heroImage}
+                    title={title}
+                    slug={slug}
+                    date={date}
+                    partnerName={partnerName}
+                    isOdd={i % 2 == 0}
+                  />
                 )
               },
             )}
           </div>
         </div>
+        <SmallCallToActionForm content={genericCallToActionContent} />
       </main>
     </Layout>
   )
 }
 
-const Author = () => {
+const CaseStudyCard: React.FC<any> = ({
+  image,
+  title,
+  slug,
+  date,
+  partnerName,
+  isOdd,
+}) => {
+  const dateObject = new Date(date)
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    year: 'numeric',
+  }).format(dateObject)
   return (
-    <div className="flex items-center">
-      <Image
-        src={JoelHooksHeadshotImage}
-        alt="Joel Hooks"
-        width={48}
-        height={48}
-        placeholder="blur"
-        priority
-        loading="eager"
-        className="rounded-full"
-      />
-      <span className="pl-2 font-medium leading-tight">Joel Hooks</span>
-    </div>
+    <Link
+      href={`/partners/${slug}`}
+      className={cx(
+        'rounded-xl border-2 border-badass-gray-800 px-6 py-6 md:pt-4 md:pb-0 flex flex-col duration-150 hover:bg-badass-gray-800 items-center',
+        isOdd ? 'md:flex-row' : 'md:flex-row-reverse',
+      )}
+    >
+      <div className="shrink-0 md:w-1/2 flex justify-center md:px-6">
+        <div className="max-w-[75%] md:max-w-none">
+          <Image src={image} alt="image" width={440} height={440} />
+        </div>
+      </div>
+      <div
+        className={cx(
+          'md:w-1/2 shrink-0 md:px-6 text-center md:text-left',
+          isOdd ? 'md:pl-10' : 'lg:pl-10 xl:pl-20',
+        )}
+      >
+        <h2 className="font-heading text-3xl md:text-4xl lg:text-5xl leading-tight md:leading-tight lg:leading-tight mt-2 md:mt-0">
+          <Balancer>{title}</Balancer>
+        </h2>
+        <div className="font-mono uppercase opacity-70 mt-4 lg:mt-6 text-xs lg:text-sm xl:text-base flex flex-col md:flex-row items-center">
+          <span>client: {partnerName}</span>
+          <span className="hidden md:inline"> &middot; </span>
+          <span>published: {formattedDate}</span>
+        </div>
+      </div>
+    </Link>
   )
 }
 
