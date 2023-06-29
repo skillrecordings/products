08ed686646d50312edd459b8779f41786524566e
Vojta Holik <vojta@egghead.io>
2023-06-29 11:55:22 +0200

    feat: unified login pages

--- apps/epic-web/src/pages/check-your-email.tsx
+++ apps/epic-web/src/pages/check-your-email.tsx
@@ -1,28 +1,11 @@
 import * as React from 'react'
 import Layout from 'components/app/layout'
-import Image from 'next/legacy/image'
-import toast from 'react-hot-toast'
+import {CheckYourEmailTemplate} from '@skillrecordings/skill-lesson/login'
 
 const CheckYourEmail = () => {
-  React.useEffect(() => {
-    toast('Check your email', {
-      icon: '✉️',
-    })
-  }, [])
-
   return (
-    <Layout meta={{title: `Log in to ${process.env.NEXT_PUBLIC_SITE_TITLE}`}}>
-      <main className="relative flex flex-grow flex-col items-center justify-center px-5 pb-16 pt-5">
-        <div className="mx-auto flex w-full max-w-md flex-col items-center gap-5 text-center">
-          <h1 className="text-center text-3xl font-extrabold leading-9 sm:text-3xl lg:text-4xl">
-            Check your email
-          </h1>
-          <p className="max-w-sm text-gray-600 dark:text-gray-300">
-            A login link will been sent to your email! Use it and you&apos;ll be
-            able to access your account.
-          </p>
-        </div>
-      </main>
+    <Layout meta={{title: 'Check your email'}}>
+      <CheckYourEmailTemplate />
     </Layout>
   )
 }
--- apps/protailwind/src/pages/check-your-email-copy.tsx
+++ apps/protailwind/src/pages/check-your-email-copy.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import Layout from '@/components/app/layout'
+import Layout from 'components/layout'
 import {CheckYourEmailTemplate} from '@skillrecordings/skill-lesson/login'
+import Image from 'next/image'
 
 const CheckYourEmail = () => {
   return (
-    <Layout meta={{title: 'Check your email'}}>
+    <Layout meta={{title: `Check your email`}} className="bg-blue-600">
       <CheckYourEmailTemplate />
     </Layout>
   )
