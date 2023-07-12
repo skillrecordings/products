49509ce82ca28e9bc3707a9dd9edf2664aa9b671
John Lindquist <johnlindquist@gmail.com>
2023-07-12 10:21:02 -0600

    fix(usage): add timeout

--- apps/scriptkit/src/pages/api/usage.ts
+++ apps/scriptkit/src/pages/api/usage.ts
@@ -18,14 +18,26 @@ const trackRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   // Get track event and properties from request body
   const {event, properties, device}: TrackPayload = req.body
   console.debug(`track ${event}`, properties, device)
-  const response = await track(event, properties, device).promise.catch(
+
+  // Timeout in ms
+  const timeout = 400
+
+  const trackPromise = track(event, properties, device).promise
+
+  const timeoutPromise = new Promise((_, reject) =>
+    setTimeout(() => reject(new Error('Request timed out')), timeout),
+  )
+
+  const response = await Promise.race([trackPromise, timeoutPromise]).catch(
     (err) => {
       console.error(err)
-      res.status(500).json({error: err})
+      res.status(500).json({error: err.message})
     },
   )
 
-  res.status(200).json(response)
+  if (response) {
+    res.status(200).json(response)
+  }
 }
 
 export default trackRoute
