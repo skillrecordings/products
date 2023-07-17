3c681e80fbd820a19e78a3ea91049c1b4983f75c
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-07-17 22:54:19 +0100

    initial LessonsSidebar component

---
+++ apps/testing-javascript/src/components/lessons-sidebar.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import cx from 'classnames'
+import {useRouter} from 'next/router'
+import MuxPlayer, {
+  type MuxPlayerRefAttributes,
+  type MuxPlayerProps,
+} from '@mux/mux-player-react'
+import {Video} from '@skillrecordings/skill-lesson/video/video'
+import {
+  VideoProvider,
+  useMuxPlayer,
+} from '@skillrecordings/skill-lesson/hooks/use-mux-player'
+import {useLesson} from '@skillrecordings/skill-lesson/hooks/use-lesson'
+import {useVideoResource} from '@skillrecordings/skill-lesson/hooks/use-video-resource'
+import {
+  customPlayFromBeginningHandler,
+  customContinueHandler,
+} from 'utils/custom-handlers'
+import {VideoTranscript} from '@skillrecordings/skill-lesson/video/video-transcript'
+
+import {trpc} from 'trpc/trpc.client'
+import Spinner from 'components/spinner'
+
+const LessonsSidebarItem: React.FC<any> = () => {
+  return (
+    <li>
+      <div>1</div>
+    </li>
+  )
+}
+
+const LessonsSidebar: React.FC<any> = ({lesson, module}) => {
+  console.log({lesson, module})
+  return (
+    <div className="min-h-[450px] border border-black/[.08] w-full flex flex-col">
+      <div className="flex p-3 border-b border-black/[.08] shrink-0">
+        <Link
+          href={`/playlists/${module.slug}`}
+          className="w-16 h-16 shrink-0 mr-3"
+        >
+          <Image src={module.image} alt="module image" width={64} height={64} />
+        </Link>
+        <Link
+          href={`/playlists/${module.slug.current}`}
+          className="text-lg leading-tight font-tt-demibold"
+        >
+          {module.title}
+        </Link>
+      </div>
+      <div className="grow relative">
+        <ul className="absolute inset-0 overflow-y-scroll divide-y">
+          {module.sections[0].lessons.map((item: any, i: number) => {
+            return (
+              <li key={item.slug}>
+                <Link
+                  href={`/lessons/${item.slug}`}
+                  className={cx(
+                    'flex px-3 py-2 text-sm items-baseline duration-100 hover:bg-gray-100',
+                    {'bg-gray-100': item.slug === lesson.slug},
+                  )}
+                >
+                  <div className="shrink-0 w-4 mr-2">{i + 1}.</div>
+                  <div className="font-tt-medium text-base">{item.title}</div>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default LessonsSidebar
