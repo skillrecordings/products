8d33dc56f3767b07190c800b2ecf7c0a996b2752
Joel Hooks <joelhooks@gmail.com>
2023-07-24 17:15:37 -0500

    Revert "Add responsive styles and autoscroll"
    This reverts commit e7f625188007b37d24d1f87593eff71f2736cfb6.

--- apps/testing-javascript/src/components/lessons-sidebar.tsx
+++ apps/testing-javascript/src/components/lessons-sidebar.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import cx from 'classnames'
-import {Element, scroller} from 'react-scroll'
 import {useRouter} from 'next/router'
 import MuxPlayer, {
   type MuxPlayerRefAttributes,
@@ -34,24 +33,9 @@ const LessonsSidebarItem: React.FC<any> = () => {
 
 const LessonsSidebar: React.FC<any> = ({lesson, module}) => {
   console.log({lesson, module})
-  const [activeElement, setActiveElement] = React.useState<string>(lesson.slug)
-  const scrollableNodeRef: any = React.createRef()
-
-  const handlerScrollToActiveElem = () => {
-    scroller.scrollTo(activeElement, {
-      duration: 0,
-      delay: 0,
-      containerId: 'scroll-container',
-    })
-  }
-
-  React.useEffect(() => {
-    handlerScrollToActiveElem()
-  }, [activeElement])
-
   return (
     <div className="min-h-[450px] border border-black/[.08] w-full flex flex-col">
-      <div className="flex items-center p-3 border-b border-black/[.08] shrink-0">
+      <div className="flex p-3 border-b border-black/[.08] shrink-0">
         <Link
           href={`/playlists/${module.slug}`}
           className="w-16 h-16 shrink-0 mr-3"
@@ -66,24 +50,15 @@ const LessonsSidebar: React.FC<any> = ({lesson, module}) => {
         </Link>
       </div>
       <div className="grow relative">
-        <ul
-          ref={scrollableNodeRef}
-          id="scroll-container"
-          className="absolute inset-0 overflow-y-auto divide-y"
-        >
+        <ul className="absolute inset-0 overflow-y-scroll divide-y">
           {module.sections[0].lessons.map((item: any, i: number) => {
             return (
               <li key={item.slug}>
-                <Element name={item.slug} />
                 <Link
                   href={`/lessons/${item.slug}`}
-                  onClick={() => setActiveElement(item.slug)}
                   className={cx(
-                    'flex px-3 py-3 lg:py-2 text-sm items-baseline duration-100 hover:bg-gray-50',
-                    {
-                      'bg-gray-100 hover:bg-gray-100':
-                        item.slug === lesson.slug,
-                    },
+                    'flex px-3 py-2 text-sm items-baseline duration-100 hover:bg-gray-100',
+                    {'bg-gray-100': item.slug === lesson.slug},
                   )}
                 >
                   <div className="shrink-0 w-4 mr-2">{i + 1}.</div>
