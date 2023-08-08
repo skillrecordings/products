97da22e715f5e6cf760c3ba27a8795373cf6e368
jbranchaud <jbranchaud@gmail.com>
2023-08-08 11:09:08 -0500

    fix(tt): pass applied bulk coupon to Stripe
    This commit also includes a refactor of how we are building the Skill
    API stripe checkout path for form submission. It consolidates the
    parameter building, adds type-checking, and uses URLSearchParams.

--- packages/skill-lesson/index.tsx
+++ packages/skill-lesson/index.tsx
@@ -1,4 +1,5 @@
 export * from './utils/ability'
+export * from './utils/build-stripe-checkout-path'
 export * from './trpc/routers/_skill-lesson-router'
 export * from './trpc/trpc-context'
 export * from './trpc/trpc.server'
