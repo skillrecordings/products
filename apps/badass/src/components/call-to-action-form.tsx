ee9ff267fa6bfdbb322c44f5d929d130e08be883
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-08-22 10:50:18 +0200

    complete desktop version

--- apps/badass/src/components/call-to-action-form.tsx
+++ apps/badass/src/components/call-to-action-form.tsx
@@ -17,72 +17,118 @@ export const CallToActionForm: React.FC<React.PropsWithChildren<any>> = ({
   const router = useRouter()
 
   return (
-    <section className="flex flex-col items-center justify-center sm:pt-16 sm:pb-16 pt-12">
-      <div className="relative max-w-2xl mx-auto flex flex-col items-center">
-        <div className="absolute sm:-left-24 sm:top-0 -top-16 sm:w-auto w-36 z-20">
-          <Image
-            loading="eager"
-            aria-hidden="true"
-            src={ImageFish}
-            width={160}
-            height={136}
-            alt="a fish"
-          />
-        </div>
-        <div className="absolute right-[-116px] z-20 sm:block hidden">
-          <Image
-            loading="eager"
-            aria-hidden="true"
-            src={ImageFormMushroomRight}
-            width={196}
-            height={478}
-            alt="mushroom"
-          />
-        </div>
-        <div className="absolute -left-24 -bottom-24 z-20 sm:block hidden">
-          <Image
-            loading="eager"
-            aria-hidden="true"
-            src={ImageFormMushroomLeft}
-            width={280 / 1.1}
-            height={459 / 1.1}
-            alt="a mushroom"
-          />
-        </div>
-        <div className="absolute -right-40 bottom-8 z-0 sm:block hidden">
-          <Image
-            loading="eager"
-            aria-hidden="true"
-            src={ImageFormLeaf}
-            width={338 / 1.1}
-            height={449 / 1.1}
-            alt="a mushroom"
-          />
-        </div>
-        <div className="bg-[#082C1B] sm:px-20 sm:py-20 px-5 py-16 rounded-md relative z-10">
-          <h2 className="font-heading sm:text-4xl text-3xl text-center">
-            {content.heading}
-          </h2>
-          <div className="pt-10 pb-5 space-y-5 opacity-80 sm:text-lg prose max-w-md mx-auto">
-            {content.description}
+    <section className="bg-badass-neutral-500 pt-[7.5rem] pb-[8.5rem] mt-44">
+      <div className="max-w-[1158px] px-6 mx-auto">
+        <div className="flex flex-col md:flex-row">
+          <div className="grow">
+            <h2 className="font-heading md:text-[2rem] md:leading-tight text-2xl leading-[1.333]">
+              {content.heading}
+            </h2>
+            <div className="mt-11 space-y-10 text-lg sm:text-xl leading-[1.75] text-[#e5e5e5]">
+              {content.description}
+            </div>
           </div>
-          <SubscribeToConvertkitForm
-            actionLabel={content.button}
-            onSuccess={(subscriber: any) => {
-              if (subscriber) {
-                const redirectUrl = redirectUrlBuilder(subscriber, '/confirm')
-                router.push(redirectUrl)
-              }
-            }}
-          />
-          <p className="text-center pt-8 text-sm opacity-50 max-w-xs mx-auto">
-            {content.info}
-          </p>
+          <div className="shrink-0 w-[434px] ml-[120px] relative">
+            <div className="w-[491px] absolute right-0 -top-[19rem]">
+              <Image
+                src="https://res.cloudinary.com/badass-courses/image/upload/v1692693157/assets/subscribe-form-image_2x_h0mweo.png"
+                width={982}
+                height={982}
+                alt="Subscribe Form image"
+              />
+            </div>
+            <div className="mt-40">
+              <SubscribeToConvertkitForm
+                actionLabel={content.button}
+                onSuccess={(subscriber: any) => {
+                  if (subscriber) {
+                    const redirectUrl = redirectUrlBuilder(
+                      subscriber,
+                      '/confirm',
+                    )
+                    router.push(redirectUrl)
+                  }
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
   )
 }
+// export const CallToActionForm: React.FC<React.PropsWithChildren<any>> = ({
+//   content,
+// }) => {
+//   const router = useRouter()
+
+//   return (
+//     <section className="flex flex-col items-center justify-center sm:pt-16 sm:pb-16 pt-12">
+//       <div className="relative max-w-2xl mx-auto flex flex-col items-center">
+//         <div className="absolute sm:-left-24 sm:top-0 -top-16 sm:w-auto w-36 z-20">
+//           <Image
+//             loading="eager"
+//             aria-hidden="true"
+//             src={ImageFish}
+//             width={160}
+//             height={136}
+//             alt="a fish"
+//           />
+//         </div>
+//         <div className="absolute right-[-116px] z-20 sm:block hidden">
+//           <Image
+//             loading="eager"
+//             aria-hidden="true"
+//             src={ImageFormMushroomRight}
+//             width={196}
+//             height={478}
+//             alt="mushroom"
+//           />
+//         </div>
+//         <div className="absolute -left-24 -bottom-24 z-20 sm:block hidden">
+//           <Image
+//             loading="eager"
+//             aria-hidden="true"
+//             src={ImageFormMushroomLeft}
+//             width={280 / 1.1}
+//             height={459 / 1.1}
+//             alt="a mushroom"
+//           />
+//         </div>
+//         <div className="absolute -right-40 bottom-8 z-0 sm:block hidden">
+//           <Image
+//             loading="eager"
+//             aria-hidden="true"
+//             src={ImageFormLeaf}
+//             width={338 / 1.1}
+//             height={449 / 1.1}
+//             alt="a mushroom"
+//           />
+//         </div>
+//         <div className="bg-[#082C1B] sm:px-20 sm:py-20 px-5 py-16 rounded-md relative z-10">
+//           <h2 className="font-heading sm:text-4xl text-3xl text-center">
+//             {content.heading}
+//           </h2>
+//           <div className="pt-10 pb-5 space-y-5 opacity-80 sm:text-lg prose max-w-md mx-auto">
+//             {content.description}
+//           </div>
+// <SubscribeToConvertkitForm
+//   actionLabel={content.button}
+//   onSuccess={(subscriber: any) => {
+//     if (subscriber) {
+//       const redirectUrl = redirectUrlBuilder(subscriber, '/confirm')
+//       router.push(redirectUrl)
+//     }
+//   }}
+// />
+//           <p className="text-center pt-8 text-sm opacity-50 max-w-xs mx-auto">
+//             {content.info}
+//           </p>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
 
 export const SmallCallToActionForm: React.FC<React.PropsWithChildren<any>> = ({
   content,
