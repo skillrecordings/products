0d8bad5270a7f7046ecf3befcd66885ac2a7431f
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-08-24 20:08:54 +0200

    tablet portrait styles

--- apps/badass/src/components/card.tsx
+++ apps/badass/src/components/card.tsx
@@ -33,9 +33,9 @@ const Card: React.FC<CardProps> = ({
     <div
       className={twMerge(
         cx(
-          'overflow-hidden pb-14 rounded-2xl border-2 border-badass-gray-800 duration-150 hover:bg-badass-gray-800 flex flex-col items-center px-4 md:px-8',
+          'overflow-hidden pb-14 rounded-2xl border-2 border-badass-gray-800 duration-150 hover:bg-badass-gray-800 flex flex-col items-center px-4 lg:px-8',
           {
-            'pb-14': type === 'caseStudy',
+            'pb-10 lg:pb-14': type === 'caseStudy',
             'pb-10': type === 'article',
           },
         ),
@@ -48,49 +48,51 @@ const Card: React.FC<CardProps> = ({
           alt={title}
           width={482}
           height={482}
-          className="-top-7 relative"
+          className="-top-7 md:-top-9 lg:-top-7 relative"
         />
       )}
-      <div className="flex flex-col items-center -mt-[80px]">
-        <h2
-          className={cx(
-            'text-2xl md:text-[2rem] font-heading max-w-[340px] text-center leading-[1.333] md:leading-tight',
-            {
-              'mt-3 md:mt-0': type === 'caseStudy',
-              'mt-4': type === 'article',
-            },
-          )}
-        >
-          {title}
-        </h2>
-        {type === 'caseStudy' && (
-          <h3 className="text-neutral-200 uppercase font-medium leading-[1.42] md:leading-[2.185] text-sm md:text-base font-mono mt-2 md:mt-4 tracking-[0.14px] md:tracking-[0.16px]">
-            with {subtitle}
-          </h3>
-        )}
-        {type === 'article' && (
-          <>
-            {authorName && authorAvatarUrl && (
-              <div className="flex space-x-2 md:space-x-4 items-center mt-5 md:mt-9">
-                <div className="w-9 h-9 md:w-12 md:h-12 rounded-full overflow-hidden">
-                  <Image
-                    src={authorAvatarUrl}
-                    alt={authorName}
-                    width={48}
-                    height={48}
-                  />
-                </div>
-                <div className="text-white opacity-80 uppercase font-mono tracking-[0.16px] text-sm md:text-base">
-                  {authorName}
-                </div>
-              </div>
+      <div className="flex flex-col items-center -mt-[80px] md:-mt-[60px] lg:-mt-[80px] relative grow">
+        <div className="grow flex flex-col items-center">
+          <h2
+            className={cx(
+              'text-2xl lg:text-[2rem] font-heading max-w-[340px] text-center leading-[1.333] lg:leading-tight',
+              {
+                'mt-3 md:mt-0': type === 'caseStudy',
+                'mt-4': type === 'article',
+              },
             )}
-            <h3 className="text-neutral-200 leading-normal md:leading-[1.75] mt-4 text-center text-base md:text-xl font-medium opacity-80">
-              {subtitle}
+          >
+            {title}
+          </h2>
+          {type === 'caseStudy' && (
+            <h3 className="text-neutral-200 uppercase font-medium leading-[1.42] lg:leading-[2.185] text-sm lg:text-base font-mono mt-2 lg:mt-4 tracking-[0.14px] lg:tracking-[0.16px] text-center">
+              with {subtitle}
             </h3>
-          </>
-        )}
-        <ButtonSecondary href={href} size="middle" className="mt-6 md:mt-10">
+          )}
+          {type === 'article' && (
+            <>
+              {authorName && authorAvatarUrl && (
+                <div className="flex space-x-2 md:space-x-4 items-center mt-5 md:mt-9">
+                  <div className="w-9 h-9 md:w-12 md:h-12 rounded-full overflow-hidden">
+                    <Image
+                      src={authorAvatarUrl}
+                      alt={authorName}
+                      width={48}
+                      height={48}
+                    />
+                  </div>
+                  <div className="text-white opacity-80 uppercase font-mono tracking-[0.16px] text-sm md:text-base">
+                    {authorName}
+                  </div>
+                </div>
+              )}
+              <h3 className="text-neutral-200 leading-normal md:leading-[1.75] mt-4 text-center text-base md:text-xl font-medium opacity-80">
+                {subtitle}
+              </h3>
+            </>
+          )}
+        </div>
+        <ButtonSecondary href={href} size="middle" className="mt-6 lg:mt-10">
           {ctaText}
         </ButtonSecondary>
       </div>
