10a316d445819b1be62698498e57fb99003b9e18
jbranchaud <jbranchaud@gmail.com>
2023-08-26 18:17:24 -0500

    feat(tjs): display duration of Modules/Lessons

--- apps/testing-javascript/src/pages/playlists/[module].tsx
+++ apps/testing-javascript/src/pages/playlists/[module].tsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
 import {GetStaticPaths, GetStaticProps} from 'next'
-import {type User} from '@skillrecordings/database'
-import {type Module} from '@skillrecordings/skill-lesson/schemas/module'
 
 import {getPlaylist, getAllPlaylists} from 'lib/playlists'
 import {ModuleProgressProvider} from 'utils/module-progress'
 import WorkshopTemplate from 'templates/workshop-template'
+import {Module} from '@types'
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
   const workshop = await getPlaylist(params?.module as string)
