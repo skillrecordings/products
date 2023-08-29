e9529fbf06756c4df43be48aad1c0736d62bdaf8
Vojta Holik <vojta@egghead.io>
2023-08-29 08:15:36 +0200

    refactor(sl): check user ability before loading full video resource (#1140)
    * refactor(sl): check ability in videoResource ctx
    
    * refactor(sl): check ability in videoResource ctx
    
    * refactor(sl): check ability in videoResource ctx

--- packages/skill-lesson/trpc/routers/video-resource.ts
+++ packages/skill-lesson/trpc/routers/video-resource.ts
@@ -1,15 +1,33 @@
 import {z} from 'zod'
 import {publicProcedure, router} from '../trpc.server'
 import {getVideoResource} from '../../lib/video-resources'
+import {defineAbilityRulesForResource} from '../../utils/define-resource-ability-rules'
+import {createAppAbility} from '../../utils/ability'
 
 export const videoResourceRouter = router({
   byId: publicProcedure
     .input(
       z.object({
         id: z.string(),
+        moduleSlug: z.string().nullish(),
+        lessonSlug: z.string(),
+        sectionSlug: z.string().optional(),
       }),
     )
     .query(async ({ctx, input}) => {
-      return await getVideoResource(input.id)
+      const {moduleSlug, sectionSlug, lessonSlug} = input
+      const abilityRules = await defineAbilityRulesForResource({
+        req: ctx.req,
+        moduleSlug,
+        sectionSlug,
+        lessonSlug,
+      })
+      const ability = createAppAbility(abilityRules || [])
+      const canShowVideo = ability.can('view', 'Content')
+
+      if (canShowVideo) {
+        return await getVideoResource(input.id)
+      }
+      return null
     }),
 })
