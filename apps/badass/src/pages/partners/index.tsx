f9e8a3806b089ada097d4098889dc3a69d5e6600
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-08-31 14:27:19 +0200

    clea up

--- apps/badass/src/pages/partners/index.tsx
+++ apps/badass/src/pages/partners/index.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react'
-import Image from 'next/image'
 import Layout from 'components/layout'
-import Link from 'next/link'
-import Balancer from 'react-wrap-balancer'
-import cx from 'classnames'
 import {GetStaticProps} from 'next'
 
 import {type CaseStudy, getAllCaseStudies} from 'lib/case-studies'
-// import {SmallCallToActionForm} from 'components/call-to-action-form'
 import {CallToActionForm} from 'components/call-to-action-form'
 import {genericCallToActionContent} from 'components/landing-content'
 import Card from 'components/card'
@@ -52,7 +47,6 @@ const CaseStudies: React.FC<React.PropsWithChildren<CaseStudiesProps>> = ({
               })}
             </div>
           </div>
-          {/* <SmallCallToActionForm content={genericCallToActionContent} /> */}
           <CallToActionForm content={genericCallToActionContent} />
         </main>
       </div>
@@ -72,53 +66,3 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
 }
 
 export default CaseStudies
-
-// <CaseStudyCard
-//   key={slug}
-//   image={heroImage}
-//   title={title}
-//   slug={slug}
-//   publishedDate={publishedDate}
-//   partnerName={partnerName}
-//   isOdd={i % 2 == 0}
-// />
-
-// const CaseStudyCard: React.FC<any> = ({
-//   image,
-//   title,
-//   slug,
-//   publishedDate,
-//   partnerName,
-//   isOdd,
-// }) => {
-//   return (
-//     <Link
-//       href={`/partners/${slug}`}
-//       className={cx(
-//         'rounded-xl border-2 border-badass-gray-800 px-6 py-6 md:pt-4 md:pb-0 flex flex-col duration-150 hover:bg-badass-gray-800 items-center',
-//         isOdd ? 'md:flex-row' : 'md:flex-row-reverse',
-//       )}
-//     >
-//       <div className="shrink-0 md:w-1/2 flex justify-center md:px-6">
-//         <div className="max-w-[75%] md:max-w-none backdrop-grayscale">
-//           <Image src={image} alt="image" width={440} height={440} />
-//         </div>
-//       </div>
-//       <div
-//         className={cx(
-//           'md:w-1/2 shrink-0 md:px-6 text-center md:text-left',
-//           isOdd ? 'md:pl-6' : 'lg:pl-10 xl:pl-20',
-//         )}
-//       >
-//         <h2 className="font-heading text-3xl md:text-4xl lg:text-5xl leading-tight md:leading-tight lg:leading-tight mt-2 md:mt-0">
-//           <Balancer>{title}</Balancer>
-//         </h2>
-//         <div className="font-mono uppercase opacity-70 mt-4 lg:mt-6 text-xs lg:text-sm xl:text-base flex flex-col lg:flex-row items-center md:items-start">
-//           <span>client: {partnerName}</span>
-//           <span className="hidden lg:inline lg:mx-3"> &middot; </span>
-//           <span>published: {publishedDate}</span>
-//         </div>
-//       </div>
-//     </Link>
-//   )
-// }
