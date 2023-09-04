768ef90268f08218fe3dc5895d09e52d1ec0dea1
Vojta Holik <vojta@egghead.io>
2023-09-04 13:03:15 +0200

    feat(ew): animate particles in hero

--- apps/epic-web/src/pages/index.tsx
+++ apps/epic-web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import Layout from 'components/app/layout'
 import {getPage} from 'lib/pages'
 import type {NextPage} from 'next'
@@ -9,6 +9,16 @@ import {useConvertkit} from '@skillrecordings/skill-lesson/hooks/use-convertkit'
 import {track} from 'utils/analytics'
 import Image from 'next/image'
 import LandingCopy from 'components/landing-copy.mdx'
+import {loadFull} from 'tsparticles'
+import Particles from 'react-particles'
+import {loadStarsPreset} from 'tsparticles-preset-stars'
+import {
+  motion,
+  MotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+} from 'framer-motion'
 
 const Index: NextPage<any> = ({page}) => {
   const {subscriber, loadingSubscriber} = useConvertkit()
@@ -33,28 +43,168 @@ const Article = () => {
   )
 }
 
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance])
+}
+
 const Header = () => {
+  const particlesInit = useCallback(async (engine: any) => {
+    await loadStarsPreset(engine)
+    // await loadFull(engine)
+  }, [])
+
+  const particlesLoaded = useCallback(async (container: any) => {
+    await container
+  }, [])
+
+  const ref = React.useRef(null)
+  const {scrollYProgress} = useScroll({target: ref})
+  const shipsParallax = useParallax(scrollYProgress, 50)
+  const foregroundMotionValue = useTransform(scrollYProgress, [0, 1], [1, 1.5])
+  const planetScale = useTransform(scrollYProgress, [0, 1], [1, 0.8])
+  const planetAnimation = useSpring(planetScale, {mass: 0.1})
   return (
-    <header className="relative flex min-h-[108vh] flex-col items-center justify-start bg-black">
-      <div className="absolute top-[22vh] z-10 mx-auto text-center xl:top-[190px]">
-        <h1 className="max-w-3xl px-5 font-bold text-white fluid-3xl sm:leading-tight lg:px-16">
+    <header
+      ref={ref}
+      className="relative flex min-h-[108vh] flex-col items-center justify-start overflow-hidden bg-black"
+    >
+      <div className="absolute top-[22vh] z-40 mx-auto text-center xl:top-[190px]">
+        <h1 className="max-w-3xl px-5 font-bold text-white shadow-black drop-shadow-lg fluid-3xl sm:leading-tight lg:px-16">
           <span className="inline-flex pb-4 font-sans text-sm font-semibold uppercase tracking-wider text-orange-300">
             Everything You Need to Know to
           </span>
           <Balancer>Ship Modern Full-Stack Web Applications</Balancer>
         </h1>
       </div>
+      <div
+        className="absolute left-0 top-0 z-20 h-full w-full"
+        style={{
+          backgroundImage:
+            'radial-gradient(transparent, transparent, black, black)',
+        }}
+      />
       <Image
-        src={require('../../public/assets/hero/hero.jpg')}
+        src={require('../../public/assets/hero/hero-front-compressed.png')}
         fill
-        className="mx-auto object-cover object-top"
+        className="z-20 mx-auto object-cover object-bottom 2xl:object-fill"
         alt=""
         quality={100}
         aria-hidden
         priority
-        placeholder="blur"
+        placeholder="empty"
       />
-      <div className="absolute bottom-0 left-0 h-32 w-full bg-gradient-to-b from-transparent dark:to-background" />
+
+      <motion.div className="absolute z-20 flex w-[600px] -translate-y-16 items-start justify-center sm:w-[700px] sm:-translate-y-24">
+        <Image
+          src={require('../../public/assets/hero/small-planet-compressed.png')}
+          width={800}
+          alt=""
+          quality={100}
+          aria-hidden
+          priority
+          placeholder="blur"
+        />
+      </motion.div>
+      <motion.div className="absolute bottom-[25%] z-20 mx-auto w-[200px] sm:w-[250px]">
+        <Image
+          src={require('../../public/assets/hero/ships-compressed.png')}
+          width={250}
+          alt=""
+          quality={100}
+          aria-hidden
+          priority
+          placeholder="blur"
+        />
+      </motion.div>
+      <div className="absolute bottom-0 left-0 z-30 h-32 w-full bg-gradient-to-b from-transparent dark:to-background" />
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        canvasClassName="absolute left-0 top-0 z-0 h-full w-full"
+        className="absolute left-0 top-0 z-0 h-full w-full"
+        options={{
+          fullScreen: {
+            enable: false,
+          },
+          preset: 'stars',
+          retina_detect: true,
+          background: {
+            opacity: 0,
+          },
+          pauseOnOutsideViewport: true,
+          particles: {
+            size: {
+              value: {min: 1.5, max: 2.5},
+            },
+            color: {
+              // value: '#67CBEB',
+              value: {
+                h: 195,
+                s: 77,
+                l: 66,
+              },
+            },
+            opacity: {
+              value: {
+                min: 0.1,
+                max: 0.8,
+              },
+            },
+
+            move: {
+              direction: 'top',
+              enable: true,
+              speed: {
+                max: 0.9,
+                min: 0.2,
+              },
+              straight: false,
+              random: true,
+            },
+          },
+        }}
+      />
+      <Particles
+        id="tsparticles2"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        canvasClassName="absolute top-0 z-0 h-[400px] w-[500px]"
+        className="absolute top-0 z-0 h-[400px] w-[800px]"
+        options={{
+          fullScreen: {
+            enable: false,
+          },
+          preset: 'stars',
+          retina_detect: true,
+          background: {
+            opacity: 0,
+          },
+          pauseOnOutsideViewport: true,
+          zLayers: 1,
+          particles: {
+            number: {
+              value: 60,
+            },
+            size: {
+              value: {min: 0.5, max: 3.5},
+            },
+            color: {
+              value: '#F85C1F',
+            },
+            move: {
+              direction: 'outside',
+              enable: true,
+              speed: {
+                max: 1.5,
+                min: 0.3,
+              },
+              straight: false,
+              random: true,
+            },
+          },
+        }}
+      />
     </header>
   )
 }
