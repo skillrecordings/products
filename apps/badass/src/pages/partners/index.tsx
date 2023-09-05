33b497daf61f56adc74b75687daa9fef8fab9143
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-09-05 14:58:55 +0200

    More Prjects on /partners page

--- apps/badass/src/pages/partners/index.tsx
+++ apps/badass/src/pages/partners/index.tsx
@@ -47,6 +47,31 @@ const CaseStudies: React.FC<React.PropsWithChildren<CaseStudiesProps>> = ({
               })}
             </div>
           </div>
+          <div className="mt-16 mb-72">
+            <h2 className="text-3xl md:text-[2rem] lg:text-[2.5rem] leading-[1.2] text-center font-heading">
+              More Projects
+            </h2>
+            <div className="mt-10 md:mt-16 lg:mt-20">
+              <div className="grid md:grid-cols-2 gap-5">
+                <Card
+                  imageUrl="https://res.cloudinary.com/badass-courses/image/upload/v1692110114/cards-images/egghead_2x_eutdgv.png"
+                  title="egghead.io"
+                  subtitle="Joel Hooks & John Lindquist"
+                  href="https://egghead.io/"
+                  type="project"
+                  ctaText="Visit Site"
+                />
+                <Card
+                  imageUrl="https://res.cloudinary.com/badass-courses/image/upload/v1692110114/cards-images/testing-js_2x_ex7mli.png"
+                  title="Testing Javascript"
+                  subtitle="Kent C. Dodds"
+                  href="https://testingjavascript.com/"
+                  type="project"
+                  ctaText="Visit Site"
+                />
+              </div>
+            </div>
+          </div>
           <CallToActionForm content={genericCallToActionContent} />
         </main>
       </div>
