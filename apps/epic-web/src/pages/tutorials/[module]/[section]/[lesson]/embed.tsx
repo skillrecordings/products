1e128170d99791d5884d4ccc09e5cf8f89c86cff
Vojta Holik <vojta@egghead.io>
2023-09-07 22:03:10 +0200

    refactor(ew): extract embeds server logic

--- apps/epic-web/src/pages/tutorials/[module]/[section]/[lesson]/embed.tsx
+++ apps/epic-web/src/pages/tutorials/[module]/[section]/[lesson]/embed.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import {GetServerSideProps, NextApiRequest} from 'next'
-import {getTutorial} from 'lib/tutorials'
-import {getSection} from 'lib/sections'
-import {getExercise} from 'lib/exercises'
+import {GetServerSideProps} from 'next'
 import {type Module} from '@skillrecordings/skill-lesson/schemas/module'
 import {type Section} from '@skillrecordings/skill-lesson/schemas/section'
 import {type Lesson} from '@skillrecordings/skill-lesson/schemas/lesson'
@@ -21,82 +18,29 @@ import {ModuleProgressProvider} from '@skillrecordings/skill-lesson/video/module
 import {useRouter} from 'next/router'
 import pluralize from 'pluralize'
 import {trpc} from 'trpc/trpc.client'
-import {getCsrfToken, getProviders, signIn, useSession} from 'next-auth/react'
+import {useSession} from 'next-auth/react'
 import {type LoginTemplateProps} from '@skillrecordings/ui/templates/login'
 import Spinner from 'components/spinner'
 import {Button} from '@skillrecordings/ui'
 import Link from 'next/link'
-import {
-  UserSchema,
-  createAppAbility,
-  defineRulesForPurchases,
-} from '@skillrecordings/skill-lesson/utils/ability'
-import {getToken} from 'next-auth/jwt'
-import {getSubscriberFromCookie} from '@skillrecordings/skill-lesson/utils/ck-subscriber-from-cookie'
-import {getProducts} from '@skillrecordings/skill-lesson/lib/products'
-import {getVideoResource} from '@skillrecordings/skill-lesson/lib/video-resources'
+import {createAppAbility} from '@skillrecordings/skill-lesson/utils/ability'
 import {Subscriber} from 'schemas/subscriber'
+import {getPropsForEmbed} from 'utils/get-props-for-embeds'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  // resource
-  const {query} = context
-  const {params} = context
-  const lessonSlug = params?.lesson as string
-  const sectionSlug = params?.section as string
-  const moduleSlug = params?.module as string
-  const module = await getTutorial(moduleSlug)
-  const section = await getSection(sectionSlug)
-  const lesson = await getExercise(lessonSlug, false)
-  const videoResourceId = lesson.videoResourceId
-
-  // ability rules
-  const country =
-    (context.req.headers['x-vercel-ip-country'] as string) ||
-    process.env.DEFAULT_COUNTRY ||
-    'US'
-
-  const token = await getToken({req: context.req})
-  const convertkitSubscriber = await getSubscriberFromCookie(
-    context.req as NextApiRequest,
-  )
-
-  let purchasedModules = []
-
-  if (token) {
-    const user = UserSchema.parse(token)
-    const productsPurchased =
-      user.purchases?.map((purchase) => purchase.productId) || []
-    purchasedModules = await getProducts(productsPurchased)
-  }
-
-  const abilityRules = defineRulesForPurchases({
-    ...(token && {user: UserSchema.parse(token)}),
-    ...(convertkitSubscriber && {
-      subscriber: convertkitSubscriber,
-    }),
-    country,
+  const isSolution = false
+  const isTutorial = true
+  const {
     module,
-    lesson,
     section,
-    purchasedModules,
-    isSolution: lesson._type === 'solution',
-  })
-
-  const ability = createAppAbility(abilityRules || [])
-
-  // can show video
-  const canShowVideo = ability.can('view', 'Content')
-
-  // full video resource
-  const videoResource =
-    canShowVideo && videoResourceId && (await getVideoResource(videoResourceId))
-
-  // login
-  const providers = await getProviders()
-  const csrfToken = await getCsrfToken(context)
-
-  // theme
-  const theme = query.theme || 'dark'
+    lesson,
+    videoResourceId,
+    videoResource,
+    theme,
+    login: {providers, csrfToken},
+    convertkitSubscriber,
+    abilityRules,
+  } = await getPropsForEmbed(context, isSolution, isTutorial)
 
   return {
     props: {
