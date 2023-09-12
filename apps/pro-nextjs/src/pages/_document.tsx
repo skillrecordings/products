260c618b5b8f8dd7698bd429062568b5668fa53e
Vojta Holik <vojta@egghead.io>
2023-09-12 13:22:54 +0200

    update(pn): styles

--- apps/pro-nextjs/src/pages/_document.tsx
+++ apps/pro-nextjs/src/pages/_document.tsx
@@ -4,7 +4,14 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed for ProNextJS.dev"
+          href="/rss.xml"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
