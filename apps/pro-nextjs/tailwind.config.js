85fd70aab437c30b64658de81ad00c256797d4ee
Vojta Holik <vojta@egghead.io>
2023-09-14 13:06:44 +0200

    style(pn): tutorial page ui tweaks

--- apps/pro-nextjs/tailwind.config.js
+++ apps/pro-nextjs/tailwind.config.js
@@ -69,7 +69,7 @@ module.exports = {
       },
       borderRadius: {
         DEFAULT: `var(--radius)`,
-        xl: `calc(var(--radius) + 16px)`,
+        xl: `calc(var(--radius) + 8px)`,
         lg: `var(--radius)`,
         md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
