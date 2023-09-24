0c307352b639e60eb4552d097be78091b7c497f8
Vojta Holik <vojta@egghead.io>
2023-09-23 17:55:28 -0700

    feat(tauto): reinit

--- apps/transcript-automation/src/pages/check-your-email.tsx
+++ apps/transcript-automation/src/pages/check-your-email.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Layout from '@/components/app/layout'
-import {CheckYourEmailTemplate} from '@skillrecordings/skill-lesson/login'
+import {CheckYourEmailTemplate} from '@skillrecordings/ui/templates/login'
 
 const CheckYourEmail = () => {
   return (
