361a083c0d92a63edff84164324faa656004dbbe
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-10-04 00:02:14 +0200

    tweaks

--- apps/testing-javascript/src/path-to-purchase/pricing.tsx
+++ apps/testing-javascript/src/path-to-purchase/pricing.tsx
@@ -73,6 +73,7 @@ type PricingProps = {
   couponId?: string
   allowPurchase?: boolean
   handleViewContents?: () => void
+  isLowerTier?: boolean
 }
 
 /**
@@ -93,6 +94,7 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
   index = 0,
   couponId,
   allowPurchase = false,
+  isLowerTier,
 }) => {
   const [quantity, setQuantity] = React.useState(1)
   const [isBuyingForTeam, setIsBuyingForTeam] = React.useState(false)
@@ -177,7 +179,11 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
   // }
 
   return (
-    <div data-pricing-component data-pricing-product-name={product.name}>
+    <div
+      data-pricing-component
+      data-pricing-product-name={product.name}
+      data-product-is-lower-tier={isLowerTier}
+    >
       {image && (
         <div data-pricing-image-container>
           <Image
@@ -196,7 +202,7 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
           <Ribbon appliedMerchantCoupon={appliedMerchantCoupon} />
         )} */}
         {!purchased && (
-          <div>
+          <div data-product-price-holder>
             {title && (
               <h2 className="font-tt-regular uppercase text-base text-zinc-500 text-center tracking-wider">
                 {title}
@@ -222,13 +228,15 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
               <div data-purchased="">
                 <CheckCircleIcon aria-hidden="true" /> Purchased
               </div>
-              <div className="flex flex-col justify-center">
-                <BuyMoreSeats
-                  productName={name}
-                  productId={productId}
-                  userId={userId as string}
-                />
-              </div>
+              {!isLowerTier && (
+                <div className="flex flex-col justify-center">
+                  <BuyMoreSeats
+                    productName={name}
+                    productId={productId}
+                    userId={userId as string}
+                  />
+                </div>
+              )}
             </div>
           </>
         ) : isSellingLive || allowPurchase ? (
@@ -236,7 +244,7 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
             <div data-downgrade-container="">
               <div data-downgrade="">Unavailable</div>
             </div>
-          ) : (
+          ) : isLowerTier ? null : (
             <div className="mt-4">
               <form
                 action={buildStripeCheckoutPath({
@@ -368,8 +376,9 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
             </div>
           )
         ) : null}
+        {isLowerTier && <div data-lower-tier-message>Not Available</div>}
         {summary && (
-          <div className="mt-6">
+          <div data-product-summary-holder className="mt-6">
             <PortableText value={summary} />
           </div>
         )}
@@ -381,7 +390,7 @@ export const Pricing: React.FC<React.PropsWithChildren<PricingProps>> = ({
             index={index}
           />
         )}
-        <div className="mt-6">
+        <div data-product-details-holder className="mt-6">
           {modules && (
             <>
               <h4 className="font-tt-demibold">Workshops</h4>
@@ -591,4 +600,26 @@ const RegionalPricingBox: React.FC<
       </label>
     </div>
   )
+}
+
+{
+  /*
+{isLowerTier ? (
+                    <div data-lower-tier-message>
+                      You have already purchased a higher tier.
+                    </div>
+                  ) : (
+                    <button
+                      data-pricing-product-checkout-button={product.name}
+                      type="submit"
+                      disabled={status === 'loading' || status === 'error'}
+                    >
+                      <span>
+                        {formattedPrice?.upgradeFromPurchaseId
+                          ? `Upgrade Now`
+                          : action || `Buy Now`}
+                      </span>
+                    </button>
+                  )}
+*/
 }
