a63ed8aef0198fd51028fd53aa443bb746537fe7
Vojta Holik <vojta@egghead.io>
2023-10-05 19:08:12 +0200

    style(ew): update landing hero

--- apps/epic-web/src/pages/index.tsx
+++ apps/epic-web/src/pages/index.tsx
@@ -106,8 +106,8 @@ const Header = () => {
       ref={ref}
       className="relative flex min-h-[108vh] flex-col items-center justify-start overflow-hidden bg-black"
     >
-      <div className="absolute top-[22vh] z-40 mx-auto text-center xl:top-[190px]">
-        <h1 className="max-w-3xl px-5 font-bold text-white shadow-black drop-shadow-lg fluid-3xl sm:leading-tight lg:px-16">
+      <div className="absolute top-[120px] z-40 mx-auto text-center sm:top-[14vh] xl:top-[16vh]">
+        <h1 className="max-w-3xl px-5 font-bold text-white shadow-black drop-shadow-lg fluid-2xl sm:leading-tight sm:fluid-3xl lg:px-16">
           <span className="inline-flex pb-4 font-sans text-sm font-semibold uppercase tracking-wider text-orange-300 shadow-black drop-shadow-md">
             Everything You Need to Know to
           </span>
@@ -135,7 +135,7 @@ const Header = () => {
       />
       <motion.div className="flex h-full w-full items-start justify-center">
         <Image
-          className="absolute z-20 flex w-[500px] -translate-y-10 items-start justify-center sm:w-[700px] sm:-translate-y-24"
+          className="absolute z-20 flex w-[500px] -translate-y-10 items-start justify-center sm:w-[600px] sm:-translate-y-24"
           src={require('../../public/assets/hero/small-planet-compressed.png')}
           width={800}
           alt=""
