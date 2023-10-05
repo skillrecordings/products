8857e3f6e8f8a228ed12c5517db00935a6f3df9d
joel <joelhooks@gmail.com>
2023-10-04 19:26:48 -0700

    add some redirects and such

--- apps/epic-web/src/pages/activate/index.tsx
+++ apps/epic-web/src/pages/activate/index.tsx
@@ -5,15 +5,21 @@ import {first} from 'lodash'
 import {prisma} from '@skillrecordings/database'
 import * as React from 'react'
 import {trpc} from 'trpc/trpc.client'
+import {getToken} from 'next-auth/jwt'
 
 export const getServerSideProps: GetServerSideProps = async ({req, query}) => {
-  // TODO: check for session, redirect to login if not with return url
+  const sessionToken = await getToken({req})
 
-  const deviceVerification = await prisma.deviceVerification.findFirst({
-    where: {
-      userCode: query?.user_code as string,
-    },
-  })
+  const unAuthedCallbackUrl = `/activate%3Fuser_code=${query?.user_code}`
+
+  if (!sessionToken) {
+    return {
+      redirect: {
+        destination: `/login?callbackUrl=${unAuthedCallbackUrl}&message=You must be logged in to activate your device.`,
+        permanent: false,
+      },
+    }
+  }
 
   return {
     props: {
@@ -24,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async ({req, query}) => {
 
 export default function Activate({userCode}: {userCode: string}) {
   const verifyDeviceMutation = trpc.deviceVerification.verify.useMutation()
-
+  const router = useRouter()
   return (
     <div>
       <h1>Activate</h1>
@@ -35,7 +41,24 @@ export default function Activate({userCode}: {userCode: string}) {
             {userCode},
             {
               onSettled: (data) => {
-                console.log(data)
+                switch (data?.status) {
+                  case 'device-verified':
+                    router.push('/activate/success')
+                    break
+                  case 'already-verified':
+                    router.push(
+                      '/activate/failure?message=Device already verified.',
+                    )
+                    break
+                  case 'code-expired':
+                    router.push('/activate/failure?message=Code expired.')
+                    break
+                  case 'no-verification-found':
+                    router.push(
+                      '/activate/failure?message=No verification found.',
+                    )
+                    break
+                }
               },
             },
           )
