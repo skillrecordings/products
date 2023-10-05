804dae159d5112c74b0e6b80a0708599436e05ea
Vojta Holik <vojta@egghead.io>
2023-10-05 16:34:31 +0200

    style(ew): device activation routes

--- apps/epic-web/src/pages/activate/failure.tsx
+++ apps/epic-web/src/pages/activate/failure.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react'
 import {useRouter} from 'next/router'
+import {GrStatusWarning} from 'react-icons/gr'
+import Balancer from 'react-wrap-balancer'
+import Layout from 'components/app/layout'
+import {XIcon} from '@heroicons/react/outline'
+import {XCircleIcon} from '@heroicons/react/solid'
 
 export default function ActivateFailure() {
   const router = useRouter()
 
-  const {message = 'unable to verify.'} = router.query
+  const {message = 'Unable to verify.'} = router.query
 
   return (
-    <div>
-      <h1>Activation Failure</h1>
-      <div>{message} Please try again.</div>
-    </div>
+    <Layout meta={{title: 'Device Activation Failed'}}>
+      <main className="mx-auto flex w-full max-w-lg flex-grow flex-col items-center justify-center pb-24 pt-16">
+        <div className="flex w-full flex-col items-center rounded p-5 text-center">
+          <XCircleIcon className="mb-5 h-10 w-10 text-rose-600 dark:text-rose-400" />
+          <h1 className="text-2xl font-bold sm:text-3xl">
+            Device Activation Failed
+          </h1>
+          <p className="w-full py-4 text-gray-600 dark:text-gray-400">
+            <Balancer>{message} Please try again.</Balancer>
+          </p>
+        </div>
+      </main>
+    </Layout>
   )
 }
--- apps/epic-web/src/pages/activate/success.tsx
+++ apps/epic-web/src/pages/activate/success.tsx
@@ -1,12 +1,28 @@
+import {CheckCircleIcon} from '@heroicons/react/outline'
+import Layout from 'components/app/layout'
+import Icon from 'components/icons'
 import * as React from 'react'
+import Balancer from 'react-wrap-balancer'
 
 export default function ActivateSuccess() {
   return (
-    <div>
-      <h1>Activation Success</h1>
-      <div>
-        You're now logged in to the workshop app. You can close this page.
-      </div>
-    </div>
+    <Layout meta={{title: 'Device Activation Successful'}}>
+      <main className="mx-auto flex w-full max-w-lg flex-grow flex-col items-center justify-center pb-24 pt-16">
+        <div className="flex w-full flex-col items-center rounded p-5 text-center">
+          <Icon
+            name="Checkmark"
+            className="mb-5 h-8 w-8 text-emerald-500 dark:text-emerald-300"
+          />
+          <h1 className="text-2xl font-bold sm:text-3xl">
+            Device Activaction Successful
+          </h1>
+          <p className="py-4 text-gray-600 dark:text-gray-400">
+            <Balancer>
+              You're now logged in to the workshop app. You can close this page.
+            </Balancer>
+          </p>
+        </div>
+      </main>
+    </Layout>
   )
 }
