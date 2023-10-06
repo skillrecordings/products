590be0ce898fb97b61d22fdcd9096d46bb312de9
joel <joelhooks@gmail.com>
2023-10-06 07:13:27 -0700

    adds api endpoints for retrieving video data

---
+++ apps/epic-web/src/lib/lessons.ts
@@ -0,0 +1,56 @@
+import {Purchase, User} from '@skillrecordings/database'
+import {getWorkshop} from 'lib/workshops'
+import {getSection} from 'lib/sections'
+import {getExercise} from 'lib/exercises'
+import {getProducts} from '@skillrecordings/skill-lesson/lib/products'
+import {getCurrentAbility} from '@skillrecordings/skill-lesson'
+import {Lesson} from '@skillrecordings/skill-lesson/schemas/lesson'
+import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
+import groq from 'groq'
+
+type GetLessonProps = {
+  useSolution?: boolean
+  lessonSlug: string
+  moduleSlug: string
+  sectionSlug: string
+  user: User & {purchases: Purchase[]}
+}
+export async function getLessonVideoForDevice({
+  useSolution = false,
+  lessonSlug,
+  moduleSlug,
+  sectionSlug,
+  user,
+}: GetLessonProps) {
+  const module = await getWorkshop(moduleSlug)
+  const section = await getSection(sectionSlug)
+  const lessondata = await getExercise(lessonSlug, false)
+
+  const lesson = useSolution ? (lessondata.solution as Lesson) : lessondata
+
+  const productsPurchased =
+    user.purchases?.map((purchase) => purchase.productId) || []
+  const purchasedModules = await getProducts(productsPurchased)
+
+  const ability = getCurrentAbility({
+    user,
+    purchasedModules,
+    lesson,
+    section,
+    module,
+  })
+
+  if (ability.can('view', 'Content')) {
+    const videoResource = await sanityClient.fetch(
+      groq`*[_type in ['videoResource'] && _id == $id][0]{
+      "transcript": transcript.text,
+      "muxPlaybackId": muxAsset.muxPlaybackId
+    }`,
+      {id: lesson.videoResourceId as string},
+    )
+
+    return {
+      ...videoResource,
+    }
+  }
+}
---
+++ apps/epic-web/src/server/lesson-for-device-req.ts
@@ -0,0 +1,43 @@
+import {getToken} from 'next-auth/jwt'
+import {getLessonVideoForDevice} from 'lib/lessons'
+import {NextApiRequest, NextApiResponse} from 'next'
+import {loadUserForToken} from 'lib/users'
+
+export async function lessonForDeviceReq({
+  req,
+  res,
+  isSolution = false,
+}: {
+  req: NextApiRequest
+  res: NextApiResponse
+  isSolution?: boolean
+}) {
+  if (req.method === 'GET') {
+    const deviceToken = req.headers.authorization?.split(' ')[1]
+    const token = await getToken({req})
+    const user = await loadUserForToken({token, deviceToken})
+    const lessonSlug = req.query.lesson as string
+    const moduleSlug = req.query.module as string
+    const sectionSlug = req.query.section as string
+
+    if (user) {
+      const lessonForDevice = await getLessonVideoForDevice({
+        lessonSlug,
+        moduleSlug,
+        sectionSlug,
+        user,
+        useSolution: isSolution,
+      })
+
+      if (lessonForDevice) {
+        res.status(200).json(lessonForDevice)
+      } else {
+        res.status(403).end()
+      }
+    } else {
+      res.status(403).end()
+    }
+  } else {
+    res.status(200).end()
+  }
+}
