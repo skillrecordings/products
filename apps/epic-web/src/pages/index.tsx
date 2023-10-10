54279ca44fd71d392de9e0e267219cda8f60a749
Vojta Holik <vojta@egghead.io>
2023-10-10 09:50:14 +0200

    feat(ew): commerce on HP

--- apps/epic-web/src/pages/index.tsx
+++ apps/epic-web/src/pages/index.tsx
@@ -25,11 +25,19 @@ import {useCoupon} from '@skillrecordings/skill-lesson/path-to-purchase/use-coup
 import {useRouter} from 'next/router'
 import {getProduct} from 'lib/products'
 import {SanityProduct} from '@skillrecordings/commerce-server/dist/@types'
+import {Pricing} from '@skillrecordings/skill-lesson/path-to-purchase/pricing'
+import {getAllProducts} from '@skillrecordings/skill-lesson/lib/products'
+import {PriceCheckProvider} from '@skillrecordings/skill-lesson/path-to-purchase/pricing-check-context'
+import {Sparkles} from './buy'
 
 const productId = process.env.NEXT_PUBLIC_DEFAULT_PRODUCT_ID
 
-const Index: NextPage<{product: SanityProduct}> = ({product}) => {
+const Index: NextPage<{product: SanityProduct; products: SanityProduct[]}> = ({
+  product,
+  products,
+}) => {
   const router = useRouter()
+  const ALLOW_PURCHASE = router.query.allowPurchase === 'true'
   const {subscriber, loadingSubscriber} = useConvertkit()
   const {data: commerceProps, status: commercePropsStatus} =
     trpc.pricing.propsForCommerce.useQuery({
@@ -65,7 +73,64 @@ const Index: NextPage<{product: SanityProduct}> = ({product}) => {
         <main className="">
           {redeemableCoupon ? <RedeemDialogForCoupon /> : null}
           <Article />
-          <Subscribe subscriber={subscriber} />
+          {true ? (
+            <section className="relative mt-16 flex flex-col items-center justify-start dark:bg-black/30">
+              <div className="flex flex-col items-center justify-center py-16">
+                <h2 className="max-w-lg text-center text-2xl font-bold sm:text-3xl lg:text-4xl">
+                  <Balancer>
+                    Become a Professional Full Stack Web Developer
+                  </Balancer>
+                </h2>
+                <h3 className="max-w-lg pt-5 text-center text-lg text-gray-600 dark:text-gray-400">
+                  <Balancer>
+                    The most comprehensive guide to professional web development
+                    by Kent C. Dodds.
+                  </Balancer>
+                </h3>
+              </div>
+              <div
+                id="buy"
+                className="relative flex flex-col items-center justify-start"
+              >
+                {/* <Sparkles /> */}
+                {products
+                  ?.filter((product: any) => product.state !== 'unavailable')
+                  .map((product, i) => {
+                    return (
+                      <PriceCheckProvider
+                        key={product.slug}
+                        purchasedProductIds={purchasedProductIds}
+                      >
+                        <div data-pricing-container="" key={product.name}>
+                          <Pricing
+                            allowPurchase={ALLOW_PURCHASE}
+                            userId={commerceProps?.userId}
+                            product={product}
+                            purchased={purchasedProductIds.includes(
+                              product.productId,
+                            )}
+                            purchases={commerceProps?.purchases}
+                            index={i}
+                            couponId={couponId}
+                          />
+                        </div>
+                      </PriceCheckProvider>
+                    )
+                  })}
+              </div>
+              {ALLOW_PURCHASE ? (
+                <Image
+                  className="-mt-16 mb-16"
+                  src="https://res.cloudinary.com/total-typescript/image/upload/v1669928567/money-back-guarantee-badge-16137430586cd8f5ec2a096bb1b1e4cf_o5teov.svg"
+                  width={130}
+                  height={130}
+                  alt="30-Day Money Back Guarantee"
+                />
+              ) : null}
+            </section>
+          ) : (
+            <Subscribe subscriber={subscriber} />
+          )}
           <AboutKent />
         </main>
       </Layout>
@@ -324,9 +389,11 @@ export default Index
 export const getStaticProps: GetStaticProps = async () => {
   const sanityProduct = await getProduct(productId as string)
 
+  const products = await getAllProducts()
   return {
     props: {
       product: sanityProduct,
+      products,
     },
   }
 }
