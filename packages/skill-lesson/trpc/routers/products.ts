0f78ab4e7a23c6d5096d0ec4949e80cd2ea0a847
joel <joelhooks@gmail.com>
2023-10-16 08:53:22 -0700

    remove products router

--- packages/skill-lesson/trpc/routers/products.ts
+++ packages/skill-lesson/trpc/routers/products.ts
@@ -1,8 +1,21 @@
 import {publicProcedure, router} from '../trpc.server'
-import {getActiveProducts} from '../../path-to-purchase/products.server'
+import {getProduct} from '../../path-to-purchase/products.server'
 import {getAllProducts} from '../../lib/products'
+import {z} from 'zod'
 
 export const productsRouter = router({
+  getProductById: publicProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+      }),
+    )
+    .query(async ({ctx, input}) => {
+      const {productId} = input
+      const product = await getProduct(productId)
+
+      return product
+    }),
   getAllProducts: publicProcedure.query(async () => {
     const products = await getAllProducts()
 
