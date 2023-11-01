2bb4d9375046ea8cf041d1d6c4ce7aa3eca1196f
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-11-01 18:14:02 +0100

    rewrite getTranscriptComponents

--- packages/skill-lesson/markdown/transcript-components.tsx
+++ packages/skill-lesson/markdown/transcript-components.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import {hmsToSeconds} from '@skillrecordings/time'
 
 /**
@@ -18,39 +19,45 @@ const getTranscriptComponents = ({
 }) => {
   return {
     p: ({children}: any) => {
-      const text = children.toString()
-      const timestampRegex = /(\d+:\d+)/
-      const matches = text.match(timestampRegex)
-
-      if (matches) {
-        const timestamp = matches[1]
-        const beforeText = text.split(matches[0])[0]
-        const afterText = text.split(matches[0])[1]
-        return (
-          <p>
-            {beforeText.replace('[', '')}
-            {canShowVideo ? (
-              <button
-                data-timestamp=""
-                className="after:content-[' '] inline-block underline after:inline-block"
-                onClick={() => {
-                  if (muxPlayerRef.current) {
-                    muxPlayerRef.current.currentTime = hmsToSeconds(timestamp)
-                    handlePlay()
-                    window.scrollTo({top: 0})
-                  }
-                }}
-              >
-                {timestamp}
-              </button>
-            ) : (
-              timestamp
-            )}
-            {afterText.replace(']', '')}
-          </p>
-        )
-      }
-      return <p>{text}</p>
+      const elements = React.Children.toArray(children)
+      const updatedChildren = elements.map((child, index) => {
+        if (typeof child === 'string') {
+          const text = child
+          const timestampRegex = /(\d+:\d+)/
+          const matches = text.match(timestampRegex)
+          if (matches) {
+            const timestamp = matches[1]
+            const beforeText = text.split(matches[0])[0]
+            const afterText = text.split(matches[0])[1]
+            return (
+              <span key={index}>
+                {beforeText.replace('[', '')}
+                {canShowVideo ? (
+                  <button
+                    data-timestamp=""
+                    className="after:content-[' '] inline-block underline after:inline-block"
+                    onClick={() => {
+                      if (muxPlayerRef.current) {
+                        muxPlayerRef.current.currentTime =
+                          hmsToSeconds(timestamp)
+                        handlePlay()
+                        window.scrollTo({top: 0})
+                      }
+                    }}
+                  >
+                    {timestamp}
+                  </button>
+                ) : (
+                  timestamp
+                )}
+                {afterText.replace(']', '')}
+              </span>
+            )
+          }
+        }
+        return child
+      })
+      return <p>{updatedChildren}</p>
     },
   }
 }
