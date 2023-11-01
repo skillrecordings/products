544099c0fcf916e02741ed22ee63449735f728b4
joel <joelhooks@gmail.com>
2023-11-01 10:35:33 -0700

    change cta

--- apps/devrel-fyi/src/text/common.ts
+++ apps/devrel-fyi/src/text/common.ts
@@ -1,5 +1,5 @@
 export default {
-  'primary-newsletter-tittle': 'Supercharge Your DevRel Career',
+  'primary-newsletter-tittle': 'Supercharge Your DevRel Team',
   'primary-newsletter-byline': '',
   'primary-newsletter-button-cta-label': 'Get Exclusive DevRel Insights',
 }
