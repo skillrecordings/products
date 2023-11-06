e2bebb59a109e4616c090b02499aaea5011523d9
jbranchaud <jbranchaud@gmail.com>
2023-11-06 15:42:40 -0600

    feat(tjs): add interview pages

--- apps/testing-javascript/src/lib/interviews.ts
+++ apps/testing-javascript/src/lib/interviews.ts
@@ -1,5 +1,6 @@
 import groq from 'groq'
 import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
+import {z} from 'zod'
 
 export const getAllInterviews = async () =>
   await sanityClient.fetch(groq`*[_type == "interview"] | order(_createdAt asc) {
@@ -11,3 +12,60 @@ export const getAllInterviews = async () =>
   slug,
   isMultiple
 }`)
+
+const ExternalImageSchema = z.object({
+  _type: z.literal('externalImage'),
+  alt: z.string(),
+  url: z.string(),
+})
+
+const SanityBlockSchema = z.array(
+  z.object({
+    style: z.literal('normal'),
+    _type: z.literal('block'),
+    children: z.array(
+      z.object({
+        _type: z.literal('span'),
+        marks: z.array(z.any()),
+        _key: z.string(),
+        text: z.string(),
+      }),
+    ),
+    markDefs: z.array(z.any()),
+    _key: z.string(),
+  }),
+)
+
+const InterviewSchema = z.object({
+  _id: z.string(),
+  slug: z.object({current: z.string()}),
+  title: z.string(),
+  description: SanityBlockSchema,
+  portraits: z.object({
+    image1: ExternalImageSchema,
+    image2: ExternalImageSchema,
+  }),
+  videoResource: z.object({
+    _id: z.string(),
+    muxPlaybackId: z.string(),
+    transcript: z.string(),
+  }),
+})
+
+export const getInterview = async (slug: string) => {
+  const interviewQuery = groq`*[_type == "interview" && slug.current == $slug][0]{
+    _id,
+    slug,
+    title,
+    description,
+    portraits,
+    "videoResource": resources[@->._type == 'videoResource'][0]->{
+      _id,
+      "muxPlaybackId": muxAsset.muxPlaybackId,
+      "transcript": castingwords.transcript
+    }
+  }`
+  const response = await sanityClient.fetch(interviewQuery, {slug})
+
+  return InterviewSchema.parse(response)
+}
---
+++ apps/testing-javascript/src/templates/interview-template.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import {useRouter} from 'next/router'
+import {type MuxPlayerRefAttributes} from '@mux/mux-player-react'
+import {Video} from '@skillrecordings/skill-lesson/video/video'
+import {VideoProvider} from '@skillrecordings/skill-lesson/hooks/use-mux-player'
+import {useVideoResource} from '@skillrecordings/skill-lesson/hooks/use-video-resource'
+import {
+  customPlayFromBeginningHandler,
+  customContinueHandler,
+} from 'utils/custom-handlers'
+import {VideoTranscript} from '@skillrecordings/skill-lesson/video/video-transcript'
+import Spinner from 'components/spinner'
+
+type Interview = {
+  title: string
+  slug: {
+    current: string
+  }
+}
+
+const InterviewTemplate = ({interview}: {interview: Interview}) => {
+  const router = useRouter()
+  const {videoResource, loadingVideoResource, videoResourceId} =
+    useVideoResource()
+  const muxPlayerRef = React.useRef<MuxPlayerRefAttributes>(null)
+
+  return (
+    <VideoProvider
+      muxPlayerRef={muxPlayerRef}
+      exerciseSlug={interview.slug.current}
+      handleContinue={customContinueHandler}
+      handlePlayFromBeginning={customPlayFromBeginningHandler}
+    >
+      <div className="container max-w-6xl pb-8 pt-4 md:pb-12 md:pt-6 lg:pb-16">
+        <main className="relative mx-auto w-full items-start border-t border-transparent 2xl:flex 2xl:max-w-none 2xl:border-gray-800">
+          <div className="flex flex-col border-gray-800 2xl:relative 2xl:h-full 2xl:w-full 2xl:border-r">
+            <Video ref={muxPlayerRef} loadingIndicator={<Spinner />} />
+          </div>
+        </main>
+        <div className="mt-12 flex flex-col-reverse lg:flex-row">
+          <div className="mt-12 grow lg:mt-0">
+            <h2 className="hidden text-[2.125rem] leading-tight lg:block">
+              {interview.title}
+            </h2>
+            <article className="lg:mt-8">
+              <div className="prose prose-md">
+                <VideoTranscript transcript={videoResource?.transcript || ''} />
+              </div>
+            </article>
+          </div>
+          <div className="w-full shrink-0 lg:ml-8 lg:max-w-[350px]">
+            <h2 className="mb-8 block text-[1.625rem] leading-tight md:text-[2.125rem] lg:hidden">
+              {interview.title}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </VideoProvider>
+  )
+}
+
+export default InterviewTemplate
