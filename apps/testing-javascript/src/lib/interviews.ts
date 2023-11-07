2b23014c01c823a13ca49cb63d6cda3323bde800
jbranchaud <jbranchaud@gmail.com>
2023-11-07 14:59:56 -0600

    feat(tjs): interviews have module/section/lessons

--- apps/testing-javascript/src/lib/interviews.ts
+++ apps/testing-javascript/src/lib/interviews.ts
@@ -1,17 +1,9 @@
 import groq from 'groq'
 import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
 import {z} from 'zod'
-
-export const getAllInterviews = async () =>
-  await sanityClient.fetch(groq`*[_type == "interview"] | order(_createdAt asc) {
-  _id,
-  _type,
-  title,
-  description,
-  portraits,
-  slug,
-  isMultiple
-}`)
+import type {Module} from '@skillrecordings/skill-lesson/schemas/module'
+import type {Section} from '@skillrecordings/skill-lesson/schemas/section'
+import type {Lesson} from '@skillrecordings/skill-lesson/schemas/lesson'
 
 const ExternalImageSchema = z.object({
   _type: z.literal('externalImage'),
@@ -36,34 +28,113 @@ const SanityBlockSchema = z.array(
   }),
 )
 
-const InterviewSchema = z.object({
-  _id: z.string(),
-  slug: z.object({current: z.string()}),
-  title: z.string(),
-  description: SanityBlockSchema,
-  portraits: z.object({
-    image1: ExternalImageSchema,
-    image2: ExternalImageSchema,
-  }),
-  videoResource: z.object({
+const InterviewSchema = z
+  .object({
     _id: z.string(),
-    muxPlaybackId: z.string(),
-    transcript: z.string(),
-  }),
-})
+    _type: z.string(),
+    slug: z.string(),
+    title: z.string(),
+    description: SanityBlockSchema,
+    portraits: z.object({
+      image1: ExternalImageSchema,
+      image2: ExternalImageSchema.optional(),
+    }),
+    resources: z
+      .object({
+        videoResourceId: z.string(),
+        _type: z.string(),
+        slug: z.string(),
+        title: z.string(),
+        _updatedAt: z.string(),
+        description: z.string().optional(),
+        body: z.string().optional(),
+      })
+      .array(),
+    videoResourceId: z.string().optional(),
+    _updatedAt: z.string().optional(),
+  })
+  .transform(({description, ...rest}) => {
+    const descriptionAsString = description
+      .map((block) => {
+        return block.children[0].text
+      })
+      .join('\n\n')
+
+    return {...rest, description: descriptionAsString}
+  })
+
+export const getAllInterviews = async () =>
+  await sanityClient.fetch(groq`*[_type == "interview"] | order(_createdAt asc) {
+  _id,
+  _type,
+  title,
+  description,
+  portraits,
+  slug,
+  isMultiple
+}`)
+
+export const getInterviewModule = async () => {
+  const interviewsQuery = groq`*[_type == "interview"] | order(_createdAt asc) {
+    _id,
+    _type,
+    "slug": slug.current,
+    title,
+    description,
+    portraits,
+    "resources": resources[@->._type == 'videoResource'][]->{
+      "videoResourceId": _id,
+      _type,
+      "slug": slug.current,
+      title,
+      _updatedAt
+    },
+    "videoResourceId": resources[@->._type == 'videoResource'][0]->_id,
+    _updatedAt
+  }`
+  const response = await sanityClient.fetch(interviewsQuery)
+  console.log(response)
+  const lessons = InterviewSchema.array().parse(response)
+
+  const sections: Section[] = [
+    {
+      _type: 'section',
+      slug: 'interviews-section',
+      title: 'Interviews',
+      lessons,
+    },
+  ]
+
+  const module: Module = {
+    slug: {current: 'interviews-module'},
+    _type: 'module',
+    title: 'Interviews',
+    moduleType: 'bonus',
+    image: null,
+    sections,
+    lessons,
+  }
+
+  return module
+}
 
 export const getInterview = async (slug: string) => {
   const interviewQuery = groq`*[_type == "interview" && slug.current == $slug][0]{
     _id,
-    slug,
+    _type,
+    "slug": slug.current,
     title,
     description,
     portraits,
-    "videoResource": resources[@->._type == 'videoResource'][0]->{
-      _id,
-      "muxPlaybackId": muxAsset.muxPlaybackId,
-      "transcript": castingwords.transcript
-    }
+    "resources": resources[@->._type == 'videoResource'][]->{
+      "videoResourceId": _id,
+      _type,
+      "slug": slug.current,
+      title,
+      _updatedAt
+    },
+    "videoResourceId": resources[@->._type == 'videoResource'][0]->_id,
+    _updatedAt
   }`
   const response = await sanityClient.fetch(interviewQuery, {slug})
 
