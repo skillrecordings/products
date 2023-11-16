bdb49ea30b76f879827d424a1cf7ff44bcf13359
Vojta Holik <vojta@egghead.io>
2023-11-16 13:56:04 +0100

    update(template): sync up styles

--- packages/ui/primitives/calendar.tsx
+++ packages/ui/primitives/calendar.tsx
@@ -5,7 +5,7 @@ import {ChevronLeft, ChevronRight} from 'lucide-react'
 import {DayPicker} from 'react-day-picker'
 
 import {cn} from '../utils/cn'
-import {defaultButtonVariants} from './button'
+import {buttonVariants} from './button'
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -26,7 +26,7 @@ function Calendar({
         caption_label: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
-          defaultButtonVariants({variant: 'outline'}),
+          buttonVariants({variant: 'outline'}),
           'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
         ),
         nav_button_previous: 'absolute left-1',
@@ -38,7 +38,7 @@ function Calendar({
         row: 'flex w-full mt-2',
         cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
-          defaultButtonVariants({variant: 'ghost'}),
+          buttonVariants({variant: 'ghost'}),
           'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
         ),
         day_selected:
