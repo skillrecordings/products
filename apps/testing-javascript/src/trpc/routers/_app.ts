83b876e117409f71bf683be4af5693007ec30fb7
jbranchaud <jbranchaud@gmail.com>
2023-11-20 10:08:12 -0600

    feat(tjs): load and render code URLs

--- apps/testing-javascript/src/trpc/routers/_app.ts
+++ apps/testing-javascript/src/trpc/routers/_app.ts
@@ -5,11 +5,13 @@ import {
 } from '@skillrecordings/skill-lesson'
 import {abilitiesRouter} from './abilities'
 import {productsRouter} from './products'
+import {lessonsRouter} from './lessons'
 
 export const appRouter = mergeRouters(
   router({
     abilities: abilitiesRouter,
     'testingJavascript.products': productsRouter,
+    'testingJavascript.lessons': lessonsRouter,
   }),
   skillLessonRouter,
 )
---
+++ apps/testing-javascript/src/trpc/routers/lessons.ts
@@ -0,0 +1,22 @@
+import {publicProcedure, router} from '@skillrecordings/skill-lesson'
+import {getLessonCodeUrls} from 'server/lessons.server'
+import {z} from 'zod'
+
+export const lessonsRouter = router({
+  getLessonCodeUrls: publicProcedure
+    .input(z.object({_id: z.string().optional()}))
+    .query(async ({input}) => {
+      const {_id} = input
+
+      if (!_id)
+        return {
+          github_branch_url: undefined,
+          github_diff_url: undefined,
+          codesandbox_url: undefined,
+        }
+
+      const codeUrls = await getLessonCodeUrls({_id})
+
+      return codeUrls
+    }),
+})
