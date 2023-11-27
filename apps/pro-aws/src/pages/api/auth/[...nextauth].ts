e79ce08219f96698794a8884d962e31c0acabf0f
joel <joelhooks@gmail.com>
2023-11-27 14:59:51 -0800

    feat: adds github provider

--- apps/pro-aws/src/pages/api/auth/[...nextauth].ts
+++ apps/pro-aws/src/pages/api/auth/[...nextauth].ts
@@ -2,14 +2,25 @@ import NextAuth, {NextAuthOptions, Theme} from 'next-auth'
 import {createOptions} from '@skillrecordings/skill-api'
 import {NextApiRequest, NextApiResponse} from 'next'
 import {withSentry} from '@sentry/nextjs'
+import GitHubProvider from 'next-auth/providers/github'
 
 const productTheme: Theme = {
   colorScheme: 'auto',
   brandColor: '#F59E0B',
+  logo: 'https://res.cloudinary.com/dnwi9abmc/image/upload/dpr_auto,f_auto,q_auto/v1701125707/proawsdev-badge_2x_wj57ri.png',
 }
+
+const providers = [
+  GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    allowDangerousEmailAccountLinking: true,
+  }),
+]
 
 export const nextAuthOptions: NextAuthOptions = createOptions({
   theme: productTheme,
+  providers,
 })
 
 export default async function NextAuthEndpoint(
