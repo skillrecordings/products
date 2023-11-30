ef088bbe9e3476b99a890f7c72bbf614a70dd39a
joel <joelhooks@gmail.com>
2023-11-29 16:28:13 -0800

    chore: turn on sentry profiling node loader

--- apps/epic-web/next.config.js
+++ apps/epic-web/next.config.js
@@ -48,6 +48,20 @@ const nextConfig = {
       },
     ]
   },
+  webpack: (config, {dev, isServer, webpack, nextRuntime}) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: 'nextjs-node-loader',
+          options: {
+            outputPath: config.output.path,
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 const configWithPlugins = withAxiom(
--- apps/total-typescript/next.config.js
+++ apps/total-typescript/next.config.js
@@ -46,6 +46,20 @@ const nextConfig = {
   async redirects() {
     return []
   },
+  webpack: (config, {dev, isServer, webpack, nextRuntime}) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: 'nextjs-node-loader',
+          options: {
+            outputPath: config.output.path,
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 const configWithPlugins = withAxiom(withMDX(nextConfig))
