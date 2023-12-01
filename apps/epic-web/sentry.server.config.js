269968e81e2a02196cb2fdd9d3f75623b14af7e4
Joel Hooks <joelhooks@gmail.com>
2023-12-01 07:25:40 -0800

    Update sentry.server.config.js

--- apps/epic-web/sentry.server.config.js
+++ apps/epic-web/sentry.server.config.js
@@ -3,7 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
-import {ProfilingIntegration} from '@sentry/profiling-node'
+//import {ProfilingIntegration} from '@sentry/profiling-node'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
@@ -14,11 +14,11 @@ Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   ignoreErrors: ['ResizeObserver'],
-  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-  integrations: [
+  //profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+  //integrations: [
     // Add profiling integration to list of integrations
-    new ProfilingIntegration(),
-  ],
+    
+  //],
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
