27994ff821354a3fd637c46968fcf9c486b75546
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-12-14 16:37:48 +0100

    example

--- packages/ui/primitives/progress.tsx
+++ packages/ui/primitives/progress.tsx
@@ -11,8 +11,9 @@ const Progress = React.forwardRef<
 >(({className, value, ...props}, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
+    data-progress-bar=""
     className={cn(
-      'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
+      'relative h-2 w-full overflow-hidden rounded-full bg-secondary',
       className,
     )}
     {...props}
@@ -20,10 +21,30 @@ const Progress = React.forwardRef<
     <ProgressPrimitive.Indicator
       data-indicator=""
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{transform: `translateX(-${100 - (value || 0)}%)`}}
+      style={{transform: `translateX(-${value}%)`}}
     />
   </ProgressPrimitive.Root>
 ))
+// const Progress = React.forwardRef<
+//   React.ElementRef<typeof ProgressPrimitive.Root>,
+//   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+// >(({className, value, ...props}, ref) => (
+//   <ProgressPrimitive.Root
+//     ref={ref}
+//     data-progress-bar=""
+//     className={cn(
+//       'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
+//       className,
+//     )}
+//     {...props}
+//   >
+//     <ProgressPrimitive.Indicator
+//       data-indicator=""
+//       className="h-full w-full flex-1 bg-primary transition-all"
+//       style={{transform: `translateX(-${100 - (value || 0)}%)`}}
+//     />
+//   </ProgressPrimitive.Root>
+// ))
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export {Progress}
