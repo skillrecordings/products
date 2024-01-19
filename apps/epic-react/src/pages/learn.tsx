132258ee105193ff1ab536ea7416c068da808adf
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-01-19 15:26:48 +0100

    output Bonuses module on the /learn page bonuses

--- apps/epic-react/src/pages/learn.tsx
+++ apps/epic-react/src/pages/learn.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/app/layout'
 import {getAllWorkshops} from '@/lib/workshops'
+import {getAllBonuses} from '@/lib/bonuses'
 import {GetStaticProps} from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -10,15 +11,18 @@ import {getOgImage} from '@/utils/get-og-image'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const workshops = await getAllWorkshops()
+  const bonuses = await getAllBonuses()
 
   return {
-    props: {workshops},
+    props: {workshops, bonuses},
     revalidate: 10,
   }
 }
 
-const Learn: React.FC<{workshops: any[]}> = ({workshops}) => {
-  console.log({WORKSHOPS: workshops})
+const Learn: React.FC<{workshops: any[]; bonuses: any[]}> = ({
+  workshops,
+  bonuses,
+}) => {
   const title = 'Learn'
 
   return (
@@ -55,6 +59,31 @@ const Learn: React.FC<{workshops: any[]}> = ({workshops}) => {
                           className="block"
                         >
                           {resource.title}
+                        </Link>
+                      )
+                    })}
+                  </ul>
+                </div>
+              </li>
+            )
+          })}
+          {bonuses.map((bonus: any) => {
+            return (
+              <li key={bonus._id} className="flex space-x-6">
+                <div className="shrink-0">
+                  <Image src={bonus.image} alt="" width={200} height={200} />
+                </div>
+                <div className="space-y-3">
+                  <h3 className="text-2xl">{bonus.title}</h3>
+                  <ul>
+                    {bonus.lessons.map((lesson: any) => {
+                      return (
+                        <Link
+                          key={lesson._id}
+                          href={`/workshops/${bonus.slug.current}/${lesson.slug}`}
+                          className="block"
+                        >
+                          {lesson.title}
                         </Link>
                       )
                     })}
