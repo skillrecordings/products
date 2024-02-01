b9d0ac369401a3304a3e7401a6ecce1e273ca62b
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-01 15:56:09 +0100

    remove deprecated studio

--- apps/badass/___studio/src/structure/modules.js
+++
@@ -1,9 +0,0 @@
-import S from '@sanity/desk-tool/structure-builder'
-import {MdOutlineViewModule} from 'react-icons/md'
-
-const modules = S.listItem()
-  .title('Modules')
-  .icon(MdOutlineViewModule)
-  .child(S.documentTypeList('module').title('All Modules'))
-
-export default modules
--- apps/badass/___studio/src/structure/podcasts.js
+++
@@ -1,9 +0,0 @@
-import S from '@sanity/desk-tool/structure-builder'
-import {FaPodcast} from 'react-icons/fa'
-
-const products = S.listItem()
-  .title('Podcasts')
-  .icon(FaPodcast)
-  .child(S.documentTypeList('podcast').title('All Podcasts'))
-
-export default products
