6687f4b0b70f1ee22c6f540356c2032622a8670e
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-01 15:56:09 +0100

    remove deprecated studio

--- apps/badass/___studio/src/components/logo/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './styles.css'
-
-const Logo = () => (
-  <img className={styles.logo} src="/static/logo@2x.png" alt="Badass Courses" />
-)
-
-export default Logo
