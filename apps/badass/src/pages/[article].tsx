e6e5adcb1d3c6ea2fc4b8354f8b528f745609bdc
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-16 03:02:50 +0100

    markdown for the Articles

--- apps/badass/src/pages/[article].tsx
+++ apps/badass/src/pages/[article].tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {GetStaticPaths, GetStaticProps} from 'next'
+import serializeMdx from '@skillrecordings/skill-lesson/markdown/serialize-mdx'
+import {type MDXRemoteSerializeResult} from 'next-mdx-remote'
+
 import ArticleTemplate from 'templates/article-template'
 import {Article, getAllArticles, getArticle} from 'lib/articles'
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
   const article = await getArticle(params?.article as string)
+  const articleBodySerialized = article?.markdownBody
+    ? await serializeMdx(article.markdownBody, {
+        syntaxHighlighterOptions: {
+          theme: 'one-dark-pro',
+        },
+      })
+    : null
 
   if (!article) {
     return {
@@ -15,6 +25,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   return {
     props: {
       article,
+      articleBodySerialized,
     },
     revalidate: 10,
   }
@@ -35,12 +46,19 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 
 type ArticlePageProps = {
   article: Article
+  articleBodySerialized: MDXRemoteSerializeResult
 }
 
 const ArticlePage: React.FC<React.PropsWithChildren<ArticlePageProps>> = ({
   article,
+  articleBodySerialized,
 }) => {
-  return <ArticleTemplate article={article} />
+  return (
+    <ArticleTemplate
+      article={article}
+      articleBodySerialized={articleBodySerialized}
+    />
+  )
 }
 
 export default ArticlePage
