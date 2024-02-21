8d2ffca368a0b47a90db90e55aa0003f27ed93af
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-21 12:08:55 +0100

    clean up CaseStudy template

--- apps/badass/src/templates/case-study-template.tsx
+++ apps/badass/src/templates/case-study-template.tsx
@@ -56,8 +56,8 @@ const CaseStudyTemplate: React.FC<
         caseStudySlug={slug}
       />
       <main data-template-case-study={slug} className="pb-10">
-        <div className="max-w-screen-md lg:max-w-[880px] lg:px-14 mx-auto w-full">
-          <div className="md:pt-16 pt-10 lg:px-0 px-5 pb-16">
+        <div className="container">
+          <div className="max-w-screen-md mx-auto md:pt-16 pt-10 pb-16">
             <article className="prose lg:prose-xl sm:prose-lg md:prose-code:text-sm max-w-none prose-p:text-neutral-200 prose-pre:prose-code:bg-transparent prose-code:bg-white/20 prose-code:px-1 prose-code:py-0.5 prose-code:rounded lg:prose-code:text-[78%] sm:prose-code:text-[80%] mb-2 font-medium">
               <MDX
                 components={mdxComponents}
