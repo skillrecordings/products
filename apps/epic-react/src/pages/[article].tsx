95f260e067ebe41f1d6a05a242628dd892079f67
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-22 22:02:14 +0100

    article page & article template

--- apps/epic-react/src/pages/[article].tsx
+++ apps/epic-react/src/pages/[article].tsx
@@ -1,38 +1,81 @@
-import React from 'react'
+import * as React from 'react'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import {GetStaticPaths, GetStaticProps} from 'next'
-import {type Article, getAllArticles, getArticle} from '@/lib/articles'
 import ArticleTemplate from '@/templates/article-template'
 import {MDXRemoteSerializeResult} from 'next-mdx-remote'
 import serializeMDX from '@skillrecordings/skill-lesson/markdown/serialize-mdx'
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const {params} = context
-  const article = await getArticle(params?.article as string)
-  if (!article) {
-    return {
-      notFound: true,
-    }
+const MDX_ARTICLES_FOLDER = 'src/content/articles'
+
+export interface FrontMatter {
+  title: string
+  slug: string
+  date: string
+  image: string
+  socialImage: string
+  imageAlt?: string
+  excerpt: string
+  keywords?: string[] | null
+}
+
+export interface ArticleProps {
+  mdx: MDXRemoteSerializeResult
+  frontMatter: FrontMatter
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const filePath = path.join(
+    process.cwd(),
+    MDX_ARTICLES_FOLDER,
+    `${params?.article as string}.mdx`,
+  )
+  const fileContents = fs.readFileSync(filePath, 'utf8')
+  const {content, data} = matter(fileContents)
+  if (data.date instanceof Date) {
+    data.date = data.date.toISOString()
   }
-  const mdx = await serializeMDX(article.body)
+  const keywords = Array.isArray(data.keywords) ? data.keywords : null
+  const frontMatter: FrontMatter = {
+    title: data.title,
+    slug: data.slug,
+    date: data?.date,
+    image: data?.image,
+    socialImage: data?.socialImage,
+    imageAlt: data?.imageAlt,
+    excerpt: data.excerpt,
+    keywords,
+  }
+  const mdxSource = await serializeMDX(content)
+
   return {
-    props: {article, mdx},
-    revalidate: 10,
+    props: {
+      mdx: mdxSource,
+      frontMatter,
+    },
   }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const articles = await getAllArticles()
-  const paths = articles.map((article: Article) => ({
-    params: {article: article.slug},
-  }))
-  return {paths, fallback: 'blocking'}
+  const postsDirectory = path.join(process.cwd(), MDX_ARTICLES_FOLDER)
+  const filenames = fs.readdirSync(postsDirectory)
+  const paths = filenames.map((filename) => {
+    return {
+      params: {
+        article: filename.replace(/\.mdx$/, ''),
+      },
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
 }
 
-const Article: React.FC<{article: Article; mdx: MDXRemoteSerializeResult}> = ({
-  article,
-  mdx,
-}) => {
-  return article ? <ArticleTemplate article={article} mdx={mdx} /> : null
+const Article: React.FC<ArticleProps> = ({mdx, frontMatter}) => {
+  return <ArticleTemplate mdx={mdx} frontMatter={frontMatter} />
 }
 
 export default Article
