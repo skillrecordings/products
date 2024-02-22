6d67f9141ed31e820dc8e602a4d862ee3d6d3705
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-02-23 00:19:32 +0100

    articles page

--- apps/epic-react/src/pages/articles.tsx
+++ apps/epic-react/src/pages/articles.tsx
@@ -1,87 +1,91 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Layout from '@/components/app/layout'
-import {getAllArticles, type Article} from '@/lib/articles'
-import {GetStaticProps} from 'next'
+import {GetStaticProps, NextPage} from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-import config from '@/config'
-import {track} from '@skillrecordings/skill-lesson/utils/analytics'
-import Header from '@/components/app/header'
-import {getOgImage} from '@/utils/get-og-image'
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const articles = await getAllArticles()
-
-  return {
-    props: {articles},
-    revalidate: 10,
-  }
+interface ArticleFrontmatter {
+  title: string
+  slug: string
+  date: string
+  excerpt: string
+  image: string
 }
 
-const Articles: React.FC<{articles: Article[]}> = ({articles}) => {
-  const title = 'Articles'
+interface ArticlesPageProps {
+  articles: ArticleFrontmatter[]
+}
 
+const ArticlesPage: NextPage<ArticlesPageProps> = ({articles}) => {
+  const title = 'Articles'
   return (
     <Layout
       meta={{
         title,
-        openGraph: {
-          images: [getOgImage({title})],
-        },
+        // openGraph: {
+        //   images: [getOgImage({title})],
+        // },
       }}
     >
-      <Header title={title} />
       <main className="mx-auto w-full max-w-screen-lg px-5">
-        <ul className="grid grid-cols-2 justify-center gap-5">
-          {articles.map((article) => {
-            const {title, image, summary, slug} = article
-            return (
-              <li key={slug} className="w-full">
-                <Link
-                  href={`/${article.slug}`}
-                  passHref
-                  onClick={() => {
-                    track('clicked view article', {
-                      article: slug,
-                    })
-                  }}
-                >
-                  <article className="mx-auto w-full max-w-screen-md">
-                    {image && image.secure_url && (
-                      <header>
-                        <Image
-                          className="aspect-video rounded"
-                          src={image.secure_url}
-                          width={image.width}
-                          height={image.height}
-                          alt="article illustration"
-                        />
-                      </header>
-                    )}
-
-                    <div className="py-5">
-                      <h2 className="text-3xl font-bold">{title}</h2>
-                      {summary && <p>{summary}</p>}
-                    </div>
-                    <div className="flex items-center gap-1.5">
-                      <Image
-                        src={require('../../public/instructor.png')}
-                        alt={config.author}
-                        width={40}
-                        height={40}
-                        placeholder="blur"
-                        className="rounded-full bg-gray-200"
-                      />
-                      <span>{config.author}</span>
-                    </div>
-                  </article>
-                </Link>
-              </li>
-            )
-          })}
+        <h1>Articles</h1>
+        <ul className="grid grid-cols-2 gap-8">
+          {articles.map((article, index) => (
+            <li key={index}>
+              <Link
+                href={article.slug}
+                className="block h-full border border-white p-4"
+              >
+                <h2>{article.title}</h2>
+                <p>{article.excerpt}</p>
+                <Image
+                  src={`/articles-images${article.image}`}
+                  alt={`Cover image for ${article.title}`}
+                  width={2280}
+                  height={1080}
+                />
+                <p>Date: {article.date}</p>
+              </Link>
+            </li>
+          ))}
         </ul>
       </main>
     </Layout>
   )
 }
 
-export default Articles
+export const getStaticProps: GetStaticProps = async () => {
+  const directory = path.join(
+    process.cwd(),
+    process.env.NEXT_PUBLIC_MDX_ARTICLES_FOLDER as string,
+  )
+  const filenames = fs.readdirSync(directory)
+
+  const articles = filenames
+    .map((filename) => {
+      const filePath = path.join(directory, filename)
+      const fileContents = fs.readFileSync(filePath, 'utf8')
+      const {data} = matter(fileContents)
+      const formattedDate =
+        typeof data.date === 'string' ? data.date : data.date.toISOString()
+
+      return {
+        title: data.title,
+        date: formattedDate,
+        excerpt: data.excerpt,
+        image: data.image,
+        slug: filename.replace(/\.mdx$/, ''),
+      }
+    })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+
+  return {
+    props: {
+      articles,
+    },
+  }
+}
+
+export default ArticlesPage
