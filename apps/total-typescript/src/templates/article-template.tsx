6156b039aba482341a63bd041f73e24d4d70c305
Vojta Holik <vojta@egghead.io>
2024-02-22 18:43:37 +0100

    update(tt): adjust article template cta logic

--- apps/total-typescript/src/templates/article-template.tsx
+++ apps/total-typescript/src/templates/article-template.tsx
@@ -88,7 +88,7 @@ const ArticleTemplate: React.FC<ArticleTemplateProps> = ({
             </span>
             <span>All Articles</span>
           </Link>
-          <h1 className="block text-left font-text text-4xl font-bold sm:text-5xl md:text-6xl">
+          <h1 className="block text-balance text-left font-text text-4xl font-bold sm:text-5xl md:text-6xl">
             {title}
           </h1>
           <div className="mt-10 flex w-full justify-center gap-10">
@@ -114,10 +114,7 @@ const ArticleTemplate: React.FC<ArticleTemplateProps> = ({
         {image && (
           <div
             className={cn(
-              'relative flex h-full w-full flex-col items-end lg:-ml-40 lg:translate-y-16 lg:brightness-125',
-              {
-                '': article.articleType !== 'bookTeaser',
-              },
+              'relative flex h-full w-full flex-col items-end lg:-ml-24 lg:translate-y-16 lg:brightness-125',
             )}
           >
             <Image
@@ -129,7 +126,6 @@ const ArticleTemplate: React.FC<ArticleTemplateProps> = ({
               height={1080 / 2}
               quality={100}
               priority
-              // fill
             />
             <time dateTime={_createdAt} className="pt-3 text-sm text-slate-600">
               Published on {format(new Date(_createdAt), 'MMM dd, y')}
@@ -138,9 +134,7 @@ const ArticleTemplate: React.FC<ArticleTemplateProps> = ({
         )}
       </header>
       <main className="relative z-10 px-5 pt-10">
-        {article.articleType === 'bookTeaser' &&
-        !subscriber &&
-        !loadingSubscriber ? (
+        {article.articleType === 'bookTeaser' ? (
           <BookTeaserCTA withImage={false} className="mb-10" />
         ) : null}
         <div className="prose relative z-10 mx-auto w-full max-w-3xl sm:prose-lg md:prose-xl prose-p:text-gray-300 prose-a:text-cyan-300 prose-a:transition hover:prose-a:text-cyan-200 sm:prose-pre:-mx-5">
@@ -166,7 +160,7 @@ const ArticleTemplate: React.FC<ArticleTemplateProps> = ({
           </div>
         </div>
         <section className="relative z-10 overflow-hidden px-5 pb-24">
-          {!subscriber && !loadingSubscriber && getCTAForArticleType(article)}
+          <ArticleCTA article={article} />
           <Share
             title={title}
             contentType={
@@ -239,12 +233,16 @@ const ArticleBodyCTA: React.FC<
   )
 }
 
-const getCTAForArticleType = (article: Article) => {
+const ArticleCTA: React.FC<{article: Article}> = ({article}) => {
+  const {subscriber, loadingSubscriber} = useConvertkit()
+
   switch (article.articleType) {
     case 'bookTeaser':
       return <BookTeaserCTA className="max-w-4xl" />
     default:
-      return <ArticleNewsletterCta article={article} />
+      return !subscriber && !loadingSubscriber ? (
+        <ArticleNewsletterCta article={article} />
+      ) : null
   }
 }
 
