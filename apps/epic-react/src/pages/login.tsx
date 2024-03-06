43301dcd2168f046bb489b73df00aaef7934984e
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-03-06 12:16:03 +0100

    login page: markup

--- apps/epic-react/src/pages/login.tsx
+++ apps/epic-react/src/pages/login.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import Layout from '@/components/app/layout'
 import {GetServerSideProps} from 'next'
 import {getCsrfToken, getProviders} from 'next-auth/react'
@@ -20,12 +21,23 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const LoginPage: React.FC<LoginTemplateProps> = ({csrfToken, providers}) => {
   return (
-    <Layout meta={{title: `Log in to ${process.env.NEXT_PUBLIC_SITE_TITLE}`}}>
-      <LoginTemplate
-        csrfToken={csrfToken}
-        providers={providers}
-        title={`Log in to ${process.env.NEXT_PUBLIC_SITE_TITLE}`}
+    <Layout
+      meta={{title: `Log in to ${process.env.NEXT_PUBLIC_SITE_TITLE}`}}
+      className="relative"
+    >
+      <Image
+        src="/assets/ring-planet-pattern@2x.jpg"
+        alt="a lost cosmonaut"
+        fill
+        className="relative z-[-1] object-cover"
       />
+      <div className="absolute inset-0 mx-auto flex w-full max-w-full flex-col justify-center">
+        <LoginTemplate
+          csrfToken={csrfToken}
+          providers={providers}
+          title="Sign into your account"
+        />
+      </div>
     </Layout>
   )
 }
