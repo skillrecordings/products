67802a5f2194b63e91cdc0834fb439b5c6924999
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-03-07 13:42:08 +0100

    tooltip for buttons

--- apps/epic-react/src/components/app/theme-toggle.tsx
+++ apps/epic-react/src/components/app/theme-toggle.tsx
@@ -3,21 +3,37 @@
 import * as React from 'react'
 import {Sun, Moon} from '@/components/icons'
 import {useTheme} from 'next-themes'
-import {Button} from '@skillrecordings/ui'
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@skillrecordings/ui'
 
 export function ThemeToggle() {
   const {theme, setTheme} = useTheme()
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => {
-        setTheme(theme === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {theme === 'light' ? <Moon /> : <Sun />}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => {
+              setTheme(theme === 'light' ? 'dark' : 'light')
+            }}
+            className="border-none p-2 opacity-75 transition-opacity duration-150 ease-in-out hover:bg-transparent hover:opacity-100"
+          >
+            {theme === 'light' ? <Moon /> : <Sun />}
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Activate {theme === 'light' ? 'Dark' : 'Light'} Mode</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
