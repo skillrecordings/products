a8c39deb68a8f461da83c76ceaa0412801ec076a
Matt Pocock <mattpocockvoice@gmail.com>
2024-03-27 10:49:36 +0000

    Fixed bug where exercises in folders did not trigger a start command

--- apps/total-typescript/src/exercise/stackblitz-iframe.test.ts
+++ apps/total-typescript/src/exercise/stackblitz-iframe.test.ts
@@ -24,6 +24,16 @@ describe('getStartCommand', () => {
       'src/02-unions-and-indexing/07.1-terminology.solution.2.ts',
       's-07.1',
     ],
+    [
+      'exercise',
+      'src/009-cannot-redeclare-block-scoped-variable.problem/index.ts',
+      'e-009',
+    ],
+    [
+      'exercise',
+      'src/04-example/004-another-example.problem/index.ts',
+      'e-004',
+    ],
   ])(
     `Should calculate the correct start command`,
     (_type, stackblitz, result) => {
