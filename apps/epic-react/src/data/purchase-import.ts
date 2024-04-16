b9060280c97575a5b4f5fb0d35462e370fc32298
jbranchaud <jbranchaud@gmail.com>
2024-04-15 19:23:36 -0500

    feat(er): allow 'Restricted' status for purchases

--- apps/epic-react/src/data/purchase-import.ts
+++ apps/epic-react/src/data/purchase-import.ts
@@ -134,6 +134,7 @@ const PurchaseSchema = z
     ip_address: z.string().nullable(),
     status: z.union([
       z.literal('Valid'),
+      z.literal('Restricted'),
       z.literal('Refunded'),
       z.literal('Banned'),
     ]),
