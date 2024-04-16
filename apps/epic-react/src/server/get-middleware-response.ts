6633989da10fd6a53f170368eec86a665f9da8e6
Zac Jones <zacjones93@gmail.com>
2024-04-16 14:18:33 -0600

    redirect logged in users to /learn route

---
+++ apps/epic-react/src/server/get-middleware-response.ts
@@ -0,0 +1,26 @@
+import {rewriteToPath} from './rewrite-next-response-to-path'
+import {NextRequest, NextResponse} from 'next/server'
+
+import {getToken} from 'next-auth/jwt'
+import {UserSchema} from '@skillrecordings/skill-lesson/utils/ability'
+
+export const SITE_ROOT_PATH = '/'
+
+export async function getMiddlewareResponse(req: NextRequest) {
+  let response = NextResponse.next()
+  const token = await getToken({req})
+
+  if (req.nextUrl.pathname === SITE_ROOT_PATH) {
+    try {
+      const user = UserSchema.parse(token)
+
+      if (user) {
+        response = rewriteToPath('/learn', req)
+      }
+    } catch (error) {
+      response = rewriteToPath('/login', req)
+    }
+  }
+
+  return response
+}
