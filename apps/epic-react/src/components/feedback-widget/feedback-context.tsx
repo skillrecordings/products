bfc53017d1f15bd6b5cdec45bf7c67032dfb60fb
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-05-06 20:13:20 +0200

    temporary changes

---
+++ apps/epic-react/src/components/feedback-widget/feedback-context.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+
+type FeedbackContextType = {
+  isFeedbackDialogOpen: boolean
+  setIsFeedbackDialogOpen: (value: boolean, location?: string) => void
+  feedbackComponent: React.ReactElement | null
+  location: string
+}
+
+const defaultFeedbackContext: FeedbackContextType = {
+  isFeedbackDialogOpen: false,
+  setIsFeedbackDialogOpen: () => {},
+  feedbackComponent: <></>,
+  location: '',
+}
+
+export function useFeedback() {
+  return React.useContext(FeedbackContext)
+}
+
+export const FeedbackContext = React.createContext(defaultFeedbackContext)
+
+export const FeedbackProvider: React.FC<React.PropsWithChildren<any>> = ({
+  children,
+}) => {
+  const [isFeedbackDialogOpen, setIsFeedbackDialogOpen] =
+    React.useState<boolean>(false)
+  const [location, setLocation] = React.useState<string>('navigation')
+  const FeedbackDialog =
+    isFeedbackDialogOpen && dynamic(() => import('./feedback-dialog'))
+  return (
+    <FeedbackContext.Provider
+      value={{
+        isFeedbackDialogOpen,
+        setIsFeedbackDialogOpen: (value, location) => {
+          location && setLocation(location)
+          setIsFeedbackDialogOpen(value)
+        },
+        feedbackComponent: FeedbackDialog ? <FeedbackDialog /> : null,
+        location,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  )
+}
