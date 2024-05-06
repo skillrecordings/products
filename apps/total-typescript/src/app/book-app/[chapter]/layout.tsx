d47bf0255e19bd01303d0b9cde00e3eee477234a
Vojta Holik <vojta@egghead.io>
2024-05-06 15:56:54 +0200

    feat(tt): work on pro essentials book

--- apps/total-typescript/src/app/book-app/[chapter]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-const ChapterLayout: React.FC<
-  React.PropsWithChildren<{params: {chapter: string}}>
-> = async ({children, params}) => {
-  return <div>{children}</div>
-}
-
-export default ChapterLayout
