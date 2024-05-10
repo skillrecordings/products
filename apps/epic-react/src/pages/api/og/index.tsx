26f862081e0f009e8e70efc5fea77f012484f89e
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-05-10 21:49:56 +0200

    wip

--- apps/epic-react/src/pages/api/og/index.tsx
+++ apps/epic-react/src/pages/api/og/index.tsx
@@ -6,74 +6,89 @@ export const config = {
   runtime: 'edge',
 }
 
-const dmSansFont = fetch(
-  new URL('../../../../public/fonts/DMSans-Medium.ttf', import.meta.url),
-).then((res) => res.arrayBuffer())
-
 export default async function handler(req: NextRequest) {
-  const dmSansFontData = await dmSansFont
+  // Images
+  const logoImageData = await fetch(
+    new URL('/public/assets/og-logo.png', import.meta.url),
+  ).then((res) => res.arrayBuffer())
+
+  // Fonts
+  const fontRegulardData = await fetch(
+    new URL('/public/fonts/inter-regular.woff', import.meta.url),
+  ).then((res) => res.arrayBuffer())
+  const fontSemiboldData = await fetch(
+    new URL('/public/fonts/inter-semibold.woff', import.meta.url),
+  ).then((res) => res.arrayBuffer())
 
   try {
     const {searchParams} = new URL(req.url)
-    const hasTitle = searchParams.has('title')
-    const hasByline = searchParams.has('byline')
-    const byline = hasByline ? searchParams.get('byline') : ''
-    const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
-      : 'My Default Title'
-    const hasImage = searchParams.has('image')
-    const image = searchParams.get('image')
-    const hasType = searchParams.has('type')
-    const type = hasType ? searchParams.get('type') : ''
+    const moduleTitle = searchParams.get('moduleTitle')
+    const moduleImage = searchParams.get('moduleImage')
+    const lessonTitle = searchParams.get('lessonTitle')
 
     return new ImageResponse(
       (
         <div
-          tw="flex w-full relative justify-center text-gray-900 items-center h-full justify-between"
           style={{
-            backgroundColor: '#fafafa',
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#0e182a',
+            padding: '30px',
           }}
         >
+          <div style={{display: 'flex', width: '100%', height: '100px'}}>
+            <img
+              width="250"
+              height="70"
+              src={logoImageData as any}
+              alt="logo image"
+              style={{display: 'block'}}
+            />
+          </div>
           <div
-            tw="absolute flex items-center justify-end h-5 w-full bottom-0 left-0 pr-6"
-            style={{background: '#4F75FF'}}
-          ></div>
-          <div tw="flex-1 flex flex-col justify-between h-full pt-12 pb-24 relative px-14">
-            {hasImage ? (
-              <div tw="flex items-center">
-                <img src={image} width={200} height={200} tw="mr-10" />
-                {hasByline && (
-                  <p tw="text-5xl leading-tight font-semibold">{byline}</p>
-                )}
-              </div>
-            ) : (
-              <Logo />
-            )}
-            <p
-              tw="tracking-tight font-bold leading-tight"
-              style={{
-                fontSize: '4rem',
-                fontFamily: 'DM Sans',
-                lineHeight: 1.1,
-              }}
-            >
-              {title}
-            </p>
-            {!hasImage && (
-              <div tw="flex items-center absolute right-14 top-12">
-                <img
-                  src={`${process.env.NEXT_PUBLIC_URL}/instructor.png`}
-                  tw="h-24 rounded-full bg-gray-100"
-                />
-                <p
-                  style={{fontSize: 36, fontFamily: 'DM Sans'}}
-                  tw="text-3xl ml-6 mb-6 text-gray-700"
-                >
-                  {process.env.NEXT_PUBLIC_PARTNER_FIRST_NAME}{' '}
-                  {process.env.NEXT_PUBLIC_PARTNER_LAST_NAME}
-                </p>
-              </div>
-            )}
+            style={{
+              display: 'flex',
+              width: '100%',
+              justifyContent: 'center',
+              height: '220px',
+            }}
+          >
+            <img
+              width="200"
+              height="200"
+              src={moduleImage as string}
+              alt="Module image"
+              style={{display: 'block'}}
+            />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              textAlign: 'center',
+              color: '#9eddf8',
+              fontFamily: 'Inter',
+              fontSize: '32px',
+              height: '64px',
+            }}
+          >
+            {moduleTitle}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              textAlign: 'center',
+              color: '#ffffff',
+              fontFamily: 'Inter Semibold',
+              fontSize: '48px',
+            }}
+          >
+            {lessonTitle}
           </div>
         </div>
       ),
@@ -82,10 +97,16 @@ export default async function handler(req: NextRequest) {
         height: 630,
         fonts: [
           {
-            name: 'DM Sans',
-            data: dmSansFontData,
+            name: 'Inter',
+            data: fontRegulardData,
             style: 'normal',
             weight: 400,
+          },
+          {
+            name: 'Inter Semibold',
+            data: fontSemiboldData,
+            style: 'normal',
+            weight: 600,
           },
         ],
       },
@@ -96,12 +117,4 @@ export default async function handler(req: NextRequest) {
       status: 500,
     })
   }
-}
-
-const Logo = () => {
-  return (
-    <div tw="text-5xl font-bold text-gray-700 py-5">
-      {process.env.NEXT_PUBLIC_SITE_TITLE}
-    </div>
-  )
 }
