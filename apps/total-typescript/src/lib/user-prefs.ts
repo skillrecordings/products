96a3da1ca61778bf994e4b7568b98c25972e90aa
Vojta Holik <vojta@egghead.io>
2024-05-29 16:25:51 +0200

    feat(tt): setup endpoints for setting and getting user local dev environment preferences

---
+++ apps/total-typescript/src/lib/user-prefs.ts
@@ -0,0 +1,153 @@
+import {z} from 'zod'
+import {prisma} from '@skillrecordings/database'
+
+const LOCAL_PREFS_TYPE = 'Local'
+
+const localPrefsFieldsSchema = z.object({
+  path: z.string(),
+  protocol: z.string(),
+})
+
+const userPrefsSchema = z.object({
+  id: z.string(),
+  userId: z.string(),
+  type: z.union([z.literal('Global'), z.literal('Local')]).default('Global'),
+  fields: z.record(z.unknown()).or(z.unknown()).default({}),
+  createdAt: z.date(),
+  updatedAt: z.date().optional().nullable(),
+})
+
+const localUserPrefsSchema = userPrefsSchema.extend({
+  fields: z.record(localPrefsFieldsSchema),
+  type: z.literal('Local'),
+})
+
+export type UserPrefs = z.infer<typeof userPrefsSchema>
+export type LocalUserPrefs = z.infer<typeof localUserPrefsSchema>
+
+export async function setLocalUserPrefs({
+  resourceId,
+  userId,
+  fields,
+}: {
+  resourceId: string
+  userId: string
+  fields: {
+    path: string
+    protocol: string
+  }
+}) {
+  if (!userId) {
+    throw new Error('Unauthorized')
+  }
+
+  // Fetch the user with their local preferences
+  const userWithLocalPrefs = await prisma.user.findFirst({
+    where: {
+      id: userId,
+      prefs: {
+        every: {
+          type: LOCAL_PREFS_TYPE,
+        },
+      },
+    },
+    include: {
+      prefs: true,
+    },
+  })
+
+  let localPrefs
+
+  if (userWithLocalPrefs) {
+    // If the user has local preferences, we prepare to update them
+    const existingFields =
+      typeof userWithLocalPrefs.prefs[0].fields === 'object'
+        ? userWithLocalPrefs.prefs[0].fields
+        : {}
+
+    // Update the user's local preferences, spreading the existing fields
+    // and adding or updating the field with the provided resourceId
+    localPrefs = await prisma.userPrefs.update({
+      where: {
+        id: userWithLocalPrefs.prefs[0].id,
+      },
+      data: {
+        fields: {
+          ...existingFields,
+          [resourceId]: {
+            path: fields.path,
+            protocol: fields.protocol,
+          },
+        },
+        updatedAt: new Date(),
+      },
+    })
+  } else {
+    // If the user doesn't have local preferences, we create them
+    // with the provided resourceId and fields
+    localPrefs = await prisma.userPrefs.create({
+      data: {
+        userId: userId,
+        type: LOCAL_PREFS_TYPE,
+        fields: {
+          [resourceId]: {
+            path: fields.path,
+            protocol: fields.protocol,
+          },
+        },
+      },
+    })
+  }
+
+  // Validate the updated or created local preferences against the schema
+  const parsedLocalPrefs = localUserPrefsSchema.safeParse(localPrefs)
+
+  if (!parsedLocalPrefs.success) {
+    throw new Error('Invalid user prefs')
+  }
+
+  return parsedLocalPrefs.data
+}
+
+export async function getLocalUserPrefs({
+  resourceId,
+  userId,
+}: {
+  resourceId: string
+  userId: string
+}) {
+  if (!userId) {
+    throw new Error('Unauthorized')
+  }
+
+  // Fetch the user with their local dev environment preferences
+  const userWithLocalPrefs = await prisma.user.findFirst({
+    where: {
+      id: userId,
+      prefs: {
+        every: {
+          type: LOCAL_PREFS_TYPE,
+        },
+      },
+    },
+    include: {
+      prefs: true,
+    },
+  })
+
+  if (!userWithLocalPrefs) {
+    return null
+  }
+  // Extract and validate the local preferences
+  const localPrefs = userWithLocalPrefs.prefs[0]
+  const parsedLocalPrefs = localUserPrefsSchema.safeParse(localPrefs)
+
+  if (!parsedLocalPrefs.success) {
+    throw new Error('Invalid user prefs', parsedLocalPrefs.error)
+  }
+
+  // Extract the local preferences for the provided resourceId
+  const localUserPrefsForResource = parsedLocalPrefs.data.fields[resourceId]
+
+  return localUserPrefsForResource
+}
---
+++ apps/total-typescript/src/trpc/routers/user-prefs.ts
@@ -0,0 +1,46 @@
+import {publicProcedure, router} from '@skillrecordings/skill-lesson'
+import {getLocalUserPrefs, setLocalUserPrefs} from '@/lib/user-prefs'
+import {getToken} from 'next-auth/jwt'
+import {z} from 'zod'
+
+export const userPrefsRouter = router({
+  setLocal: publicProcedure
+    .input(
+      z.object({
+        resourceId: z.string(),
+        fields: z.object({
+          path: z.string(),
+          protocol: z.string(),
+        }),
+      }),
+    )
+    .mutation(async ({ctx, input}) => {
+      const token = await getToken({req: ctx.req})
+      if (!token) {
+        throw new Error('Unauthorized')
+      }
+
+      return await setLocalUserPrefs({
+        resourceId: input.resourceId,
+        userId: token.id as string,
+        fields: input.fields,
+      })
+    }),
+  getLocal: publicProcedure
+    .input(
+      z.object({
+        resourceId: z.string(),
+      }),
+    )
+    .query(async ({ctx, input}) => {
+      const token = await getToken({req: ctx.req})
+      if (!token) {
+        throw new Error('Unauthorized')
+      }
+
+      return await getLocalUserPrefs({
+        resourceId: input.resourceId,
+        userId: token.id as string,
+      })
+    }),
+})
