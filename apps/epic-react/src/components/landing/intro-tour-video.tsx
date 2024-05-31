83c50ca5b51cf9c2b0f93cf4faaf51b917cf3807
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-05-31 20:34:14 +0200

    add rounded corners to the Tour video

--- apps/epic-react/src/components/landing/intro-tour-video.tsx
+++ apps/epic-react/src/components/landing/intro-tour-video.tsx
@@ -4,7 +4,7 @@ const TOUR_VIDEO_ID = '02pm13UEJQ4M9Xkc9kIjX6E6gvnZ4ELM5GG57IN2UV3E'
 
 const IntroTourVideo = () => {
   return (
-    <div className="mb-10 overflow-auto rounded-none border border-transparent sm:overflow-hidden sm:rounded-md sm:border-er-gray-100 lg:-mx-8">
+    <div className="mb-10 overflow-hidden rounded-lg border border-transparent sm:overflow-hidden sm:rounded-md sm:border-er-gray-100 lg:-mx-8">
       <MuxPlayer
         streamType="on-demand"
         playbackId={TOUR_VIDEO_ID}
