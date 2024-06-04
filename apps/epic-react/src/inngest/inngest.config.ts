dfd675d4fbb0a9074df939ca8a8b16099c17d293
Zac Jones <zacjones93@gmail.com>
2024-06-04 11:37:02 -0600

    feat(epic react next) add stripe inngest functions to app

--- apps/epic-react/src/inngest/inngest.config.ts
+++ apps/epic-react/src/inngest/inngest.config.ts
@@ -1,6 +1,8 @@
 import {inngest} from '@/inngest/inngest.server'
+import {stripeCheckoutCompleted} from '@/inngest/functions/stripe/checkout-completed'
+import {stripeWebhookReceived} from '@/inngest/functions/stripe/webhook-received'
 
 export const inngestConfig = {
   client: inngest,
-  functions: [],
+  functions: [stripeCheckoutCompleted, stripeWebhookReceived],
 }
