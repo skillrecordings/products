3b6060bdf6b3b1c91194b5ea1c701120e43d6bb6
Zac Jones <zacjones93@gmail.com>
2024-06-04 11:16:14 -0600

    feat(epic-react-next) add stipe events and functions

--- apps/epic-react/src/inngest/inngest.server.ts
+++ apps/epic-react/src/inngest/inngest.server.ts
@@ -1,6 +1,15 @@
 import {Inngest, EventSchemas} from 'inngest'
+import {
+  STRIPE_CHECKOUT_COMPLETED_EVENT,
+  STRIPE_WEBHOOK_RECEIVED_EVENT,
+  type StripeCheckoutCompleted,
+  type StripeWebhookReceived,
+} from '@skillrecordings/inngest'
 
-export type IngestEvents = {}
+export type IngestEvents = {
+  [STRIPE_CHECKOUT_COMPLETED_EVENT]: StripeCheckoutCompleted
+  [STRIPE_WEBHOOK_RECEIVED_EVENT]: StripeWebhookReceived
+}
 export const inngest = new Inngest({
   id: process.env.INNGEST_APP_NAME || process.env.NEXT_PUBLIC_SITE_TITLE,
   schemas: new EventSchemas().fromRecord<IngestEvents>(),
