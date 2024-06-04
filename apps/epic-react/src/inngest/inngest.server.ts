2cddae53a6c75c0b0f4e34b0d2f353ffbca50e74
Zac Jones <zacjones93@gmail.com>
2024-06-04 15:52:14 -0600

    feat(epic react next) add purchase transfer complete function

--- apps/epic-react/src/inngest/inngest.server.ts
+++ apps/epic-react/src/inngest/inngest.server.ts
@@ -9,11 +9,16 @@ import {
   SANITY_WEBHOOK_EVENT,
   SanityWebhookEvent,
 } from './functions/sanity/sanity-inngest-events'
+import {
+  PURCHASE_TRANSFERRED_EVENT,
+  PurchaseTransferred,
+} from '@skillrecordings/skill-lesson/inngest/events'
 
 export type IngestEvents = {
   [STRIPE_CHECKOUT_COMPLETED_EVENT]: StripeCheckoutCompleted
   [STRIPE_WEBHOOK_RECEIVED_EVENT]: StripeWebhookReceived
   [SANITY_WEBHOOK_EVENT]: SanityWebhookEvent
+  [PURCHASE_TRANSFERRED_EVENT]: PurchaseTransferred
 }
 export const inngest = new Inngest({
   id: process.env.INNGEST_APP_NAME || process.env.NEXT_PUBLIC_SITE_TITLE,
