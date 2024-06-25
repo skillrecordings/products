3881e1292d80e89c472f1e4f620f5d0a98070742
Vojta Holik <vojta@egghead.io>
2024-06-25 09:38:46 +0200

    feat(tt): prepare home page cta for PE and CV products

--- apps/total-typescript/src/pages/index.tsx
+++ apps/total-typescript/src/pages/index.tsx
@@ -1,17 +1,24 @@
 import {propsForCommerce} from '@skillrecordings/commerce-server'
-import {CommerceProps} from '@skillrecordings/commerce-server/dist/@types'
+import {
+  CommerceProps,
+  SanityProduct,
+} from '@skillrecordings/commerce-server/dist/@types'
 import {GetServerSideProps} from 'next'
 import {getToken} from 'next-auth/jwt'
 import * as React from 'react'
 import {HomeTemplate} from '../templates/home-template'
-import {getPricing} from '@skillrecordings/skill-lesson/lib/pricing'
+import {getPricing} from '@/lib/pricing'
 
 export const getServerSideProps: GetServerSideProps = async ({req, query}) => {
   const token = await getToken({req})
   const pricing = await getPricing('primary')
   const products = pricing && pricing.products
 
-  return await propsForCommerce({query, token, products})
+  return await propsForCommerce({
+    query,
+    token,
+    products: products as unknown as SanityProduct[],
+  })
 }
 
 export const HomePage: React.FC<React.PropsWithChildren<CommerceProps>> = (
