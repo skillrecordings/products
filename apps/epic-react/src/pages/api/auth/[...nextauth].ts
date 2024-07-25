2dae5eeb7ef77db9581bda4efb9406165317fc97
Nicoll <nicoll.g4@gmail.com>
2024-07-25 14:15:26 -0500

    update styles and add cookies for embeds

--- apps/epic-react/src/pages/api/auth/[...nextauth].ts
+++ apps/epic-react/src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,63 @@
-import NextAuth, {NextAuthOptions, Theme} from 'next-auth'
+import NextAuth, {NextAuthOptions, Theme, type CookiesOptions} from 'next-auth'
 import {createOptions} from '@skillrecordings/skill-api'
 import {NextApiRequest, NextApiResponse} from 'next'
 import {withSentry} from '@sentry/nextjs'
+
+const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL
 
 const productTheme: Theme = {
   colorScheme: 'auto',
   brandColor: '#F59E0B',
 }
 
+const cookies: Partial<CookiesOptions> = {
+  sessionToken: {
+    name: `${VERCEL_DEPLOYMENT ? '__Secure-' : ''}next-auth.session-token`,
+    options: {
+      httpOnly: true,
+      sameSite: 'none', // 'lax',
+      path: '/',
+      secure: true,
+    },
+  },
+  callbackUrl: {
+    name: `${VERCEL_DEPLOYMENT ? '__Secure-' : ''}next-auth.callback-url`,
+    options: {
+      sameSite: 'none', // 'lax',
+      path: '/',
+      secure: true,
+    },
+  },
+  csrfToken: {
+    name: `${VERCEL_DEPLOYMENT ? '__Host-' : ''}next-auth.csrf-token`,
+    options: {
+      httpOnly: true,
+      sameSite: 'none', // 'lax',
+      path: '/',
+      secure: true,
+    },
+  },
+}
+
 export const nextAuthOptions: NextAuthOptions = createOptions({
   theme: productTheme,
+  cookies,
 })
 
 export default async function NextAuthEndpoint(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  withSentry(NextAuth(req, res, createOptions({req, theme: productTheme})))
+  NextAuth(
+    req,
+    res,
+    createOptions({
+      req,
+      theme: productTheme,
+      cookies,
+    }),
+  )
 }
-
 export const config = {
   api: {
     externalResolver: true,
