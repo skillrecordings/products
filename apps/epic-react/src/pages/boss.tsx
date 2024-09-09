6df3cc9564f44adfb4341ec9ddb69c34586c1634
Vojta Holik <vojta@egghead.io>
2024-09-09 12:53:02 +0200

    feat(er): fixes ER-157

---
+++ apps/epic-react/src/pages/boss.tsx
@@ -0,0 +1,21 @@
+import Layout from '@/components/app/layout'
+import BossLetter from '@/components/boss-letter.mdx'
+
+const BossLetterPage = () => {
+  return (
+    <Layout
+      meta={{
+        title: 'Boss Letter',
+        ogImage: {
+          url: 'https://res.cloudinary.com/epic-web/image/upload/v1725878973/dear-boss_2x.jpg',
+        },
+      }}
+    >
+      <main className="prose prose-lg mx-auto w-full max-w-2xl px-5 py-16 dark:prose-invert">
+        <BossLetter />
+      </main>
+    </Layout>
+  )
+}
+
+export default BossLetterPage
