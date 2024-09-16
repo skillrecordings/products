ed582a014be19c2313b4f9fcc5f7d3a2c629b202
Vojta Holik <vojta@egghead.io>
2024-09-16 16:04:24 +0200

    feat(er2): update landing copy structure

--- apps/epic-react/src/components/landing/intro-tour-video.tsx
+++ apps/epic-react/src/components/landing/intro-tour-video.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react'
 import MuxPlayer from '@mux/mux-player-react'
 import {useTheme} from 'next-themes'
-import {useMountedState} from 'react-use'
 
 const TOUR_VIDEO_ID = '02pm13UEJQ4M9Xkc9kIjX6E6gvnZ4ELM5GG57IN2UV3E'
 
 const IntroTourVideo = () => {
   const {theme} = useTheme()
-  const isMounted = useMountedState()
   const playerRef = React.useRef(null)
+  const poster =
+    theme === 'light'
+      ? `https://res.cloudinary.com/epic-web/image/upload/v1726494776/tour-of-epic-react-v2-thumbnail--light_2x.jpg`
+      : `https://res.cloudinary.com/epic-web/image/upload/v1726492067/tour-of-epic-react-v2-thumbnail_2x.jpg`
   React.useEffect(() => {
-    if (playerRef.current && isMounted) {
-      playerRef.current.poster =
-        theme === 'light'
-          ? `https://res.cloudinary.com/epic-web/image/upload/v1726494776/tour-of-epic-react-v2-thumbnail--light_2x.jpg`
-          : `https://res.cloudinary.com/epic-web/image/upload/v1726492067/tour-of-epic-react-v2-thumbnail_2x.jpg`
+    if (playerRef.current) {
+      playerRef.current.poster = poster
     }
-  }, [isMounted, theme, playerRef])
+  }, [theme, playerRef])
 
   return (
     <div className="mx-auto mb-10 max-w-4xl overflow-hidden rounded-lg border border-transparent sm:overflow-hidden sm:rounded-md sm:border-er-gray-100">
@@ -25,11 +24,7 @@ const IntroTourVideo = () => {
         playbackId={TOUR_VIDEO_ID}
         className="block"
         ref={playerRef}
-        poster={
-          theme === 'light'
-            ? `https://res.cloudinary.com/epic-web/image/upload/v1726494776/tour-of-epic-react-v2-thumbnail--light_2x.jpg`
-            : `https://res.cloudinary.com/epic-web/image/upload/v1726492067/tour-of-epic-react-v2-thumbnail_2x.jpg`
-        }
+        poster={poster}
       />
     </div>
   )
