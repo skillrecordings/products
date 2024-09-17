35293cedcc5d9f5733bed651678e3026682a3a1b
Vojta Holik <vojta@egghead.io>
2024-09-17 15:42:01 +0200

    style(er2): tweaks

--- apps/epic-react/src/components/app/navigation/index.tsx
+++ apps/epic-react/src/components/app/navigation/index.tsx
@@ -765,7 +765,7 @@ export const productOnSalePathBuilder = (product: {
   if (product.type === 'live') {
     return `/events/${product.slug}`
   } else if (product.modules && product.modules.length > 1) {
-    return `/products/${product.slug}`
+    return '/#buy' // `/products/${product.slug}`
   } else {
     return `/workshops/${product?.modules?.[0]?.slug?.current}`
   }
