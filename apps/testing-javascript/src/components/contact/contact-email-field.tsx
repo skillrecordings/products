5c8cfc2d34a5db947f54e8c4efc9ac33b346480c
Nicoll <nicoll.g4@gmail.com>
2024-09-17 13:54:25 -0500

    feat(tjs): add feedback widget and bulk-form

---
+++ apps/testing-javascript/src/components/contact/contact-email-field.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react'
+import cx from 'classnames'
+import {FormikErrors, FormikTouched, useField} from 'formik'
+import {FeedbackFormValues} from '@/feedback-widget/form'
+import isEmpty from 'lodash/isEmpty'
+import {useSession} from 'next-auth/react'
+import Spinner from '@skillrecordings/skill-lesson/spinner'
+
+const ContactEmailField: React.FC<
+  React.PropsWithChildren<{
+    errors: FormikErrors<FeedbackFormValues>
+    touched: FormikTouched<FeedbackFormValues>
+  }>
+> = ({errors, touched}) => {
+  const {data: user, status: userLoadingStatus} = useSession()
+  const [field] = useField({
+    name: 'email',
+  })
+
+  return (
+    <div>
+      <div className="flex w-full items-center justify-between">
+        <label
+          className="inline-block flex-shrink-0 pb-1 font-semibold"
+          htmlFor="text"
+        >
+          Your email address{' '}
+          {userLoadingStatus === 'loading' ? (
+            <Spinner aria-hidden="true" className="inline-block h-3 w-3" />
+          ) : (
+            !user?.user?.email && (
+              <span className="font-normal text-gray-500">(required)</span>
+            )
+          )}
+        </label>
+        {errors.email && touched.email ? (
+          <div
+            aria-live="polite"
+            className="inline-block pb-1 text-xs font-medium leading-tight text-pink-300 sm:text-sm"
+          >
+            {errors.email}
+          </div>
+        ) : null}
+      </div>
+      <div
+        className={cx({
+          'rounded-md ring-pink-600 ring-opacity-20 ring-offset-1':
+            errors.email && touched.email,
+        })}
+      >
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required={true}
+          value={field.value}
+          onChange={(e) => {
+            field.onChange(e)
+          }}
+          onBlur={(e) => {
+            field.onBlur(e)
+          }}
+          placeholder="you@example.com"
+          disabled={!isEmpty(user)}
+          className={cx(
+            `block w-full rounded-md border border-gray-200 bg-gray-100 p-3 shadow focus:ring-blue-300 `,
+            {
+              'bg-gray-100 bg-transparent text-gray-600 shadow-none ':
+                !isEmpty(user),
+            },
+          )}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ContactEmailField
---
+++ apps/testing-javascript/src/pages/for-teams.tsx
@@ -0,0 +1,46 @@
+import Layout from '@/components/layout'
+import React from 'react'
+import BulkForm from '@/components/contact/bulk-form'
+import ForTeams from '@/components/for-teams-copy.mdx'
+import {Companies} from '@/components/companies'
+
+const ForTeamsPage = () => {
+  return (
+    <Layout
+      meta={{
+        title: 'Testing JavaScript For Teams',
+        description:
+          'Improve the skills of your team with Testing JavaScript. Get a quote today and see how we can help you.',
+        ogImage: {
+          url: '',
+          alt: '',
+        },
+      }}
+    >
+      <header className="mx-auto w-full max-w-screen-md px-5 pt-16 text-center text-2xl font-bold">
+        <h1 className="mx-auto text-center text-4xl font-semibold">
+          Testing JavaScript For Teams
+        </h1>
+      </header>
+      <main>
+        <div className="prose prose-lg mx-auto w-full max-w-2xl px-5 py-16  text-black">
+          <ForTeams />
+          <Companies />
+        </div>
+        <div className="mx-auto text-center ">
+          <h1 className=" text-2xl font-semibold">
+            Invest in Your Team, Invest in Your Future
+          </h1>
+          <p className="pb-12 pt-4 text-lg">
+            Fill out the form below to request a quote for your team:
+          </p>
+        </div>
+        <div className="mx-auto flex w-full max-w-lg items-center justify-center px-5 pb-16">
+          <BulkForm />
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
+export default ForTeamsPage
