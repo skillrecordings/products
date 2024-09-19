b403f80973bc2e6a68c598bd71e3b9f316758fad
joel <joelhooks@gmail.com>
2024-09-19 09:12:37 -0700

    fix: protect action server side

--- apps/epic-react/src/app/launch/page.tsx
+++ apps/epic-react/src/app/launch/page.tsx
@@ -1,11 +1,27 @@
 import RedButton from '@/app/launch/red-button'
 import {launch} from '@/app/launch/actions'
+import {getServerSession} from 'next-auth/next'
+import {notFound} from 'next/navigation'
 
 export default async function LaunchPage() {
+  const session = await getServerSession({
+    callbacks: {
+      session: async ({session, token}) => {
+        if (token?.id && session.user) {
+          // @ts-ignore
+          session.user.role = token.role
+        }
+        return session
+      },
+    },
+  })
+
+  const canPress = ['ADMIN', 'SUPERADMIN'].includes(session?.user?.role || '')
+
   return (
     <div>
       <form action={launch}>
-        <RedButton />
+        <RedButton canPress={canPress} />
       </form>
     </div>
   )
