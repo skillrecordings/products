3b09534ee5506c0d79af57e0cf0ab284ec529c41
joel <joelhooks@gmail.com>
2024-09-19 08:08:33 -0700

    feat(erv2): launch button also updates status of products

--- apps/epic-react/src/app/launch/actions.ts
+++ apps/epic-react/src/app/launch/actions.ts
@@ -14,4 +14,26 @@ export async function launch() {
       active: true,
     })
     .commit()
+
+  const PRO_ID = '2b20d9d6-239e-48e6-8862-9162ec009a83'
+  const STANDARD_ID = '2c7866b2-2aa2-48bd-846c-b6aa1388ae5f'
+  const BASIC_ID = '1ee952f5-9bf2-47f7-9c91-a6fe8c10a509'
+
+  const sanityProductIds = [PRO_ID, STANDARD_ID, BASIC_ID]
+
+  for (const sanityProductId of sanityProductIds) {
+    await sanityWriteClient
+      .patch(sanityProductId)
+      .set({
+        state: 'active',
+      })
+      .commit()
+  }
+
+  await sanityWriteClient
+    .patch(prices._id)
+    .set({
+      active: true,
+    })
+    .commit()
 }
