dc1c54700918670e3ac55be36337838a7ef36ce8
joel <joelhooks@gmail.com>
2024-09-21 21:08:26 -0700

    faq entries

--- apps/epic-react/src/pages/privacy.tsx
+++ apps/epic-react/src/pages/privacy.tsx
@@ -31,7 +31,7 @@ Please read these Terms of Service carefully before accessing or using our websi
 
 Any new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.
 
-Our storefront is created with Gatsby. They provide us with the online e-commerce platform that allows us to sell our products and services to you.
+Our storefront is created with Next.js and served on Vercel. They provide us with the online platform that allows us to sell our products and services to you.
 
 ## Section 1: Online Store Terms
 
