84b534f0cb03fdeb329ad3fe1351a2e3c81851cc
Zac Jones <zacjones93@gmail.com>
2024-09-23 09:17:02 -0600

    fix(er): keep launch page h1 text-white

--- apps/epic-react/src/app/launch/red-button.tsx
+++ apps/epic-react/src/app/launch/red-button.tsx
@@ -62,7 +62,7 @@ export default function RedButton({canPress}: {canPress: boolean}) {
       }}
     >
       <Starfield speed={starfieldSpeed} />
-      <h1 className="left-.5 absolute top-7 z-10 text-5xl font-bold opacity-50">
+      <h1 className="left-.5 absolute top-7 z-10 text-5xl font-bold text-white opacity-50">
         epicreact.dev/launch
       </h1>
       <button
