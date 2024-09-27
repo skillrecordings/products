61cfc03677f3ed77c3f8ad06ff1cebf375385176
joel <joelhooks@gmail.com>
2024-09-27 11:37:01 -0700

    fix for types

--- apps/epic-react/src/pages/products/index.tsx
+++ apps/epic-react/src/pages/products/index.tsx
@@ -56,7 +56,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         .string()
         .optional()
         .parse(query?.coupon || query?.code),
-    )) || query?.coupon
+    )) || (query?.coupon as string)
 
   const allowPurchase =
     pricingActive ||
