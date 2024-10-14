e9c244ae366b27639e937177396461a84ffed40c
Vojta Holik <vojta@egghead.io>
2024-10-14 16:09:22 +0200

    feat(ewd): workshops search and filter

--- apps/epic-web/src/pages/workshops/index.tsx
+++ apps/epic-web/src/pages/workshops/index.tsx
@@ -17,12 +17,29 @@ import {
 } from '@skillrecordings/skill-lesson/video/module-progress'
 import {trpc} from 'trpc/trpc.client'
 import {createAppAbility} from '@skillrecordings/skill-lesson/utils/ability'
-import {Progress, Skeleton} from '@skillrecordings/ui'
+import {
+  Input,
+  Label,
+  Progress,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Skeleton,
+} from '@skillrecordings/ui'
 import {getAllBonuses} from 'lib/bonuses'
 import {cn} from '@skillrecordings/ui/utils/cn'
 
 import type {Workshop} from 'lib/workshops'
 import {getFullStackVol1Workshops} from '../../lib/workshops'
+import {useDebounce} from 'pages/search'
+import type {Contributor} from 'lib/contributors'
+import groq from 'groq'
+import {sanityClient} from 'utils/sanity-client'
+import {Search} from 'lucide-react'
+import Spinner from 'components/spinner'
+import type {SearchResult} from 'trpc/routers/search'
 
 export async function getStaticProps() {
   const workshops = await getAllWorkshops()
@@ -31,14 +48,33 @@ export async function getStaticProps() {
   const fullStackWorkshopSeriesProduct = await getProduct(
     process.env.NEXT_PUBLIC_DEFAULT_PRODUCT_ID,
   )
+  const contributors = await sanityClient.fetch(groq`
+  *[_type == 'contributor'] {
+    _id,
+    name,
+    "slug": slug.current,
+    picture {
+      "url": asset->url,
+      alt
+    },
+    'workshops': *[_type == 'module' && moduleType == 'workshop' && references(^._id)] {
+      _id,
+    }
+  }`)
+
+  const contributorsWithWorkshops = contributors.filter(({workshops}: any) => {
+    return workshops.length > 0
+  })
 
   return {
     props: {
-      workshops: workshops.filter((workshop) => {
-        return !fullStackVol1Workshops.some((fullStackWorkshop) => {
-          return workshop.slug.current === fullStackWorkshop.slug.current
-        })
-      }),
+      workshops,
+      contributors: contributorsWithWorkshops,
+      // workshops: workshops.filter((workshop) => {
+      //   return !fullStackVol1Workshops.some((fullStackWorkshop) => {
+      //     return workshop.slug.current === fullStackWorkshop.slug.current
+      //   })
+      // }),
       fullStackVol1Workshops: fullStackVol1Workshops.filter((workshop) => {
         return workshop.moduleType !== 'bonus'
       }),
@@ -60,10 +96,12 @@ const sectionsFlatMap = (sections: any[]) => {
 
 const WorkshopsPage: React.FC<{
   workshops: Workshop[]
+  contributors: Contributor[]
   fullStackVol1Workshops: Workshop[]
   bonuses?: Module[]
   fullStackWorkshopSeriesProduct: Product
 }> = ({
+  contributors,
   workshops,
   fullStackWorkshopSeriesProduct,
   bonuses,
@@ -81,6 +119,30 @@ const WorkshopsPage: React.FC<{
 
   const canViewContent = ability.can('view', 'Content')
   const isRestricted = ability.can('view', 'RegionRestriction')
+  const [query, setQuery] = React.useState('')
+  const [contributor, setContributor] = React.useState('')
+
+  const router = useRouter()
+
+  React.useEffect(() => {
+    router.query.q && setQuery(router.query.q.toString())
+    router.query.c && setContributor(router.query.c.toString())
+  }, [router])
+
+  const debouncedQuery = useDebounce(query, 500)
+  const {data: resultsForQuery, isFetching} =
+    trpc.search.resultsForQuery.useQuery(
+      {
+        query: debouncedQuery,
+        resourceType: 'module',
+        moduleType: 'workshop',
+        orderBy: 'newest',
+        contributor,
+      },
+      {
+        initialData: workshops,
+      },
+    )
 
   return (
     <Layout
@@ -92,22 +154,19 @@ const WorkshopsPage: React.FC<{
         },
       }}
     >
-      <header className="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-between px-5 pt-16 lg:flex-row lg:items-start">
-        <div className="flex flex-col items-center space-y-3 text-center lg:items-start lg:text-left">
-          <h1 className="flex flex-col text-4xl font-semibold">
-            <span className="mb-2 inline-block bg-gradient-to-r from-blue-500 to-fuchsia-600 bg-clip-text text-xs uppercase tracking-widest text-transparent dark:from-blue-300 dark:to-fuchsia-400">
+      <header className="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,#EAEBFF_0%,transparent_65%)] px-5 pt-12 dark:bg-[radial-gradient(ellipse_at_top,#1a1e2c_0%,transparent_80%)] sm:pt-16 lg:flex-row lg:items-start">
+        <div className="flex flex-col items-center space-y-5 text-center lg:items-center lg:text-center">
+          <h1 className="flex flex-col text-3xl font-semibold sm:text-4xl lg:text-5xl">
+            <span className="mx-auto mb-4 inline-flex bg-gradient-to-r from-blue-500 to-fuchsia-600 bg-clip-text text-sm uppercase tracking-widest text-transparent dark:from-blue-300 dark:to-fuchsia-400">
               Professional
             </span>{' '}
-            Full Stack Development Workshops
+            Development Workshops
           </h1>
-          <h2 className="w-full max-w-md text-base text-gray-600 dark:text-gray-400">
-            <Balancer>
-              A collection of exercise-driven, in-depth Web Development
-              workshops.
-            </Balancer>
+          <h2 className="w-full max-w-lg text-balance text-gray-600 dark:text-gray-400 sm:text-lg">
+            A collection of exercise-driven, in-depth Web Development workshops.
           </h2>
         </div>
-        <div className="flex w-full max-w-md items-center justify-center pt-16 lg:min-h-[204px] lg:justify-end lg:pl-8 lg:pt-0">
+        {/* <div className="flex w-full max-w-md items-center justify-center pt-16 lg:min-h-[204px] lg:justify-end lg:pl-8 lg:pt-0">
           {abilityRulesStatus === 'loading' ? (
             <div className="relative">
               <ProductCTA
@@ -129,12 +188,90 @@ const WorkshopsPage: React.FC<{
               )}
             </>
           )}
-        </div>
+        </div> */}
       </header>
-      <main className="relative z-10 mx-auto flex w-full max-w-screen-lg flex-col justify-center gap-5 px-5 pb-24 pt-16">
-        {workshops && (
-          <ul className="flex flex-col gap-5">
-            {workshops.map((workshop, i) => {
+      <main className="relative z-10 mx-auto flex w-full max-w-screen-lg flex-col justify-center gap-5 px-5 pb-24 pt-8">
+        <div className="flex w-full items-center justify-center gap-10 sm:justify-between">
+          <div className="flex w-full flex-col items-end gap-2 sm:flex-row">
+            <div className="relative flex w-full items-center">
+              <Search className="absolute left-3 h-4 w-4 text-gray-500" />
+              <Input
+                className="w-full pl-9"
+                type="search"
+                placeholder="Search workshops"
+                value={query}
+                onChange={(e) => {
+                  if (e.target.value) {
+                    router.push(
+                      router.pathname,
+                      {
+                        query: {q: e.target.value},
+                      },
+                      {
+                        shallow: true,
+                      },
+                    )
+                  } else {
+                    router.push(
+                      router.pathname,
+                      {
+                        query: {},
+                      },
+                      {
+                        shallow: true,
+                      },
+                    )
+                  }
+                  return setQuery(e.target.value)
+                }}
+              />
+              <div className="absolute right-1 flex h-full w-8 items-center justify-center sm:hidden">
+                {isFetching && <Spinner className="h-4 w-4" />}
+              </div>
+            </div>
+            <Label htmlFor="contributor" className="w-full sm:max-w-[220px]">
+              <span className="pb-2 text-sm opacity-90">Instructor</span>
+              <Select
+                defaultValue={contributor}
+                onValueChange={(value) => {
+                  return setContributor(value)
+                }}
+              >
+                <SelectTrigger className="w-full [&_span]:flex [&_span]:items-center [&_span]:gap-1 [&_span]:pr-3">
+                  <SelectValue placeholder="Instructor" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value={''}>All</SelectItem>
+                  {contributors.map(({slug, name, picture}) => {
+                    return (
+                      <SelectItem
+                        className="[&_span]:flex [&_span]:items-center [&_span]:gap-1"
+                        value={slug}
+                      >
+                        {picture?.url && (
+                          <Image
+                            src={picture?.url}
+                            alt={name}
+                            className="mr-2 rounded-full"
+                            width={24}
+                            height={24}
+                          />
+                        )}
+                        {name}
+                      </SelectItem>
+                    )
+                  })}
+                </SelectContent>
+              </Select>
+            </Label>
+          </div>
+          <div className="hidden h-full w-8 items-center justify-center sm:flex">
+            {isFetching && <Spinner className="-mb-5 h-5 w-5" />}
+          </div>
+        </div>
+        {resultsForQuery && (
+          <ul className={cn('grid grid-cols-1 gap-5 md:grid-cols-2', {})}>
+            {resultsForQuery.map((workshop: SearchResult) => {
               return (
                 <ModuleProgressProvider moduleSlug={workshop.slug.current}>
                   <Teaser module={workshop} key={workshop.slug.current} />
@@ -143,7 +280,7 @@ const WorkshopsPage: React.FC<{
             })}
           </ul>
         )}
-        {fullStackVol1Workshops && (
+        {/* {fullStackVol1Workshops && (
           <ul className="flex flex-col gap-5">
             <div className="relative flex items-center justify-center py-5">
               <h3 className="relative z-10 bg-background px-3 py-1 text-center font-mono text-sm font-medium uppercase">
@@ -186,7 +323,7 @@ const WorkshopsPage: React.FC<{
               )
             })}
           </ul>
-        )}
+        )} */}
       </main>
     </Layout>
   )
@@ -195,14 +332,13 @@ const WorkshopsPage: React.FC<{
 export default WorkshopsPage
 
 const Teaser: React.FC<{
-  module: Workshop | Module
+  module: SearchResult
   index?: number
 }> = ({module, index}) => {
   let {title, slug, image, description} = module
   let content = 'lessons' in module ? module.lessons : module.sections
   let instructor = 'instructor' in module ? module.instructor : null
 
-  const router = useRouter()
   const moduleProgress = useModuleProgress()
   const isModuleInProgress = (moduleProgress?.completedLessonCount || 0) > 0
 
@@ -218,16 +354,17 @@ const Teaser: React.FC<{
 
   const canViewContent = ability.can('view', 'Content')
   const ref = React.useRef(null)
-  const lessonType =
-    content &&
-    (content?.[0]?._type === 'section'
-      ? sectionsFlatMap(content)[0]?._type
-      : content[0]?._type)
+  const lessonType = 'lesson'
+  const lessonCount = module.lessonCount
+  // content &&
+  // (content?.[0]?._type === 'section'
+  //   ? sectionsFlatMap(content)[0]?._type
+  //   : content[0]?._type)
 
   return (
     <motion.li>
       <Link
-        className="relative flex w-full flex-col items-center gap-10 overflow-hidden rounded-md border border-gray-100 bg-white bg-gradient-to-tr from-transparent to-white/50 p-5 shadow-soft-xl transition before:absolute before:left-0 before:top-0 before:h-px before:w-full before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent before:content-[''] dark:border-transparent dark:from-gray-900 dark:to-gray-800 dark:hover:brightness-110 md:flex-row md:p-10 md:pl-16"
+        className="relative flex h-full w-full flex-col items-center gap-5 overflow-hidden rounded-md border border-gray-100 bg-white bg-gradient-to-tr from-transparent to-white/50 p-5 px-8 pb-8 shadow-soft-xl transition before:absolute before:left-0 before:top-0 before:h-px before:w-full before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent before:content-[''] dark:border-transparent dark:from-gray-900 dark:to-gray-800 dark:hover:brightness-110 md:flex-col"
         href={{
           pathname:
             module.moduleType === 'bonus'
@@ -272,24 +409,26 @@ const Teaser: React.FC<{
             <Image
               src={image}
               alt={title}
-              width={300}
+              width={250}
+              height={250}
               quality={100}
-              height={300}
               priority
             />
           </div>
         )}
-        <div className="flex w-full flex-col items-center text-center md:items-start md:text-left">
+        <div className="flex w-full flex-col items-center text-center md:items-center md:text-center">
           <div className="flex w-full items-center justify-center gap-3 md:justify-start">
-            <h3 className="w-full max-w-xl text-2xl font-semibold sm:text-3xl">
-              <Balancer>{title}</Balancer>
+            <h3 className="w-full max-w-xl text-balance text-2xl font-semibold sm:text-3xl">
+              {title}
             </h3>
           </div>
           {isModuleInProgress ? (
-            <div className="mt-3 flex w-full items-center justify-center gap-2 font-mono text-xs md:justify-start">
+            <div className="mt-3 flex w-full items-center justify-center gap-2 text-xs font-medium md:justify-center">
               <span className="uppercase opacity-75">
-                {moduleProgress?.completedLessonCount}/
-                {moduleProgress?.lessonCount} completed
+                <span className="font-semibold">
+                  {moduleProgress?.completedLessonCount}
+                </span>{' '}
+                / {moduleProgress?.lessonCount} completed
               </span>
               <Progress
                 value={moduleProgress?.percentComplete}
@@ -297,15 +436,20 @@ const Teaser: React.FC<{
                 max={100}
               />
             </div>
-          ) : null}
+          ) : (
+            <div
+              // spacer
+              className="h-[28px]"
+            />
+          )}
           {description && (
-            <div className="pt-5">
-              <p className="text-gray-600 dark:text-gray-300">
-                <Balancer>{description}</Balancer>
+            <div className="pt-3">
+              <p className="text-balance text-gray-600 dark:text-gray-300">
+                {description}
               </p>
             </div>
           )}
-          <div className="flex flex-row items-center gap-3 pt-6 text-sm text-gray-600 dark:text-gray-400">
+          <div className="flex flex-row items-center gap-3 pt-4 text-sm text-gray-600 dark:text-gray-400">
             <div className="flex items-center gap-2 overflow-hidden rounded-full sm:justify-center">
               <div className="flex items-center justify-center overflow-hidden rounded-full bg-gray-200 dark:bg-background">
                 <Image
@@ -320,12 +464,11 @@ const Teaser: React.FC<{
               </div>
               <span>{instructor?.name || 'Kent C. Dodds'}</span>
             </div>
-            {moduleProgress?.lessonCount && (
+            {lessonCount && (
               <>
                 {'・'}
                 <div>
-                  {moduleProgress.lessonCount}{' '}
-                  {pluralize(lessonType, moduleProgress.lessonCount)}
+                  {lessonCount} {pluralize(lessonType, lessonCount)}
                 </div>
               </>
             )}
--- apps/epic-web/src/trpc/routers/search.ts
+++ apps/epic-web/src/trpc/routers/search.ts
@@ -2,23 +2,39 @@ import {publicProcedure, router} from '@skillrecordings/skill-lesson'
 import {z} from 'zod'
 import {sanityClient} from '@skillrecordings/skill-lesson/utils/sanity-client'
 
+function getOrder(orderBy: (typeof searchQuerySchema)['_output']['orderBy']) {
+  switch (orderBy) {
+    case 'newest':
+      return '_createdAt desc'
+    default:
+      return ''
+  }
+}
+
+const searchQuerySchema = z.object({
+  query: z.string(),
+  resourceType: z.string().optional(),
+  moduleType: z.string().optional(),
+  numResults: z.number().default(20),
+  orderBy: z.enum(['newest']).optional().default('newest'),
+  contributor: z.string().optional(),
+})
+
 export const searchRouter = router({
   resultsForQuery: publicProcedure
-    .input(
-      z.object({
-        query: z.string(),
-        resourceType: z.string().optional(),
-        numResults: z.number().default(20),
-      }),
-    )
+    .input(searchQuerySchema)
     .query(async ({ctx, input}) => {
-      const {resourceType} = input
+      const {resourceType, moduleType, orderBy, contributor} = input
       const results = await sanityClient.fetch(
         `  *[_type in [${
           resourceType
             ? `"${resourceType}"`
             : '"article", "tip", "module", "exercise", "explainer", "talk"'
-        }]]
+        }]${moduleType ? ` && moduleType == "${moduleType}"` : ''}${
+          contributor
+            ? ` && contributors[@.role == 'instructor'][0].contributor->slug.current == "${contributor}"`
+            : ''
+        }]
     | score(
       title match $searchQuery 
       || _type match "module"
@@ -30,7 +46,7 @@ export const searchRouter = router({
       || boost(pt::text(transcript) match $searchQuery, 0.5)
 
     )
-    | order(_score desc)
+    | order(_score desc${orderBy ? `, ${getOrder(orderBy)}` : ''})
     {
       description,
       _score,
@@ -38,6 +54,24 @@ export const searchRouter = router({
       title,
       slug,
       _type,
+      "image": image.asset->url,
+      "lessonCount": count(resources[@->._type in ['lesson', 'exercise', 'explainer']] + resources[@->._type == 'section']->resources[@->._type in ['lesson', 'exercise', 'explainer']]),
+      "instructor": contributors[@.role == 'instructor'][0].contributor->{
+        _id,
+        _type,
+        _updatedAt,
+        _createdAt,
+        name,
+        bio,
+        links[] {
+          url, label
+        },
+        picture {
+            "url": asset->url,
+            alt
+        },
+        "slug": slug.current,
+      },
       moduleType,
       "section": *[_type in ['section'] && references(^._id)][0]{
         _id,
@@ -61,3 +95,59 @@ export const searchRouter = router({
       return results
     }),
 })
+
+export const searchResultSchema = z.object({
+  description: z.string().nullable(),
+  _score: z.number(),
+  _id: z.string(),
+  title: z.string(),
+  slug: z.object({
+    current: z.string(),
+  }),
+  _type: z.string(),
+  image: z.string().url().nullable(),
+  lessonCount: z.number().nullable(),
+  instructor: z
+    .object({
+      _id: z.string(),
+      _type: z.string(),
+      _updatedAt: z.string(),
+      _createdAt: z.string(),
+      name: z.string(),
+      bio: z.string().nullable(),
+      links: z
+        .array(
+          z.object({
+            url: z.string().url(),
+            label: z.string(),
+          }),
+        )
+        .nullable(),
+      picture: z
+        .object({
+          url: z.string().url(),
+          alt: z.string().nullable(),
+        })
+        .nullable(),
+      slug: z.string(),
+    })
+    .nullable(),
+  moduleType: z.string(),
+  sections: z
+    .object({
+      _id: z.string(),
+      slug: z.string(),
+      title: z.string(),
+    })
+    .nullable(),
+  module: z
+    .object({
+      _id: z.string(),
+      slug: z.string(),
+      title: z.string(),
+      moduleType: z.string().nullable(),
+    })
+    .nullable(),
+})
+
+export type SearchResult = z.infer<typeof searchResultSchema>
