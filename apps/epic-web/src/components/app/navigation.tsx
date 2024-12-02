b55b435db6a6e209a9354937d29b66e32c3176a2
Vojta Holik <vojta@egghead.io>
2024-12-02 15:32:39 +0100

    feat(ewd): megabundle ctas

--- apps/epic-web/src/components/app/navigation.tsx
+++ apps/epic-web/src/components/app/navigation.tsx
@@ -977,7 +977,8 @@ export const productOnSalePathBuilder = (product: {
   if (product.type === 'live') {
     return `/events/${product.slug}`
   } else if (product.modules && product.modules.length > 1) {
-    return `/products/${product.slug}`
+    // TODO: make this more robust
+    return `/${product.slug}`
   } else {
     return `/workshops/${product?.modules?.[0]?.slug?.current}`
   }
@@ -1004,8 +1005,12 @@ export const Banner: React.FC<{
   if (router.query.lesson) {
     return null
   }
+
   // Don't show the banner on product page
   if (router.query.slug === productOnSale?.slug) {
+    return null
+  }
+  if (router.pathname === `/${productOnSale?.slug}`) {
     return null
   }
 
